4cc810986ed87ffb23437321a899b1b361d933f4
Zhong Jianxin <azuwis@gmail.com>
2021-07-22 09:40:33 +0800

    Add system wide vim package

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -5,10 +5,9 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [
-      # pkgs.vim
-    ];
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -26,6 +25,9 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
+  environment.variables = {
+    EDITOR = "vim";
+  };
   system.defaults.NSGlobalDomain = {
     InitialKeyRepeat = 20;
     KeyRepeat = 2;
