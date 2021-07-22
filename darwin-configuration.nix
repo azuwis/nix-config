de6156744b58875f5f981cb6c62e0357239991a9
Zhong Jianxin <azuwis@gmail.com>
2021-07-22 09:34:04 +0800

    Init

---
+++ darwin-configuration.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <home-manager/nix-darwin> ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [
+      # pkgs.vim
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  system.defaults.NSGlobalDomain = {
+    InitialKeyRepeat = 20;
+    KeyRepeat = 2;
+  };
+  system.defaults.dock.autohide = true;
+  system.defaults.trackpad = {
+    Clicking = true;
+    Dragging = true;
+    TrackpadThreeFingerDrag = true;
+  };
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+  system.patches = [
+    (pkgs.writeText "pam-sudo.patch" ''
+      --- a/etc/pam.d/sudo
+      +++ b/etc/pam.d/sudo
+      @@ -1,4 +1,5 @@
+       # sudo: auth account password session
+      +auth       sufficient     pam_tid.so
+       auth       sufficient     pam_smartcard.so
+       auth       required       pam_opendirectory.so
+       account    required       pam_permit.so
+    '')
+  ];
+
+  # Home Manager
+  # nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.users.azuwis = import ./home.nix { email = "azuwis@gmail.com"; name = "Zhong Jianxin"; };
+  environment.pathsToLink = [ "/share/zsh" ];
+}
---
+++ home.nix
@@ -0,0 +1,69 @@
+{ email, name }: { pkgs, ... }: {
+  home.packages = with pkgs; [
+    pure-prompt
+    zsh-fast-syntax-highlighting
+    zsh-history-substring-search
+  ];
+
+  # Git
+  programs.git = {
+    enable = true;
+    userEmail = email;
+    userName = name;
+    aliases = {
+      ci = "commit";
+      cp = "cherry-pick";
+      fixup = "!sh -c 'git commit --fixup=$1 && git rebase --interactive --autosquash $1~' -";
+      lg = "log --abbrev-commit --graph --date=relative --pretty=format:'%C(yellow)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset'";
+      st = "status";
+    };
+  };
+
+  # Vim
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      lightline-vim
+      vim-nix
+    ];
+    extraConfig = ''
+      set noshowmode
+      let g:lightline = {
+        \ 'colorscheme': 'seoul256',
+        \ }
+    '';
+  };
+
+  # Zsh
+  programs.fzf.enable = true;
+  programs.zoxide.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    defaultKeymap = "emacs";
+    history.extended = true;
+    shellAliases = {
+      l = "ls -alh";
+      ll = "ls -l";
+    };
+    initExtra = ''
+      # Pure prompt
+      . ${pkgs.pure-prompt}/share/zsh/site-functions/async
+      . ${pkgs.pure-prompt}/share/zsh/site-functions/prompt_pure_setup
+      RPS1=""
+
+      # Fast syntax highlight
+      . ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
+
+      # History substring search
+      . ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+      bindkey '^[OA' history-substring-search-up
+      bindkey '^[OB' history-substring-search-down
+      bindkey -M emacs '^P' history-substring-search-up
+      bindkey -M emacs '^N' history-substring-search-down
+      bindkey -M vicmd 'k' history-substring-search-up
+      bindkey -M vicmd 'j' history-substring-search-down
+    '';
+  };
+}
