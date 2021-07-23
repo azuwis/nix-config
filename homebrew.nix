728121390a19fa83708c60e9ff81d796a24b20a6
Zhong Jianxin <azuwis@gmail.com>
2021-07-23 14:17:23 +0800

    homebrew: More settings

--- homebrew.nix
+++ homebrew.nix
@@ -4,11 +4,17 @@
   environment.systemPath =  [ "/opt/homebrew/bin" ];
   homebrew = {
     enable = true;
+    autoUpdate = true;
     cleanup = "zap";
+    global = {
+      brewfile = true;
+      noLock = true;
+    };
     brewPrefix = "/opt/homebrew/bin";
     taps = [
+      "homebrew/core"
       "homebrew/cask"
-      "homebrew/homebrew-services"
+      # "homebrew/homebrew-services"
     ];
     brews = [
     ];
