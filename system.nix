b21a35b46d5d3caaac452b9fd2f03556593f7483
Zhong Jianxin <azuwis@gmail.com>
2021-07-23 14:11:02 +0800

    system: Add setting for dock/finder

--- system.nix
+++ system.nix
@@ -1,18 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  system.defaults.NSGlobalDomain = {
-    InitialKeyRepeat = 20;
-    KeyRepeat = 2;
-  };
-  system.defaults.dock.autohide = true;
-  system.defaults.trackpad = {
-    Clicking = true;
-    Dragging = true;
-    TrackpadThreeFingerDrag = true;
+  system.defaults = {
+    NSGlobalDomain = {
+      InitialKeyRepeat = 20;
+      KeyRepeat = 2;
+    };
+    dock = {
+      autohide = true;
+      mru-spaces = false;
+      minimize-to-application = true;
+    };
+    finder = {
+      AppleShowAllExtensions = true;
+      _FXShowPosixPathInTitle = true;
+      FXEnableExtensionChangeWarning = false;
+    };
+    screencapture.location = "/tmp";
+    trackpad = {
+      Clicking = true;
+      Dragging = true;
+      TrackpadThreeFingerDrag = true;
+    };
   };
   system.keyboard = {
     enableKeyMapping = true;
     remapCapsLockToControl = true;
   };
+  time.timeZone = "Asia/Shanghai";
 }
