f0b8c6962d8a0b5ebde041a40fdd3a1366ee59d5
Zhong Jianxin <azuwis@gmail.com>
2021-08-13 21:40:45 +0800

    simple-bar: Use let instead of packageOverrides

--- simple-bar.nix
+++ simple-bar.nix
@@ -1,19 +1,20 @@
 { config, pkgs, ... }:
 
+let simple-bar = pkgs.callPackage ./pkgs/simple-bar {};
+
+in
+
 {
-  nixpkgs.config.packageOverrides = super: {
-    simple-bar = pkgs.callPackage ./pkgs/simple-bar {};
-  };
   home-manager.users."${config.my.user}" = {
-    home.packages = [ pkgs.simple-bar ];
+    home.packages = [ simple-bar ];
     home.activation.ubersicht = ''
       ubersicht_widgets=~/Library/Application\ Support/Übersicht/widgets
       mkdir -p "$ubersicht_widgets"
       rm -f "$ubersicht_widgets/GettingStarted.jsx"
       rm -f "$ubersicht_widgets/logo.png"
-      if [ ! -e "$ubersicht_widgets/simple-bar" ] || [ "$(readlink "$ubersicht_widgets/simple-bar")" != "${pkgs.simple-bar}" ]
+      if [ ! -e "$ubersicht_widgets/simple-bar" ] || [ "$(readlink "$ubersicht_widgets/simple-bar")" != "${simple-bar}" ]
       then
-        ln -sfn "${pkgs.simple-bar}" "$ubersicht_widgets/simple-bar"
+        ln -sfn "${simple-bar}" "$ubersicht_widgets/simple-bar"
       fi
     '';
   };
