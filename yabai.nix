3474ec9dccd9685390c92688fb2e0a961252f51c
Zhong Jianxin <azuwis@gmail.com>
2021-08-19 23:58:44 +0800

    yabai: Only bind lalt and lshift

--- yabai.nix
+++ yabai.nix
@@ -42,43 +42,43 @@
   services.skhd = {
     enable = true;
     skhdConfig = ''
-      alt - 1 : yabai -m space --focus 1
-      alt - 2 : yabai -m space --focus 2
-      alt - 3 : yabai -m space --focus 3
-      alt - 4 : yabai -m space --focus 4
-      alt - 5 : yabai -m space --focus 5
-      alt - 6 : yabai -m space --focus 6
-      alt - 7 : yabai -m space --focus 7
-      alt - 8 : yabai -m space --focus 8
-      alt - 9 : yabai -m space --focus 9
-      alt - 0 : yabai -m space --focus 10
-      shift + alt - 1 : yabai -m window --space 1
-      shift + alt - 2 : yabai -m window --space 2
-      shift + alt - 3 : yabai -m window --space 3
-      shift + alt - 4 : yabai -m window --space 4
-      shift + alt - 5 : yabai -m window --space 5
-      shift + alt - 6 : yabai -m window --space 6
-      shift + alt - 7 : yabai -m window --space 7
-      shift + alt - 8 : yabai -m window --space 8
-      shift + alt - 9 : yabai -m window --space 9
-      shift + alt - 0 : yabai -m window --space 10
-      alt - l : yabai -m window --focus east || yabai -m window --focus stack.next
-      alt - j : yabai -m window --focus south
-      alt - h : yabai -m window --focus west || yabai -m window --focus stack.prev
-      alt - k : yabai -m window --focus north
-      shift + alt - l : yabai -m window --swap east
-      shift + alt - j : yabai -m window --swap south
-      shift + alt - h : yabai -m window --swap west
-      shift + alt - k : yabai -m window --swap north
-      shift + alt - 0x29 : yabai -m window --toggle split
-      alt - c : yabai -m window --toggle float && yabai -m window --grid 6:6:1:1:4:4
-      alt - f : yabai -m window --toggle zoom-fullscreen
-      alt - m : yabai -m window --minimize
-      alt - tab : yabai -m space --focus recent
-      alt - w : yabai -m space --layout stack
-      alt - e : yabai -m space --layout bsp
-      alt - s : yabai -m space --layout float
-      alt - return : open -na ~/Applications/Alacritty.app
+      lalt - 1 : yabai -m space --focus 1
+      lalt - 2 : yabai -m space --focus 2
+      lalt - 3 : yabai -m space --focus 3
+      lalt - 4 : yabai -m space --focus 4
+      lalt - 5 : yabai -m space --focus 5
+      lalt - 6 : yabai -m space --focus 6
+      lalt - 7 : yabai -m space --focus 7
+      lalt - 8 : yabai -m space --focus 8
+      lalt - 9 : yabai -m space --focus 9
+      lalt - 0 : yabai -m space --focus 10
+      lshift + lalt - 1 : yabai -m window --space 1
+      lshift + lalt - 2 : yabai -m window --space 2
+      lshift + lalt - 3 : yabai -m window --space 3
+      lshift + lalt - 4 : yabai -m window --space 4
+      lshift + lalt - 5 : yabai -m window --space 5
+      lshift + lalt - 6 : yabai -m window --space 6
+      lshift + lalt - 7 : yabai -m window --space 7
+      lshift + lalt - 8 : yabai -m window --space 8
+      lshift + lalt - 9 : yabai -m window --space 9
+      lshift + lalt - 0 : yabai -m window --space 10
+      lalt - l : yabai -m window --focus east || yabai -m window --focus stack.next
+      lalt - j : yabai -m window --focus south
+      lalt - h : yabai -m window --focus west || yabai -m window --focus stack.prev
+      lalt - k : yabai -m window --focus north
+      lshift + lalt - l : yabai -m window --swap east
+      lshift + lalt - j : yabai -m window --swap south
+      lshift + lalt - h : yabai -m window --swap west
+      lshift + lalt - k : yabai -m window --swap north
+      lshift + lalt - 0x29 : yabai -m window --toggle split
+      lalt - c : yabai -m window --toggle float && yabai -m window --grid 6:6:1:1:4:4
+      lalt - f : yabai -m window --toggle zoom-fullscreen
+      lalt - m : yabai -m window --minimize
+      lalt - tab : yabai -m space --focus recent
+      lalt - w : yabai -m space --layout stack
+      lalt - e : yabai -m space --layout bsp
+      lalt - s : yabai -m space --layout float
+      lalt - return : open -na ~/Applications/Alacritty.app
     '';
   };
 
