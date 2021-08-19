b404e2e657735a3a133de744b61689254d9a0348
Zhong Jianxin <azuwis@gmail.com>
2021-08-19 23:57:33 +0800

    yabai: Add nibar signals

--- yabai.nix
+++ yabai.nix
@@ -5,6 +5,10 @@
     ${config.my.user} ALL = (root) NOPASSWD: ${pkgs.yabai}/bin/yabai --load-sa
   '';
 
+  environment.systemPackages = [
+    pkgs.websocat
+  ];
+
   services.yabai = {
     enable = true;
     package = pkgs.yabai;
@@ -19,6 +23,14 @@
       sudo yabai --load-sa
       yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
       yabai -m rule --add app="System Preferences" manage=off
+      for event in space_changed display_changed
+      do
+        yabai -m signal --add event=$event action='echo \{\"type\":\"WIDGET_WANTS_REFRESH\",\"payload\":\"nibar-spaces-jsx\"\} | websocat -E --origin http://127.0.0.1:41416 ws://127.0.0.1:41416' label=nibar_spaces_$event
+      done
+      for event in application_front_switched
+      do
+        yabai -m signal --add event=$event action='echo \{\"type\":\"WIDGET_WANTS_REFRESH\",\"payload\":\"nibar-windows-jsx\"\} | websocat -E --origin http://127.0.0.1:41416 ws://127.0.0.1:41416' label=nibar_windows_$event
+      done
     '';
   };
 
