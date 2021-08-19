43b260b49fd55923d81c0caf9e6a6b313ffd8d4e
Zhong Jianxin <azuwis@gmail.com>
2021-08-19 21:59:28 +0800

    yabai: Use stdenvNoCC

--- overlays.nix
+++ overlays.nix
@@ -1,18 +1,5 @@
 [( self: super: {
   rime-csp = super.callPackage ./pkgs/rime-csp {};
   simple-bar = super.callPackage ./pkgs/simple-bar {};
-  yabai = super.yabai.overrideAttrs (o: rec {
-    version = "3.3.10-1";
-    src = builtins.fetchTarball {
-      url = "https://github.com/azuwis/yabai/releases/download/v3.3.10/yabai-v${version}.tar.gz";
-      sha256 = "1za1b3hdns9hhhwpq3fvya2528rc17cyz9sl0jq4q6y1i35jkrm6";
-    };
-
-    installPhase = ''
-      mkdir -p $out/bin
-      mkdir -p $out/share/man/man1/
-      cp ./archive/bin/yabai $out/bin/yabai
-      cp ./archive/doc/yabai.1 $out/share/man/man1/yabai.1
-    '';
-  });
+  yabai = super.callPackage ./pkgs/yabai {};
 })]
---
+++ pkgs/yabai/default.nix
@@ -0,0 +1,28 @@
+{ lib, stdenvNoCC, fetchzip }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "yabai";
+  version = "3.3.10-1";
+
+  src = fetchzip {
+    url = "https://github.com/azuwis/yabai/releases/download/v3.3.10/yabai-v${version}.tar.gz";
+    sha256 = "0lsybxx8k503kn8v519kq47p1miqpciy4fyjm09afciq5bmqd96g";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/man/man1/
+    cp ./bin/yabai $out/bin/yabai
+    cp ./doc/yabai.1 $out/share/man/man1/yabai.1
+  '';
+
+  meta = with lib; {
+    description = ''
+      A tiling window manager for macOS based on binary space partitioning
+    '';
+    homepage = "https://github.com/koekeishiya/yabai";
+    platforms = platforms.darwin;
+    maintainers = with maintainers; [ cmacrae shardy ];
+    license = licenses.mit;
+  };
+}
