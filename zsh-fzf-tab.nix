f08a83b91ecc4d02b06553043fc56ae107e173e7
Zhong Jianxin <azuwis@gmail.com>
2021-08-22 12:13:27 +0800

    Refactor zsh-fzf-tab

--- zsh-fzf-tab.nix
+++ zsh-fzf-tab.nix
@@ -1,15 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  home-manager.users."${config.my.user}" = { config, lib, pkgs, ... }: {
-    home.packages = with pkgs; [
-      zsh-fzf-tab
-    ];
-    programs.zsh = {
-      initExtra = ''
-        # zsh-fzf-tab
-        . ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-      '';
-    };
+  home.packages = with pkgs; [
+    zsh-fzf-tab
+  ];
+  programs.zsh = {
+    initExtra = ''
+      # zsh-fzf-tab
+      . ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+    '';
   };
 }
