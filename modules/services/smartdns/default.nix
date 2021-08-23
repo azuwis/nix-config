5d660deee6ea3d06cd3f64b27890111acad5b04d
Zhong Jianxin <azuwis@gmail.com>
2021-08-24 01:33:22 +0800

    smartdns: Add service to set dns server
    when network changed

--- modules/services/smartdns/default.nix
+++ modules/services/smartdns/default.nix
@@ -9,6 +9,35 @@ let
     pwd
     sed -e 's,^,server=/,' -e 's,$,/${cfg.bind}#${toString cfg.localDomainPort},' ${domains} >$out
   '';
+  smartdnsUpdate = pkgs.writeScript "smartdns-update" ''
+    #!/bin/sh
+    smartdns="${cfg.bind}"
+    if ! grep -q '^nameserver ' /var/run/resolv.conf
+    then
+      networksetup -listallnetworkservices | grep -v '*' | while read -r service
+      do
+        if [ "$(networksetup -getdnsservers "$service")" = "$smartdns" ]
+        then
+          networksetup -setdnsservers "$service" Empty
+        fi
+      done
+    else
+      iface="$(netstat -rnf inet | awk '/^default/ {print $4}')"
+      test -z "$iface" && exit
+      dns="$(ipconfig getoption "$iface" domain_name_server)"
+      test -z "$dns" && exit
+      if ! grep -qFx "nameserver $dns" /var/run/smartdns-update
+      then
+        echo "nameserver $dns" > /var/run/smartdns-update
+      fi
+      service="$(networksetup -listnetworkserviceorder | sed -rn "s/.*: ([^,]+),.*$iface\).*/\1/p")"
+      test -z "$service" && exit
+      if [ "$(networksetup -getdnsservers "$service")" != "$smartdns" ]
+      then
+        networksetup -setdnsservers "$service" "$smartdns"
+      fi
+    fi
+  '';
 in
 
 {
@@ -58,10 +87,23 @@ in
         "--keep-in-foreground"
         "--listen-address=${cfg.bind}"
         "--port=${toString cfg.localDomainPort}"
+        "--resolv-file=/var/run/smartdns-update"
         "--strict-order"
       ];
       serviceConfig.KeepAlive = true;
       serviceConfig.RunAtLoad = true;
+    };
+
+    launchd.daemons.smartdns-update = {
+      serviceConfig.ProgramArguments = [
+        "${smartdnsUpdate}"
+      ];
+      serviceConfig.RunAtLoad = true;
+      serviceConfig.ThrottleInterval = 1;
+      serviceConfig.WatchPaths = [
+        "/var/run/resolv.conf"
+      ];
+      # serviceConfig.StandardErrorPath = "/tmp/smartdns.log";
     };
 
     launchd.daemons.smartdns = {
