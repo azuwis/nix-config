8b30aafed0ebdf8fecee4f4eb2af52daa886c62c
Zhong Jianxin <azuwis@gmail.com>
2021-08-25 16:17:17 +0800

    yabai: Remove nibar settings

--- yabai.nix
+++ yabai.nix
@@ -21,14 +21,6 @@
       sudo yabai --load-sa
       yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
       yabai -m rule --add app="System Preferences" manage=off
-      for event in space_changed display_changed
-      do
-        yabai -m signal --add event=$event action='echo \{\"type\":\"WIDGET_WANTS_REFRESH\",\"payload\":\"nibar-spaces-jsx\"\} | websocat -E --origin http://127.0.0.1:41416 ws://127.0.0.1:41416' label=nibar_spaces_$event
-      done
-      for event in application_front_switched
-      do
-        yabai -m signal --add event=$event action='echo \{\"type\":\"WIDGET_WANTS_REFRESH\",\"payload\":\"nibar-windows-jsx\"\} | websocat -E --origin http://127.0.0.1:41416 ws://127.0.0.1:41416' label=nibar_windows_$event
-      done
       yabai -m space 5 --layout float
     '';
   };
