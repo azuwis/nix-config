e726c48b3c510b018de8055597df761fa0db86ab
Zhong Jianxin <azuwis@gmail.com>
2021-08-25 17:24:22 +0800

    smartdns: Set dns only when iface down and up

--- services/smartdns/default.nix
+++ services/smartdns/default.nix
@@ -13,6 +13,7 @@ let
     smartdns="${cfg.bind}"
     if ! grep -q '^nameserver ' /var/run/resolv.conf
     then
+      rm -f /var/run/smartdns-update
       networksetup -listallnetworkservices | grep -v '*' | while read -r service
       do
         if [ "$(networksetup -getdnsservers "$service")" = "$smartdns" ]
@@ -23,17 +24,17 @@ let
     else
       iface="$(netstat -rnf inet | awk '/^default/ {print $4; exit}')"
       test -z "$iface" && exit
-      dns="$(ipconfig getoption "$iface" domain_name_server)"
-      test -z "$dns" && exit
-      if ! grep -qFx "nameserver $dns" /var/run/smartdns-update
-      then
-        echo "nameserver $dns" > /var/run/smartdns-update
-      fi
       service="$(networksetup -listnetworkserviceorder | sed -rn "s/.*: ([^,]+),.*$iface\).*/\1/p")"
       test -z "$service" && exit
-      if [ "$(networksetup -getdnsservers "$service")" != "$smartdns" ]
+      if [ ! -e /var/run/smartdns-update ]
       then
         networksetup -setdnsservers "$service" "$smartdns"
+      fi
+      dns="$(ipconfig getoption "$iface" domain_name_server)"
+      test -z "$dns" && exit
+      if ! grep -qFx "nameserver $dns" /var/run/smartdns-update 2>/dev/null
+      then
+        echo "nameserver $dns" > /var/run/smartdns-update
       fi
     fi
   '';
