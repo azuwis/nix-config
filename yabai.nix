f7ae580bc43c9264677db90f7d522d574d156608
Zhong Jianxin <azuwis@gmail.com>
2021-08-26 22:26:29 +0800

    yabai: Comment about command to disable SIP

--- yabai.nix
+++ yabai.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
+  # csrutil enable --without fs --without debug --without nvram
+  # nvram boot-args=-arm64e_preview_abi
   environment.etc."sudoers.d/yabai".text = ''
     ${config.my.user} ALL = (root) NOPASSWD: ${pkgs.yabai}/bin/yabai --load-sa
   '';
