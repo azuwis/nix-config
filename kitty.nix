9f1ad0c76d5aa8932e1244a9a0fe08e6a9d0a540
Zhong Jianxin <azuwis@gmail.com>
2021-08-29 19:39:55 +0800

    kitty: Use nord colorscheme

--- kitty.nix
+++ kitty.nix
@@ -11,6 +11,30 @@ if builtins.hasAttr "hm" lib then
     cursor_blink_interval 0
     copy_on_select yes
     hide_window_decorations yes
+
+    # nord colorscheme
+    foreground           #D8DEE9
+    background           #2E3440
+    selection_foreground #000000
+    selection_background #FFFACD
+    url_color            #0087BD
+    cursor               #81A1C1
+    color0  #3B4252
+    color8  #4C566A
+    color1  #BF616A
+    color9  #BF616A
+    color2  #A3BE8C
+    color10 #A3BE8C
+    color3  #EBCB8B
+    color11 #EBCB8B
+    color4  #81A1C1
+    color12 #81A1C1
+    color5  #B48EAD
+    color13 #B48EAD
+    color6  #88C0D0
+    color14 #8FBCBB
+    color7  #E5E9F0
+    color15 #ECEFF4
   '';
 }
 
