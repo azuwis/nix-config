f230f6e071c3415a2dbe8c9e6df7fe0d0a26c6cd
Zhong Jianxin <azuwis@gmail.com>
2021-08-29 19:37:25 +0800

    spacebar: Use nord colorscheme

--- spacebar/init.sh
+++ spacebar/init.sh
@@ -7,14 +7,14 @@ spacebar -m config spacing_left 16
 spacebar -m config spacing_right 14
 spacebar -m config text_font "JetBrains Mono:Regular:13.0"
 spacebar -m config icon_font "JetBrains Mono:Regular:13.0"
-spacebar -m config background_color 0xff1e1f21
-spacebar -m config foreground_color 0x99ffffff
-spacebar -m config space_icon_color 0xddf5bf4f
+spacebar -m config background_color 0xff2e3440
+spacebar -m config foreground_color 0xbbd8dee9
+spacebar -m config space_icon_color 0xffebcb8b
 spacebar -m config space_icon_color_secondary 0xff78c4d4
 spacebar -m config space_icon_color_tertiary 0xfffff9b0
-spacebar -m config power_icon_color 0x99ffffff
-spacebar -m config battery_icon_color 0x99ffffff
-spacebar -m config clock_icon_color 0x99ffffff
+spacebar -m config power_icon_color 0xbbd8dee9
+spacebar -m config battery_icon_color 0xbbd8dee9
+spacebar -m config clock_icon_color 0xbbd8dee9
 spacebar -m config power_icon_strip 􀛨 􀢋
 spacebar -m config space_icon_strip 1° 2° 3° 4° 5° 6° 7° 8° 9° 10°
 spacebar -m config clock_icon " "
