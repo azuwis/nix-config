b989b25a7752158e55d748605d4cbd50bf07e076
Zhong Jianxin <azuwis@gmail.com>
2021-09-04 11:23:11 +0800

    sketchybar: Basic working settings

---
+++ sketchybar/default.nix
@@ -0,0 +1,86 @@
+{ config, lib, pkgs, ... }:
+
+let scripts = ./scripts;
+
+in
+
+{
+  environment.systemPackages = [ pkgs.jq ];
+  launchd.user.agents.sketchybar.serviceConfig = {
+    StandardErrorPath = "/tmp/sketchybar.log";
+    StandardOutPath = "/tmp/sketchybar.log";
+  };
+  services.sketchybar.enable = true;
+  services.sketchybar.package = pkgs.sketchybar;
+  services.sketchybar.extraConfig = ''
+    #!/bin/sh
+    sketchybar -m freeze on
+
+    # bar
+    sketchybar -m config height 24
+    sketchybar -m config position top
+    sketchybar -m config padding_left 10
+    sketchybar -m config padding_right 10
+    sketchybar -m config bar_color 0xff2e3440
+
+    # scripts cache
+    sketchybar -m default cache_scripts on
+
+    # spaces
+    sketchybar -m default icon_font "JetBrains Mono:Regular:13.0"
+    sketchybar -m default icon_color 0xbbd8dee9
+    sketchybar -m default label_font "JetBrains Mono:Regular:13.0"
+    sketchybar -m default label_color 0xbbd8dee9
+    sketchybar -m default icon_padding_right 16
+    sketchybar -m default icon_highlight_color 0xffebcb8b
+
+    for i in {1..8}
+    do
+      sketchybar -m add component space space$i left
+      sketchybar -m set space$i associated_display 1
+      sketchybar -m set space$i associated_space $i
+      sketchybar -m set space$i icon $i
+    done
+
+    # status default
+    sketchybar -m default icon_padding_left 10
+    sketchybar -m default icon_padding_right 6
+
+    # clock
+    sketchybar -m add item clock right
+    sketchybar -m set clock update_freq 10
+    sketchybar -m set clock script "${scripts}/status.sh"
+    sketchybar -m set clock icon_padding_left 2
+
+    # power
+    sketchybar -m add item power right
+
+    # wifi
+    sketchybar -m add item wifi right
+
+    # load
+    sketchybar -m add item load right
+    sketchybar -m set load icon 􀍽
+
+    # network
+    sketchybar -m add item network right
+    # sketchybar -m set network icon 􀄬
+
+    # end
+    sketchybar -m freeze off
+    sketchybar -m update
+
+    # ram disk
+    cache="$HOME/.cache/sketchybar"
+    mkdir -p "$cache"
+    if ! mount | grep -qF "$cache"
+    then
+      disk=$(hdiutil attach -nobrowse -nomount ram://1024)
+      disk="''${disk%% *}"
+      newfs_hfs -v sketchybar "$disk"
+      mount -t hfs -o nobrowse "$disk" "$cache"
+    fi
+  '';
+  services.yabai.config.external_bar = "main:24:0";
+  system.defaults.NSGlobalDomain._HIHideMenuBar = true;
+}
---
+++ sketchybar/scripts/status.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+STATUS="$HOME/.cache/sketchybar/status"
+
+readable() {
+  local bytes=$1
+  local kib=$(( bytes >> 10 ))
+  if [ "$kib" -lt 0 ]; then
+    printf "?K"
+  elif [ "$kib" -gt 1024 ]; then
+    local mib_int=$(( kib >> 10 ))
+    local mib_dec=$(( kib % 1024 * 976 / 10000 ))
+    if [ "$mib_dec" -lt 10 ]; then
+      mib_dec="0${mib_dec}"
+    fi
+    printf "%s.%sM" "$mib_int" "$mib_dec"
+  else
+    printf "%sK" "$kib"
+  fi
+}
+
+get_battery() {
+  local battery status
+  battery="$(pmset -g batt)"
+  BATTERY_LABEL="${battery%\%*}"
+  BATTERY_LABEL="${BATTERY_LABEL##*	}"
+  read -r _ _ _ status _ <<< "$battery"
+  BATTERY_ICON="􀛨"
+  if [ "$status" = "'AC" ]
+  then
+    BATTERY_ICON="􀢋"
+  fi
+}
+
+get_wifi() {
+  local wifi test
+  wifi=$(networksetup -getairportnetwork en0)
+  read -r _ _ test WIFI_LABEL <<< "$wifi"
+  if [ "$test" = "Network:" ]
+  then
+      WIFI_ICON="􀙇"
+  else
+      WIFI_LABEL=""
+      WIFI_ICON="􀙈"
+  fi
+}
+
+get_load() {
+  local load
+  load=$(sysctl -n vm.loadavg)
+  read -r _ _ load _ <<< "$load"
+  LOAD="$load"
+}
+
+get_network() {
+  local network ibytes obytes last_ibytes last_obytes
+  network=$(netstat -ibn -I en0)
+  network="${network##*en0}"
+  read -r _ _ _ _ _ ibytes _ _ obytes _ <<< "${network}"
+  ISPEED="-1"
+  OSPEED="-1"
+  if [ -e "$STATUS" ]
+  then
+    read -r last_ibytes last_obytes < "$STATUS"
+    ISPEED=$(( (ibytes - last_ibytes) / 10 ))
+    OSPEED=$(( (obytes - last_obytes) / 10 ))
+  fi
+  echo "$ibytes $obytes" > "$STATUS"
+}
+
+get_battery
+get_wifi
+get_load
+get_network
+
+sketchybar -m freeze on
+sketchybar -m set clock label "$(date +'%a %m-%d %H:%M')"
+sketchybar -m set power icon "$BATTERY_ICON"
+sketchybar -m set power label "${BATTERY_LABEL}%"
+sketchybar -m set wifi icon "$WIFI_ICON"
+sketchybar -m set wifi label "$WIFI_LABEL"
+if [ -z "$WIFI_LABEL" ]
+then
+  sketchybar -m set wifi icon_padding_right 0
+else
+  sketchybar -m set wifi icon_padding_right 6
+fi
+sketchybar -m set load label "$LOAD"
+sketchybar -m set network label "$(readable "$ISPEED")↓ $(readable "$OSPEED")↑"
+sketchybar -m freeze off
