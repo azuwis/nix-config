d1341856441e7bd7557f40919ea7391e3f31652d
Zhong Jianxin <azuwis@gmail.com>
2021-09-05 22:45:29 +0800

    mpv: More settings

--- mpv.nix
+++ mpv.nix
@@ -3,9 +3,25 @@
 {
   programs.mpv = {
     enable = true;
+    bindings = {
+      Y = "add sub-scale +0.1";                # increase subtitle font size
+      G = "add sub-scale -0.1";                # decrease subtitle font size
+      y = "sub_step -1";                       # immediately display next subtitle
+      g = "sub_step +1";                       # previous
+      R = "cycle_values window-scale 2 0.5 1"; # switch between 2x, 1/2, unresized window size
+    };
     config = {
-      profile = "gpu-hq";
+      audio-display = false;
       force-window = true;
+      hidpi-window-scale = false;
+      hwdec = "auto";
+      keep-open = true;
+      keep-open-pause = false;
+      osd-on-seek = false;
+      profile = "gpu-hq";
+      script-opts = "osc-seekbarstyle=knob,osc-deadzonesize=1,osc-minmousemove=1";
+      sub-auto = "fuzzy";
+      sub-codepage = "gbk";
     };
   };
 }
