12ac0e5b63570af263948b1bf2eb41efb9546739
Zhong Jianxin <azuwis@gmail.com>
2021-09-08 00:49:22 +0800

    mpv: Set fonts.conf

--- mpv.nix
+++ mpv.nix
@@ -36,4 +36,34 @@ in
       sub-codepage = "gbk";
     };
   };
+  xdg.configFile."mpv/fonts.conf".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE fontconfig SYSTEM "../fonts.dtd">
+    <fontconfig>
+      <dir>/System/Library/Fonts</dir>
+      <!-- <dir>/Library/Fonts</dir> -->
+      <!-- <dir>~/Library/Fonts</dir> -->
+
+      <!-- Default fonts for sans, serif, monospace -->
+      <alias>
+        <family>sans-serif</family>
+          <prefer>
+            <family>PingFang SC</family>
+          </prefer>
+      </alias>
+      <alias>
+        <family>serif</family>
+          <prefer>
+            <family>Songti SC</family>
+          </prefer>
+      </alias>
+      <alias>
+        <family>monospace</family>
+          <prefer>
+            <family>Menlo</family>
+            <family>PingFang SC</family>
+          </prefer>
+      </alias>
+    </fontconfig>
+  '';
 }
