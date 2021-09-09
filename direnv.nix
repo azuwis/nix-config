0cf99942186899dd9419c0ed3f903547f6ec2a41
Zhong Jianxin <azuwis@gmail.com>
2021-09-09 16:26:16 +0800

    direnv: Comment stdlib

--- direnv.nix
+++ direnv.nix
@@ -2,16 +2,16 @@
 
 {
   programs.direnv.enable = true;
-  programs.direnv.stdlib = ''
-    : ''${XDG_CACHE_HOME:=$HOME/.cache}
-    declare -A direnv_layout_dirs
-    direnv_layout_dir() {
-      echo "''${direnv_layout_dirs[$PWD]:=$(
-        echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-        echo -n "$PWD" | shasum | cut -d ' ' -f 1
-      )}"
-    }
-  '';
+  # programs.direnv.stdlib = ''
+  #   : ''${XDG_CACHE_HOME:=$HOME/.cache}
+  #   declare -A direnv_layout_dirs
+  #   direnv_layout_dir() {
+  #     echo "''${direnv_layout_dirs[$PWD]:=$(
+  #       echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+  #       echo -n "$PWD" | shasum | cut -d ' ' -f 1
+  #     )}"
+  #   }
+  # '';
   programs.direnv.nix-direnv.enable = true;
   programs.zsh.initExtra = ''
     # nix-direnv
