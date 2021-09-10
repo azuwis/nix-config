8462658c9b1f2759b886a20d6e4aa13fd3c38091
Zhong Jianxin <azuwis@gmail.com>
2021-09-10 14:39:28 +0800

    kitty: Move symbol_map to extraConfig
    to allow multiple symbol_map

--- kitty.nix
+++ kitty.nix
@@ -18,7 +18,6 @@ if builtins.hasAttr "hm" lib then
     cursor_blink_interval = 0;
     hide_window_decorations = true;
     scrollback_pager_history_size = 1;
-    symbol_map = "U+E5FA-U+E62B,U+E700-U+E7C5,U+F000-U+F2E0,U+E200-U+E2A9,U+F500-U+FD46,U+E300-U+E3EB,U+F400-U+F4A8,U+2665,U+26A1,U+F27C,U+E0A3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D2,U+E0D4,U+23FB-U+23FE,U+2B58,U+F300-U+F313,U+E000-U+E00D JetBrainsMono Nerd Font";
     foreground = "#D8DEE9";
     background = "#2E3440";
     selection_foreground = "#000000";
@@ -42,6 +41,10 @@ if builtins.hasAttr "hm" lib then
     color7 = "#E5E9F0";
     color15 = "#ECEFF4";
   };
+  programs.kitty.extraConfig = ''
+    symbol_map U+E5FA-U+E62B,U+E700-U+E7C5,U+F000-U+F2E0,U+E200-U+E2A9,U+F500-U+FD46,U+E300-U+E3EB,U+F400-U+F4A8,U+2665,U+26A1,U+F27C,U+E0A3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D2,U+E0D4,U+23FB-U+23FE,U+2B58,U+F300-U+F313,U+E000-U+E00D JetBrainsMono Nerd Font
+    # symbol_map U+100000-U+10FFFF SF Pro
+  '';
 }
 
 else
