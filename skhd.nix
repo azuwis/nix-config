1e45e6cfd2d42d8f6c407436168e8bb697a211a0
Zhong Jianxin <azuwis@gmail.com>
2021-09-17 10:39:49 +0800

    skhd: Set sketchbar icon when fullscreen

--- skhd.nix
+++ skhd.nix
@@ -48,7 +48,10 @@ else
       lshift + lalt - p : alacritty -o window.dimensions.columns=50 -o window.dimensions.lines=6 -o window.position.x=1000 -o window.position.y=48 --title=Passmenu --command passmenu
       lshift + lalt - 0x29 : yabai -m window --toggle split
       lalt - c : yabai -m window --toggle float && yabai -m window --grid 6:6:1:1:4:4
-      lalt - f : yabai -m window --toggle zoom-fullscreen; yabai -m window --grid 1:1:0:0:1:1
+      lalt - f : yabai -m window --toggle zoom-fullscreen; yabai -m window --grid 1:1:0:0:1:1; \
+                 read -r index fullscreen <<< "$(yabai -m query --windows --window mouse | jq -r '[.space, ."zoom-fullscreen"] | @sh')"; \
+                 if [ "$fullscreen" = 1 ]; then; icon="↑"; padding=14; else; icon="$index"; padding=16; fi; \
+                 sketchybar -m batch --set "space$index" icon="$icon" label_padding_right="$padding"
       lalt - m : yabai -m window --minimize
       lalt - t : yabai -m window --toggle float
       lalt - tab : yabai -m space --focus recent
