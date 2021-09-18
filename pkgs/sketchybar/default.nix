30c4368ce9f1eb166574165679925d1f26b00346
Zhong Jianxin <azuwis@gmail.com>
2021-09-18 11:24:35 +0800

    sketchybar: Update to v1.2.4

--- pkgs/sketchybar/default.nix
+++ pkgs/sketchybar/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "sketchybar";
-  version = "1.2.2";
+  version = "1.2.4";
 
   src = fetchFromGitHub {
     owner = "FelixKratz";
     repo = "SketchyBar";
     rev = "v${version}";
-    sha256 = "0q97w1pdcc81rfci2sl3x13r5l3vgc7isi15p0bpgzplmjvmlhdj";
+    sha256 = "08ayjxx9s352s5wffg0inzrmjx900ayyvpbfk7s887b8mvspb5ql";
   };
 
   buildInputs = with pkgs.darwin.apple_sdk.frameworks; [
