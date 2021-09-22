d687aec193d668275e88dd674c7b52500705718c
Zhong Jianxin <azuwis@gmail.com>
2021-09-22 11:35:44 +0800

    smartnet: Add script to toggle

--- smartnet.nix
+++ smartnet.nix
@@ -1,6 +1,33 @@
 { config, lib, pkgs, ... }:
 
+let
+  smartnet = pkgs.writeScriptBin "smartnet" ''
+    smartdns_resolv_script=${config.services.smartdns.resolv.script}
+    on() {
+      sudo "$smartdns_resolv_script" enable
+      sudo pfctl -e -f /etc/pf.conf 2>&1 | tail -n1
+    }
+    off() {
+      sudo "$smartdns_resolv_script" disable
+      sudo pfctl -d 2>&1 | tail -n1
+    }
+    action="$1"
+    if [ -n "$action" ]
+    then
+      "$action"
+    else
+      if sudo launchctl list "${config.launchd.daemons.smartdns-resolv.serviceConfig.Label}" 1>/dev/null 2>/dev/null
+      then
+        off
+      else
+        on
+      fi
+    fi
+  '';
+in
+
 {
+  environment.systemPackages = [ smartnet ];
   services.redsocks2.enable = true;
   services.redsocks2.extraConfig = ''
     tcpdns {
