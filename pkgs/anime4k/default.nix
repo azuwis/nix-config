f7eb308ffc52a0d231eb69369c34b3fe8292a4a5
Zhong Jianxin <azuwis@gmail.com>
2021-09-24 23:41:23 +0800

    anime4k: Update to v4.0.1

--- pkgs/anime4k/default.nix
+++ pkgs/anime4k/default.nix
@@ -1,12 +1,12 @@
 { stdenvNoCC, fetchzip }:
 
-stdenvNoCC.mkDerivation {
+stdenvNoCC.mkDerivation rec {
   pname = "anime4k";
-  version = "v4.0.0-RC";
+  version = "v4.0.1";
   src = fetchzip {
-    url = "https://github.com/bloc97/Anime4K/releases/download/v4.0.0-RC/Anime4K_v4.0.zip";
+    url = "https://github.com/bloc97/Anime4K/releases/download/v4.0.1/Anime4K_v4.0.zip";
     stripRoot = false;
-    sha256 = "0xfhmm45hbnab5kn763g3fxg8pc9kp45fg43nfgy10iplhva0lgv";
+    sha256 = "18x5q7zvkf5l0b2phh70ky6m99fx1pi6mhza4041b5hml7w987pl";
   };
   installPhase = ''
     mkdir $out
