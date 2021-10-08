4fd76789e80464b46209af490ae5dbf461e81bab
Zhong Jianxin <azuwis@gmail.com>
2021-10-08 15:26:33 +0800

    sketchybar: Update to v1.4.1

--- pkgs/sketchybar/default.nix
+++ pkgs/sketchybar/default.nix
@@ -1,23 +1,31 @@
 { pkgs, lib, stdenv, fetchFromGitHub }:
 
+let
+  arch = if stdenv.isAarch64 then "arm" else "x86";
+in
+
 stdenv.mkDerivation rec {
   pname = "sketchybar";
-  version = "1.3.1";
+  version = "1.4.1";
 
   src = fetchFromGitHub {
     owner = "FelixKratz";
     repo = "SketchyBar";
     rev = "v${version}";
-    sha256 = "0qp07ps31sq965lyzl5dlzggi8vzmpbn97wkc24hjby17xpdd0b8";
+    sha256 = "1rkngrkd0cincakmklj37iww9989vqb8qjbv09a4wzgdchww4h8j";
   };
 
   buildInputs = with pkgs.darwin.apple_sdk.frameworks; [
     Carbon Cocoa SkyLight
   ];
 
+  buildPhase = ''
+    make ${arch}
+  '';
+
   installPhase = ''
     mkdir -p $out/bin
-    cp ./bin/sketchybar $out/bin/sketchybar
+    cp ./bin/sketchybar_${arch} $out/bin/sketchybar
   '';
 
   meta = with lib; {
