1af4d638d62ec1421b5a171a6123e3863075ff50
Zhong Jianxin <azuwis@gmail.com>
2021-10-22 16:34:55 +0800

    Move module lists to flake.nix

--- flake.nix
+++ flake.nix
@@ -21,12 +21,42 @@
             from = { id = "local"; type = "indirect"; };
           };
         }
-        ./darwin.nix
+        ./common/my.nix
+        ./darwin/emacs # emacs-all-the-icons-fonts
+        ./darwin/homebrew.nix
+        ./darwin/hostname.nix
+        ./darwin/kitty.nix # sudo keep TERMINFO_DIRS env
+        ./darwin/sketchybar
+        ./darwin/skhd.nix
+        ./darwin/smartnet.nix
+        ./darwin/squirrel.nix
+        ./darwin/sudo.nix
+        ./darwin/system.nix
+        ./darwin/yabai.nix
+        ./services/redsocks2
+        ./services/shadowsocks
+        ./services/sketchybar
+        ./services/smartdns
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.azuwis = import ./home.nix;
+          home-manager.users.azuwis = { imports = [
+            ./common/alacritty.nix
+            ./common/direnv.nix
+            ./common/git.nix
+            ./common/mpv.nix
+            ./common/my.nix
+            ./common/neovim.nix
+            ./common/packages.nix
+            ./common/zsh.nix
+            ./darwin/emacs
+            ./darwin/home-manager-apps.nix
+            ./darwin/kitty.nix
+            ./darwin/packages.nix
+            ./darwin/skhd.nix
+            ./darwin/squirrel.nix
+          ]; };
         }
       ];
     };
--- home.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  imports = import ./modules.nix { inherit lib; };
-}
