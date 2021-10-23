97c2c17b38ca146fcba139466a3be457f1691de9
Zhong Jianxin <azuwis@gmail.com>
2021-10-24 00:30:19 +0800

    droid: More system config

--- droid/system.nix
+++ droid/system.nix
@@ -2,6 +2,13 @@
 
 {
   environment.etcBackupExtension = ".bak";
+  nix.extraConfig = ''
+    keep-env-derivations = true
+    keep-outputs = true
+    tarball-ttl = 43200
+  '';
+  nixpkgs.overlays = import ../overlays.nix;
   system.stateVersion = "21.05";
+  time.timeZone = "Asia/Shanghai";
   user.shell = "${pkgs.zsh}/bin/zsh";
 }
