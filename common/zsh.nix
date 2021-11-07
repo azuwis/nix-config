3855c4e2b79436ed3c76e8eb06568855a018f235
Zhong Jianxin <azuwis@gmail.com>
2021-11-07 23:08:58 +0800

    scripts: Add bs, remove ds() from zsh

--- common/zsh.nix
+++ common/zsh.nix
@@ -61,13 +61,6 @@
 
       # zsh-nix-shell
       . ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
-
-      # darwin
-      ds() {
-        pushd ~/.config/nixpkgs
-        darwin-rebuild switch --flake .
-        popd
-      }
     '';
   };
 }
