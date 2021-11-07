1a9f254f81264633aef33aa21708bd435e84d4cd
Zhong Jianxin <azuwis@gmail.com>
2021-11-07 22:45:19 +0800

    appmenu/passmenu -> appfzf/passfzf

--- darwin/skhd.nix
+++ darwin/skhd.nix
@@ -40,12 +40,12 @@ else
       lalt - j : yabai -m window --focus south
       lalt - h : yabai -m window --focus west || yabai -m window --focus stack.prev || yabai -m window --focus stack.last
       lalt - k : yabai -m window --focus north
-      lalt - space : alacritty -o window.dimensions.columns=50 -o window.dimensions.lines=6 -o window.position.x=1000 -o window.position.y=48 --title=Passmenu --command appmenu
+      lalt - space : alacritty -o window.dimensions.columns=50 -o window.dimensions.lines=6 -o window.position.x=1000 -o window.position.y=48 --title=Fzf --command appfzf
       lshift + lalt - l : yabai -m window --swap east
       lshift + lalt - j : yabai -m window --swap south
       lshift + lalt - h : yabai -m window --swap west
       lshift + lalt - k : yabai -m window --swap north
-      lshift + lalt - p : alacritty -o window.dimensions.columns=50 -o window.dimensions.lines=6 -o window.position.x=1000 -o window.position.y=48 --title=Passmenu --command passmenu
+      lshift + lalt - p : alacritty -o window.dimensions.columns=50 -o window.dimensions.lines=6 -o window.position.x=1000 -o window.position.y=48 --title=Fzf --command passfzf
       lshift + lalt - 0x29 : yabai -m window --toggle split
       lalt - c : yabai -m window --toggle float && yabai -m window --grid 6:6:1:1:4:4
       lalt - f : yabai -m window --toggle zoom-fullscreen; yabai -m window --grid 1:1:0:0:1:1; \
