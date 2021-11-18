aa63cd5e5685c38d7ffbbaeece608591200b884b
Zhong Jianxin <azuwis@gmail.com>
2021-11-18 09:07:23 +0800

    common/system: Set nixpkgs.config.allowUnfree

--- common/system.nix
+++ common/system.nix
@@ -7,6 +7,7 @@
     experimental-features = flakes nix-command
     tarball-ttl = 43200
   '';
+  nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = import ../overlays.nix;
   programs.zsh.enable = true;
   time.timeZone = "Asia/Shanghai";
