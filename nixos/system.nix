7dbcf5f6585a03c712c4e1b2410329fd0cc48c52
Zhong Jianxin <azuwis@gmail.com>
2021-11-18 23:56:01 +0800

    nixos/system: Enable openssh

--- nixos/system.nix
+++ nixos/system.nix
@@ -5,6 +5,11 @@
   boot.loader.efi.canTouchEfiVariables = false;
   networking.useDHCP = false;
   security.sudo.wheelNeedsPassword = false;
+  services.openssh = {
+    enable = true;
+    challengeResponseAuthentication = false;
+    passwordAuthentication = false;
+  };
   system.stateVersion = "21.11";
   users.groups.azuwis = {};
   users.users."${config.my.user}" = {
