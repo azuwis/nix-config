0264b56cc906389f8515c6ae72453aaddb7fc1c3
Zhong Jianxin <azuwis@gmail.com>
2021-11-28 23:24:48 +0800

    common/system: Set nix auto-optimise-store

--- common/system.nix
+++ common/system.nix
@@ -3,8 +3,9 @@
 {
   nix.allowedUsers = [ "${config.my.user}" ];
   nix.extraOptions = ''
-    keep-outputs = true
+    auto-optimise-store = true
     experimental-features = flakes nix-command
+    keep-outputs = true
     tarball-ttl = 43200
   '';
   nixpkgs.config.allowUnfree = true;
