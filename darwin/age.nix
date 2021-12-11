1bce50d32b313aebd274434ae61200c372e04252
Zhong Jianxin <azuwis@gmail.com>
2021-12-11 22:52:46 +0800

    darwin/age: Add wg0

--- darwin/age.nix
+++ darwin/age.nix
@@ -3,4 +3,8 @@
 {
   # https://github.com/LnL7/nix-darwin/pull/381
   age.sshKeyPaths = [ "/etc/age/keys.txt" ];
+  age.secrets.wg0 = {
+    file = "/etc/age/wg0.age";
+    path = "/etc/wireguard/wg0.conf";
+  };
 }
