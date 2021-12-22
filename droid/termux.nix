13d757709153c2416e7fc7470ebf14af0ff3baf3
Zhong Jianxin <azuwis@gmail.com>
2021-12-22 10:28:34 +0800

    droid/system: Setup font

--- droid/termux.nix
+++ droid/termux.nix
@@ -11,5 +11,8 @@ in
       $VERBOSE_ECHO "Config Termux..."
       $DRY_RUN_CMD mkdir -p "${config.user.home}/.termux"
       $DRY_RUN_CMD echo "${termuxConfig}" > "${config.user.home}/.termux/termux.properties"
+      if [ ! -e "${config.user.home}/.termux/font.ttf" ]; then
+        $DRY_RUN_CMD "${pkgs.curl}/bin/curl" -fsSL -o "${config.user.home}/.termux/font.ttf" 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.ttf'
+      fi
   '';
 }
