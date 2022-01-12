603e0a583f0d557fc02b25b360bb95ba1cecb2be
Zhong Jianxin <azuwis@gmail.com>
2022-01-12 11:28:45 +0800

    neovim: Add nvchad

--- common/neovim.nix
+++ common/neovim.nix
@@ -1,5 +1,25 @@
 { config, lib, pkgs, ... }:
 
+let
+  nvchad = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "nvchad";
+    version = "2022-01-11";
+    src = pkgs.fetchFromGitHub {
+      owner = "NvChad";
+      repo = "NvChad";
+      rev = "b1338beb0f775deb55c91a88a09e6738e73e5f97";
+      sha256 = "sha256-tLCIhV2uwJrwh4feIahfk6xuDhZPGQWhQD1LjufwcBM=";
+    };
+    postPatch = ''
+      ${pkgs.gnused}/bin/sed -i \
+        -e 's/show_close_icon = true,/show_close_icon = false,/' \
+        -e 's/show_buffer_close_icons = true,/show_buffer_close_icons = false,/' \
+        lua/plugins/configs/bufferline.lua
+    '';
+    meta.homepage = "https://github.com/NvChad/NvChad/";
+  };
+in
+
 {
   home.sessionVariables.EDITOR = "nvim";
   programs.neovim = {
@@ -13,12 +33,13 @@
       yaml-language-server
     ] ++ lib.optionals (builtins.hasAttr "stylua" pkgs) [ stylua ];
     extraConfig = ''
-      runtime nvchad-init.lua
+      source ${nvchad}/init.lua
       set commentstring=#\ %s
       autocmd! FileType gitcommit exec 'norm gg' | startinsert!
     '';
     plugins = with pkgs.vimPlugins; [
       csv-vim
+      nvchad
       vim-jsonnet
       vim-nix
     ];
