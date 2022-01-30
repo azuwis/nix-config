ae27b90350ef6b4d9b6ec9ad860af98dbb629771
Zhong Jianxin <azuwis@gmail.com>
2022-01-30 22:46:56 +0800

    redsocks: Use DarwinTools to provide sw_vers

--- pkgs/redsocks2/default.nix
+++ pkgs/redsocks2/default.nix
@@ -1,15 +1,15 @@
-{ stdenv, fetchFromGitHub, cacert, curl, libevent, openssl }:
+{ stdenv, fetchFromGitHub, cacert, curl, libevent, openssl, darwin }:
 
 stdenv.mkDerivation {
   pname = "redsocks2";
-  version = "2021-08-22";
+  version = "unstable-2021-08-22";
   src = fetchFromGitHub {
     owner = "azuwis";
     repo = "redsocks";
-    rev = "a73d1ed28ade6173746bca27c4c9c770c026b103";
-    sha256 = "0g422q13gaf2vhfwjv77h94m72c08lxp14q3rznihbhhnw8sbv76";
+    rev = "5a3b5a6bd3b99a6e0bafa73c57faa4db2f3b61ee";
+    sha256 = "11b71a3n0iljb5nbpdgbxlsa4g94vlgdrrnwgldc5rfc8wcc2ggw";
   };
-  buildInputs = [ cacert curl libevent openssl ];
+  buildInputs = [ cacert curl libevent openssl darwin.DarwinTools ];
   buileFlags = [ "DISABLE_SHADOWSOCKS=true" ];
   installPhase = ''
     install -d 0644 $out/bin
