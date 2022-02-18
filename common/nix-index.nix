f5f78a0545253e8a62af0574a0b898b1f1ab5530
Zhong Jianxin <azuwis@gmail.com>
2022-02-18 15:34:53 +0800

    common/nix-index: Fix sha256 for linux

--- common/nix-index.nix
+++ common/nix-index.nix
@@ -2,9 +2,13 @@
 
 let
   arch = if pkgs.stdenv.isDarwin then "darwin" else "linux";
+  sha256 = {
+    darwin = "sha256-lye/KGknE5KM62mFWt+LU4EM0i9BRGoj+JFe4RTbtQ8=";
+    linux = "sha256-6cnyhz/RPWlcwcAgRT7g2KcyvZto0Lz8sFbtL7P25Gc=";
+  }.${arch};
   indexCache = pkgs.fetchurl {
     url = "https://github.com/Mic92/nix-index-database/releases/download/2022-01-09/index-x86_64-${arch}";
-    sha256 = "sha256-lye/KGknE5KM62mFWt+LU4EM0i9BRGoj+JFe4RTbtQ8=";
+    inherit sha256;
   };
 in
 
