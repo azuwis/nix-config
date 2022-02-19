5b9a9a6ad1083fa206364652bcf0d2cb058e15b3
Zhong Jianxin <azuwis@gmail.com>
2022-02-19 23:31:17 +0800

    neovim: Update orgmode

--- common/neovim/nvchad/plugins.lua
+++ common/neovim/nvchad/plugins.lua
@@ -24,18 +24,13 @@ return {
   },
 
   {
-    "kristijanhusak/orgmode.nvim",
+    "nvim-orgmode/orgmode",
     after = "nvim-treesitter",
     config = function()
-      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-      parser_config.org = {
-        install_info = {
-          url = 'https://github.com/milisims/tree-sitter-org',
-          revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
-          files = {'src/parser.c', 'src/scanner.cc'},
-        },
-        filetype = 'org',
-      }
+      -- Load custom tree-sitter grammar for org filetype
+      require('orgmode').setup_ts_grammar()
+
+      -- Tree-sitter configuration
       require'nvim-treesitter.configs'.setup {
         -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
         highlight = {
