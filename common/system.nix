d9edbf591fc6debc5e0a61c5540c436092ceb8c6
Zhong Jianxin <azuwis@gmail.com>
2022-02-27 22:27:29 +0800

    system: Create /etc/nix/inputs/nixpkgs
    and set nixPath to it, so that updating nix flake nixpkgs will also
    immediately update <nixpkgs>

--- common/system.nix
+++ common/system.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, darwinNixpkgs, ... }:
 
+let
+  nixpkgs = darwinNixpkgs;
+in
+
 {
   nix.extraOptions = ''
     auto-optimise-store = true
@@ -7,8 +11,9 @@
     keep-outputs = true
     tarball-ttl = 43200
   '';
-  nix.nixPath = [ "nixpkgs=${darwinNixpkgs}" ];
-  nix.registry.nixpkgs.flake = darwinNixpkgs;
+  environment.etc."nix/inputs/nixpkgs".source = nixpkgs;
+  nix.nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
+  nix.registry.nixpkgs.flake = nixpkgs;
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = import ./overlays.nix;
   programs.zsh.enable = true;
