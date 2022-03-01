d57e02fb4991ba9e800951388307c05fda79b120
Zhong Jianxin <azuwis@gmail.com>
2022-03-01 14:13:01 +0800

    neovim: Ignore rm error

--- common/neovim/default.nix
+++ common/neovim/default.nix
@@ -9,7 +9,7 @@
   ];
   # workaround for https://github.com/lewis6991/impatient.nvim/issues/42
   home.activation.neovim = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    rm ~/.cache/nvim/luacache_chunks ~/.cache/nvim/luacache_modpaths
+    rm -f ~/.cache/nvim/luacache_chunks ~/.cache/nvim/luacache_modpaths
   '';
   home.sessionVariables.EDITOR = "nvim";
   programs.neovim = {
