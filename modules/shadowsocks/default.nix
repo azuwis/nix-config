91ec1fb9148ffe2a4db6d398dbe2cb3590f3f7fe
Zhong Jianxin <azuwis@gmail.com>
2022-03-07 21:59:30 +0800

    shadowsocks: Allow config programArgs/user/uid

--- modules/shadowsocks/default.nix
+++ modules/shadowsocks/default.nix
@@ -19,6 +19,26 @@ in {
       description = "This option specifies the shadowsocks package to use.";
     };
 
+    services.shadowsocks.programArgs = mkOption {
+      type = types.listOf types.str;
+      default = [ "${cfg.package}/bin/ss-local" "-c" "${cfg.config}" ];
+      description = "This option specifies the shadowsocks program and args to use.";
+    };
+
+    services.shadowsocks.user = mkOption {
+      type = types.str;
+      default = "shadowsocks";
+      defaultText = "shadowsocks";
+      description = "This option specifies the shadowsocks user to use.";
+    };
+
+    services.shadowsocks.uid = mkOption {
+      type = types.int;
+      default = 590;
+      defaultText = "590";
+      description = "This option specifies the shadowsocks uid to use.";
+    };
+
     services.shadowsocks.config = mkOption {
       type = types.path;
       default = "/etc/shadowsocks/ss-local.json";
@@ -27,29 +47,32 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ];
+  config = mkMerge [
+    (mkIf cfg.enable {
+      environment.systemPackages = [ cfg.package ];
 
-    users.knownGroups = [ "shadowsocks" ];
-    users.knownUsers = [ "shadowsocks" ];
-    users.groups.shadowsocks = { gid = mkDefault 590; };
-    users.users.shadowsocks = {
-      uid = mkDefault 590;
-      gid = config.users.groups.shadowsocks.gid;
-      home = "/var/empty";
-      shell = "/usr/bin/false";
-    };
+      launchd.daemons.shadowsocks = {
+        serviceConfig.ProgramArguments = [
+          "/bin/sh" "-c" ''/bin/wait4path /nix/store && exec "$@"'' "--"
+        ] ++ cfg.programArgs;
 
-    launchd.daemons.shadowsocks = {
-      serviceConfig.ProgramArguments = [
-        "/bin/sh" "-c" ''/bin/wait4path /nix/store && exec "$@"'' "--"
-        "${cfg.package}/bin/ss-local" "-c" "${cfg.config}"
-      ];
+        serviceConfig.UserName = cfg.user;
+        serviceConfig.KeepAlive = true;
+        serviceConfig.RunAtLoad = true;
+        serviceConfig.SoftResourceLimits.NumberOfFiles = 4096;
+      };
+    })
 
-      serviceConfig.UserName = "shadowsocks";
-      serviceConfig.KeepAlive = true;
-      serviceConfig.RunAtLoad = true;
-      serviceConfig.SoftResourceLimits.NumberOfFiles = 4096;
-    };
-  };
+    (mkIf (cfg.enable && cfg.user != "root") {
+      users.knownGroups = [ "shadowsocks" ];
+      users.knownUsers = [ "shadowsocks" ];
+      users.groups.shadowsocks = { gid = cfg.uid; };
+      users.users.shadowsocks = {
+        uid = cfg.uid;
+        gid = config.users.groups.shadowsocks.gid;
+        home = "/var/empty";
+        shell = "/usr/bin/false";
+      };
+    })
+  ];
 }
