0ac49ffe287233da8a6f1234aa0f09f96ba7c979
Zhong Jianxin <azuwis@gmail.com>
2022-03-14 21:19:13 +0800

    legacyfox: Add meta info

--- pkgs/legacyfox/default.nix
+++ pkgs/legacyfox/default.nix
@@ -1,16 +1,24 @@
-{ stdenvNoCC, fetchFromGitHub }:
+{ lib, stdenvNoCC, fetchFromGitHub }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "legacyfox";
   version = "2.3";
+
   src = fetchFromGitHub {
     owner = "girst";
     repo = "LegacyFox-mirror-of-git.gir.st";
     rev = "v${version}";
     sha256 = "sha256-3p6W/O8LfsIuAp/qFJFCzO5ufA2ElCEKgFUfg00/z/U=";
   };
+
   installPhase = ''
     mkdir $out/
     cp -r defaults/ legacy/ config.js legacy.manifest $out/
   '';
+
+  meta = with lib; {
+    description = "Monkeypatching Firefox Quantum to run VimFx";
+    homepage = "https://git.gir.st/LegacyFox.git";
+    license = licenses.mpl20;
+  };
 }
