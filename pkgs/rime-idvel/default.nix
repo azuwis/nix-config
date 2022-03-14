937538db62274263db2378af377808fd17da27ae
Zhong Jianxin <azuwis@gmail.com>
2022-03-14 21:34:37 +0800

    rime-idvel: Add meta info

--- pkgs/rime-idvel/default.nix
+++ pkgs/rime-idvel/default.nix
@@ -1,14 +1,16 @@
-{ stdenvNoCC, fetchFromGitHub }:
+{ lib, stdenvNoCC, fetchFromGitHub }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "rime-idvel";
   version = "unstable-2022-03-01";
+
   src = fetchFromGitHub {
     owner = "iDvel";
     repo = "rime-settings";
     rev = "e367add5e0c6dbbc7f150ff9e63fbf6ba1e7d0a9";
     sha256 = "0z0p4vclb6y1iz17jlz275zhcyyl642bx3nnfzhxjr61xg94ydi1";
   };
+
   installPhase = ''
     mkdir $out/
     sed -i -e '/cn_dicts\/av/d' -e '/cn_dicts\/private/d' pinyin_simp.dict.yaml
@@ -16,4 +18,9 @@ stdenvNoCC.mkDerivation rec {
     mkdir $out/cn_dicts
     cp cn_dicts/8105.dict.yaml cn_dicts/sys*.dict.yaml $out/cn_dicts/
   '';
+
+  meta = with lib; {
+    description = "Rime pinyin dict from iDvel";
+    homepage = "https://github.com/iDvel/rime-settings";
+  };
 }
