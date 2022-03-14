257b95e4a46f5e1341f01d8d28a9666af1db0ca6
Zhong Jianxin <azuwis@gmail.com>
2022-03-14 21:40:53 +0800

    neovim: Update nvchad

--- common/neovim/nvchad.nix
+++ common/neovim/nvchad.nix
@@ -3,12 +3,12 @@
 let
   nvchad = pkgs.vimUtils.buildVimPluginFrom2Nix {
     pname = "nvchad";
-    version = "unstable-2022-03-01";
+    version = "unstable-2022-03-13";
     src = pkgs.fetchFromGitHub {
       owner = "NvChad";
       repo = "NvChad";
-      rev = "1777be0064c5f24bcd1a589c5fbfe3bbd330f160";
-      sha256 = "1wswpxahx9disj1l9n04pvrcnb53h5h1qi8d3g4nzmj7dag6zipc";
+      rev = "4ad0bf1b22c15e8ee1df5572504109d9899a9f51";
+      sha256 = "0gzis69mnw47a2vdmsn42s9sf27f1ks6c119fyi9svy9vx9x1py2";
     };
     meta.homepage = "https://github.com/NvChad/NvChad/";
   };
