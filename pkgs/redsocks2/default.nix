f61c655f5065648858f58e6764db766130b1a1f2
Zhong Jianxin <azuwis@gmail.com>
2022-03-14 21:27:57 +0800

    redsocks2: Add meta info

--- pkgs/redsocks2/default.nix
+++ pkgs/redsocks2/default.nix
@@ -1,18 +1,28 @@
-{ stdenv, fetchFromGitHub, cacert, curl, libevent, openssl, darwin }:
+{ lib, stdenv, fetchFromGitHub, cacert, curl, libevent, openssl, darwin }:
 
 stdenv.mkDerivation {
   pname = "redsocks2";
   version = "unstable-2021-08-22";
+
   src = fetchFromGitHub {
     owner = "azuwis";
     repo = "redsocks";
     rev = "5a3b5a6bd3b99a6e0bafa73c57faa4db2f3b61ee";
     sha256 = "11b71a3n0iljb5nbpdgbxlsa4g94vlgdrrnwgldc5rfc8wcc2ggw";
   };
+
   buildInputs = [ cacert curl libevent openssl darwin.DarwinTools ];
+
   makeFlags = [ "DISABLE_SHADOWSOCKS=true" "release" ];
+
   installPhase = ''
     install -d 0644 $out/bin
     install -m 0755 redsocks2 $out/bin
   '';
+
+  meta = with lib; {
+    description = "Transparent redirector of any TCP/UDP connection to proxy";
+    homepage = "https://github.com/azuwis/redsocks";
+    license = licenses.asl20;
+  };
 }
