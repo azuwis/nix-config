1c6f4342b6faa82356e40cd172e5b6d5afa46315
Zhong Jianxin <azuwis@gmail.com>
2022-03-23 22:34:43 +0800

    nuc: Set cpuFreqGovernor to schedutil

--- hosts/nuc.nix
+++ hosts/nuc.nix
@@ -9,10 +9,12 @@
     ../nixos/sway.nix
     ../nixos/torrent.nix
   ];
+  powerManagement.cpuFreqGovernor = "schedutil";
   fileSystems."/".options = [ "compress-force=zstd" ];
   fileSystems."/srv".options = [ "compress=zstd" ];
   networking.hostName = "nuc";
   networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = false;
   networking.interfaces.vlan1.useDHCP = true;
   networking.vlans.vlan1.id = 1;
   networking.vlans.vlan1.interface = "eno1";
