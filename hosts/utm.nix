6d36a6fb50659eaab072bf2398c36f082bfce19c
Zhong Jianxin <azuwis@gmail.com>
2022-03-25 15:35:45 +0800

    utm: Set compress-force for /

--- hosts/utm.nix
+++ hosts/utm.nix
@@ -5,6 +5,6 @@
     # nixos-generate-config --show-hardware-config > utm-hardware.nix
     ./utm-hardware.nix
   ];
-  fileSystems."/".options = [ "compress=zstd" ];
+  fileSystems."/".options = [ "compress-force=zstd" ];
   networking.hostName = "utm";
 }
