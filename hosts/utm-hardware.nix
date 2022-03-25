37ee14f4dbd95a80ca38f579329050aa63679b95
Zhong Jianxin <azuwis@gmail.com>
2022-03-25 15:33:51 +0800

    utm: Update auto generated utm-hardware.nix

--- hosts/utm-hardware.nix
+++ hosts/utm-hardware.nix
@@ -25,4 +25,10 @@
 
   swapDevices = [ ];
 
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = lib.mkDefault false;
+  networking.interfaces.enp0s8.useDHCP = lib.mkDefault true;
+
 }
