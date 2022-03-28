e851c4ff77640adb5c5b06c296919836062db0ee
Zhong Jianxin <azuwis@gmail.com>
2022-03-28 23:48:52 +0800

    fcitx5: Remove fcitx5-configtool
    lots of kde stuff

--- nixos/fcitx5/default.nix
+++ nixos/fcitx5/default.nix
@@ -1,8 +1,20 @@
 { config, lib, pkgs, ...}:
 
+let
+  fcitx5 = pkgs.fcitx5-with-addons.override {
+    fcitx5-configtool = null;
+    addons = with pkgs; [
+      fcitx5-chinese-addons
+    ];
+  };
+in
+
 if builtins.hasAttr "hm" lib then
 
 {
+  # home.activation.fcitx5 = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #   ${fcitx5}/bin/fcitx5-remote -r || true
+  # '';
   xdg.configFile.fcitx5.source = ./config;
 }
 
@@ -10,7 +22,6 @@ else
 
 {
   i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = with pkgs; [
-    fcitx5-chinese-addons
-  ];
+  i18n.inputMethod.package = lib.mkForce fcitx5;
+  systemd.user.services.fcitx5-daemon.script = lib.mkForce "${fcitx5}/bin/fcitx5";
 }
