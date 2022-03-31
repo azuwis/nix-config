154009e0b0609bd819fd48518ee87d0cff7966c7
Zhong Jianxin <azuwis@gmail.com>
2022-04-01 02:26:45 +0800

    hass: Add basic hass config

--- nixos/hass/default.nix
+++ nixos/hass/default.nix
@@ -9,4 +9,51 @@ in
     ./mosquitto.nix
     ./zigbee2mqtt.nix
   ];
+
+  services.home-assistant = {
+    enable = true;
+    # package = (pkgs.home-assistant.override {
+    #   extraComponents = [
+    #   ];
+    # }).overrideAttrs (o: { doInstallCheck = false; });
+  };
+
+  services.home-assistant.config = {
+    homeassistant = {
+      name = "Home";
+      latitude = "!secret latitude";
+      longitude = "!secret longitude";
+      elevation = "!secret elevation";
+      customize_domain.climate.icon = "mdi:air-conditioner";
+    };
+    frontend.themes = {
+      custom = {
+        state-icon-active-color = "#F57F17";
+      };
+    };
+    config = {};
+    http = {
+      server_host = "127.0.0.1";
+      use_x_forwarded_for = true;
+      trusted_proxies = [ "127.0.0.1" ];
+    };
+    history = {};
+    logbook = {};
+    sun = {};
+    logger.default = "warning";
+  };
+
+  services.nginx.virtualHosts.hass = {
+    serverName = "h.${domain}";
+    onlySSL = true;
+    useACMEHost = "default";
+    extraConfig = ''
+      ssl_client_certificate ${../ca.crt};
+      ssl_verify_client on;
+    '';
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8123";
+      proxyWebsockets = true;
+    };
+  };
 }
