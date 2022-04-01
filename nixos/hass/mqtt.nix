6e40a33303b0c93539ffc072a7cd081d1f5140f1
Zhong Jianxin <azuwis@gmail.com>
2022-04-01 14:30:35 +0800

    mqtt: Fix mosquitto ACL

--- nixos/hass/mqtt.nix
+++ nixos/hass/mqtt.nix
@@ -4,12 +4,13 @@
   services.mosquitto = {
     enable = true;
     listeners = [{
+      acl = [ "pattern readwrite #" ];
       address = "127.0.0.1";
-      settings = {
-        allow_anonymous = true;
-      };
+      omitPasswordAuth = true;
+      settings.allow_anonymous = true;
     }];
   };
 
   services.home-assistant.config.mqtt = {};
+  # services.home-assistant.config.logger.logs."homeassistant.components.mqtt" = "debug";
 }
