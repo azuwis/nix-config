b4a8f3ad7dd73ba7525d989a4c154fa85a987377
Zhong Jianxin <azuwis@gmail.com>
2022-04-01 14:36:41 +0800

    nuc: Another vlan

--- hosts/nuc.nix
+++ hosts/nuc.nix
@@ -15,9 +15,15 @@
   fileSystems."/srv".options = [ "compress=zstd" ];
   networking.hostName = "nuc";
   networking.interfaces.eno1.useDHCP = false;
-  networking.interfaces.vlan1.useDHCP = true;
   networking.vlans.vlan1.id = 1;
   networking.vlans.vlan1.interface = "eno1";
+  networking.interfaces.vlan1.useDHCP = true;
+  networking.vlans.vlan5.id = 5;
+  networking.vlans.vlan5.interface = "eno1";
+  networking.interfaces.vlan5.ipv4 = {
+    addresses = [{ address = "192.168.20.250"; prefixLength = 24; }];
+    routes = [{ address = "240.0.0.0"; prefixLength = 4; }];
+  };
   services.beesd.filesystems = {
     nixos = {
       spec = "/";
