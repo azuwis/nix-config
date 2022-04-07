2e5acc0de84804a793332f79eb5e613cc587b31b
Zhong Jianxin <azuwis@gmail.com>
2022-04-07 20:04:49 +0800

    flake: Use nixos-unstable as inputs.nixpkgs
    According to https://status.nixos.org/, nixos-unstable is more stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     # utils.url = "github:gytis-ivaskevicius/flake-utils-plus/master";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus/pull/117/head";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home.url = "github:nix-community/home-manager/master";
