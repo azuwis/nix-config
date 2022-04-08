06d85d5a4fb16075b97a4385a502ee949e05293d
Zhong Jianxin <azuwis@gmail.com>
2022-04-08 10:37:02 +0800

    rime-idvel: Update to HEAD

--- pkgs/rime-idvel/default.nix
+++ pkgs/rime-idvel/default.nix
@@ -2,21 +2,19 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "rime-idvel";
-  version = "unstable-2022-03-16";
+  version = "unstable-2022-04-01";
 
   src = fetchFromGitHub {
     owner = "iDvel";
     repo = "rime-settings";
-    rev = "9e637e1287f89f38bf82aeeefd70aa7d5a388be3";
-    sha256 = "1nxa2dnhfhs54y2p4zi7rkkm3954j9pzs7mx3bjz83spbssim5af";
+    rev = "c5a3db390ea3a9f0f8df83d9835aa0be1ea0f312";
+    sha256 = "1f5a89vg4r10l3lz20hh432i25gfw2n6dc301xf6h654gnpvv3xj";
   };
 
   installPhase = ''
     mkdir $out/
-    sed -i -e '/cn_dicts\/av/d' -e '/cn_dicts\/private/d' pinyin_simp.dict.yaml
-    cp pinyin_simp.dict.yaml $out/
-    mkdir $out/cn_dicts
-    cp cn_dicts/8105.dict.yaml cn_dicts/sys*.dict.yaml $out/cn_dicts/
+    sed -i -e '/cn_dicts\/private/d' pinyin_simp.dict.yaml
+    cp -r cn_dicts/ pinyin_simp.dict.yaml $out/
   '';
 
   meta = with lib; {
