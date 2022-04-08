0ab0e0c56c9f14ee1191b9f805f64e3f48208da9
Zhong Jianxin <azuwis@gmail.com>
2022-04-08 10:32:02 +0800

    nix-index: Use github latest URL

--- common/nix-index.nix
+++ common/nix-index.nix
@@ -3,12 +3,12 @@
 let
   indexCache = {
     darwin = pkgs.fetchurl {
-      url = "https://github.com/Mic92/nix-index-database/releases/download/2022-03-20/index-x86_64-darwin";
-      sha256 = "0359mv6pxgk9hsi2sq50p9xr8d89lajy3f67rilwq777wvqrgs55";
+      url = "https://github.com/Mic92/nix-index-database/releases/latest/download/index-x86_64-darwin";
+      sha256 = "05gr9w15bpvm3kai0gz5bg0w2nv74v8srm1v8im6yf1qr0823vin";
     };
     linux = pkgs.fetchurl {
-      url = "https://github.com/Mic92/nix-index-database/releases/download/2022-03-20/index-x86_64-linux";
-      sha256 = "10kwka1314miqc4c11qkp5r1s4d2j1dxzl3bg6rvx6lxmxqhywn2";
+      url = "https://github.com/Mic92/nix-index-database/releases/latest/download/index-x86_64-linux";
+      sha256 = "0qz243j42502fcqb9xah5sda8y408qb933mm3hzgw0bjydhk1k2q";
     };
   }.${pkgs.stdenv.hostPlatform.parsed.kernel.name};
 in
