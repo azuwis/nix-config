edd4195c51e98e9b6361b6f26c7ab22b78d36ab5
Zhong Jianxin <azuwis@gmail.com>
2022-04-08 10:35:58 +0800

    torrent-ratio: For update-nix-fetchgit

--- pkgs/torrent-ratio/default.nix
+++ pkgs/torrent-ratio/default.nix
@@ -6,9 +6,9 @@ buildGoModule rec {
 
   src = fetchFromGitHub {
     owner = "azuwis";
-    repo = pname;
+    repo = "torrent-ratio";
     rev = "436d0f6b7825183cc4c3b1ce8a1a842668993233";
-    sha256 = "sha256-yJ3i5PNnSUxcwRkT23uL8mcEtDieskfaf3mt1vJqbZk=";
+    sha256 = "16bddbrddbbrgzd4gcly72s08rzjidxxn4qrq5f4qjb7ygjf57f8";
   };
 
   vendorSha256 = "sha256-HH0VHleShuv91QkV1CC8thgBWe5RgoUKhXa706Ked04=";
