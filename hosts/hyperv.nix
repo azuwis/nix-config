85ce7072ae8bac514ee48ccba209dedc69668528
Zhong Jianxin <azuwis@gmail.com>
2022-04-13 10:20:03 +0800

    hyperv: Reset services.udev.extraHwdb

--- hosts/hyperv.nix
+++ hosts/hyperv.nix
@@ -18,8 +18,6 @@
       { devices = [ "nodev" ]; path = "/boot3"; }
     ];
   };
-  # conflict with hyperv_drm, needed by sway
-  boot.blacklistedKernelModules = [ "hyperv_fb" ];
   fileSystems."/boot1" = { options = [ "defaults" "nofail" "x-systemd.device-timeout=5s" ]; };
   fileSystems."/boot2" = { options = [ "defaults" "nofail" "x-systemd.device-timeout=5s" ]; };
   fileSystems."/boot3" = { options = [ "defaults" "nofail" "x-systemd.device-timeout=5s" ]; };
@@ -29,4 +27,10 @@
   networking.hostId = "65e3452b";
   services.zfs.autoScrub.enable = true;
   services.zfs.autoSnapshot.enable = true;
+
+  # conflict with hyperv_drm, needed by sway
+  boot.blacklistedKernelModules = [ "hyperv_fb" ];
+
+  # follow OS keyboard, so capslock/ctrl will NOT be swapped back
+  services.udev.extraHwdb = lib.mkForce "";
 }
