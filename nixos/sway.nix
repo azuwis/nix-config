52667cd1def0cbb6134e4cc20e8c260207f1ac92
Zhong Jianxin <azuwis@gmail.com>
2022-04-16 00:21:40 +0800

    sway: Split foot config to foot.nix
    Also disable systemd server, if managed by systemd, when sway exit, the
    foot server will not come back

--- nixos/sway.nix
+++ nixos/sway.nix
@@ -3,24 +3,13 @@
 if builtins.hasAttr "hm" lib then
 
 {
+  imports = [
+    ./foot.nix
+  ];
+
   home.packages = with pkgs; [
     pulsemixer
   ];
-
-  programs.foot = {
-    enable = true;
-    server.enable = true;
-    settings = {
-      main = {
-        dpi-aware = "no";
-        font = lib.mkDefault "monospace:size=12";
-        include = "${pkgs.fetchurl {
-          url = "https://codeberg.org/dnkl/foot/raw/commit/a1796ba5cd1cc7b6ef03021d7db57503e445b5dd/themes/nord";
-          sha256 = "sha256-YBoRdklYm2nK9xdypxNZFTloJ3xhKH0d4MvykGUP3i0=";
-        }}";
-      };
-    };
-  };
 
   wayland.windowManager.sway = {
     enable = true;
@@ -29,7 +18,6 @@ if builtins.hasAttr "hm" lib then
       gaps.smartBorders = "no_gaps";
       menu = "${pkgs.fuzzel}/bin/fuzzel --lines=8 --no-icons --background-color=2E3440FF --text-color=D8DEE9FF --selection-color=4C566AFF --selection-text-color=E8DEE9FF --log-level=error";
       output."*".bg = "#2E3440 solid_color";
-      terminal = "footclient";
       window.hideEdgeBorders = "both";
     };
   };
