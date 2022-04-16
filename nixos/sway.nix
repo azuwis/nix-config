b37175b0a9fbe723252d56eec9153d99ab35ed79
Zhong Jianxin <azuwis@gmail.com>
2022-04-16 12:44:05 +0800

    sway: Set fuzzel font

--- nixos/sway.nix
+++ nixos/sway.nix
@@ -17,7 +17,7 @@ if builtins.hasAttr "hm" lib then
     package = null;
     config = {
       gaps.smartBorders = "no_gaps";
-      menu = "${pkgs.fuzzel}/bin/fuzzel --lines=8 --no-icons --background-color=2E3440FF --text-color=D8DEE9FF --selection-color=4C566AFF --selection-text-color=E8DEE9FF --log-level=error";
+      menu = "${pkgs.fuzzel}/bin/fuzzel --lines=8 --no-icons --font=monospace:pixelsize=20 --background-color=2E3440FF --text-color=D8DEE9FF --selection-color=4C566AFF --selection-text-color=E8DEE9FF --log-level=error";
       output."*".bg = "#2E3440 solid_color";
       window.hideEdgeBorders = "both";
     };
