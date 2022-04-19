94bbaa8469015a06da528fa3be8145b33f20008f
Zhong Jianxin <azuwis@gmail.com>
2022-04-20 00:07:35 +0800

    sway: mod+c do not toggle floating

--- nixos/sway.nix
+++ nixos/sway.nix
@@ -27,7 +27,7 @@ if builtins.hasAttr "hm" lib then
         "${mod}+Tab" = "workspace back_and_forth";
         # stop graphical-session.target so services like foot will not try to restart itself
         "${mod}+Shift+e" = "exec swaynag -t warning -m 'Do you really want to exit sway?' -b 'Yes, exit sway' 'systemctl --user stop graphical-session.target; swaymsg exit'";
-        "${mod}+c" = "floating toggle; move absolute position center";
+        "${mod}+c" = "floating enable; move absolute position center";
       };
       output."*".bg = "#2E3440 solid_color";
       window.hideEdgeBorders = "both";
