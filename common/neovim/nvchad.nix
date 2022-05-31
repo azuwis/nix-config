b0a42504890649a2067c9b62f21b3a5275d2eab6
Zhong Jianxin <azuwis@gmail.com>
2022-05-31 15:42:43 +0800

    neovim: Update nvchad to HEAD

--- common/neovim/nvchad.nix
+++ common/neovim/nvchad.nix
@@ -3,12 +3,12 @@
 let
   nvchad = pkgs.vimUtils.buildVimPluginFrom2Nix {
     pname = "nvchad";
-    version = "unstable-2022-05-27";
+    version = "unstable-2022-05-30";
     src = pkgs.fetchFromGitHub {
       owner = "NvChad";
       repo = "NvChad";
-      rev = "f8b5571466173f6518ab22351bacda186fd23b2e";
-      sha256 = "1v2c27vp4jzzy3hwc4sw0a4mjl39nmn7cqg8vk5jva6h8x836p7c";
+      rev = "59de298d8fc05b4ee2633cd2dfe9d517cf6f6d68";
+      sha256 = "020a1x6m6ng0zi0694bv7pnzbnphrw1lms3jshr28kdrw3nfd4ba";
     };
     # nvim-lsp-installer does not work well on NixOS, e.g. It checks nodejs when yamlls exe already exists
     postPatch = ''
