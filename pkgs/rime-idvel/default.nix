7825849a4ac0ef103f2636f6234a8f7ae1b24783
Zhong Jianxin <azuwis@gmail.com>
2022-06-15 13:51:20 +0800

    rime-idvel: Update

--- pkgs/rime-idvel/default.nix
+++ pkgs/rime-idvel/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "rime-idvel";
-  version = "unstable-2022-06-12";
+  version = "unstable-2022-06-15";
 
   src = fetchFromGitHub {
     owner = "iDvel";
-    repo = "rime-settings";
-    rev = "68ed82581e9a554af1cf4869f84a26d895d42132";
-    sha256 = "178ai7myr8a83iiykl9sw9vandj3nkd89vrwzxw9x67cib91nmig";
+    repo = "rime-ice";
+    rev = "1a87095c5f1b33ae3f22bc793c04f6a2390790bd";
+    sha256 = "0ax6m2vygak7k83ml7idp4j4bwf9q1abb5kq7h2x4sl1mk31f044";
   };
 
   installPhase = ''
