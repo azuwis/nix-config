74e6924ec7b0131230cc1423c1aae2ae449dbdd4
Zhong Jianxin <azuwis@gmail.com>
2022-06-16 10:04:02 +0800

    sketchybar: Update to v2.7.0

--- pkgs/sketchybar/default.nix
+++ pkgs/sketchybar/default.nix
@@ -10,13 +10,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "sketchybar";
-  version = "2.5.2";
+  version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "FelixKratz";
     repo = "SketchyBar";
     rev = "v${version}";
-    sha256 = "sha256-xkgNPVrGxmi8377+G1HQ8SdwS0fOnGhwYy43yLRyvF0=";
+    sha256 = "sha256-wW/gHf/ZqDFkwl2Wc94V9b3KoAc6fsIFCDULlxnLXm8=";
   };
 
   buildInputs = [ Carbon Cocoa SkyLight ]
