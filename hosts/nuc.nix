389017811932081bdc64ead80f5dcdc0f1b341ef
Zhong Jianxin <azuwis@gmail.com>
2022-10-11 16:10:30 +0800

    nuc: Disable vlan config

--- hosts/nuc.nix
+++ hosts/nuc.nix
@@ -16,16 +16,16 @@
   powerManagement.cpuFreqGovernor = "schedutil";
   fileSystems."/srv".options = [ "compress=zstd" ];
   networking.hostName = "nuc";
-  networking.useDHCP = false;
-  networking.vlans.vlan1.id = 1;
-  networking.vlans.vlan1.interface = "eno1";
-  networking.interfaces.vlan1.useDHCP = true;
-  networking.vlans.vlan5.id = 5;
-  networking.vlans.vlan5.interface = "eno1";
-  networking.interfaces.vlan5.ipv4 = {
-    addresses = [{ address = "192.168.20.250"; prefixLength = 24; }];
-    routes = [{ address = "240.0.0.0"; prefixLength = 4; }];
-  };
+  # networking.useDHCP = false;
+  # networking.vlans.vlan1.id = 1;
+  # networking.vlans.vlan1.interface = "eno1";
+  # networking.interfaces.vlan1.useDHCP = true;
+  # networking.vlans.vlan5.id = 5;
+  # networking.vlans.vlan5.interface = "eno1";
+  # networking.interfaces.vlan5.ipv4 = {
+  #   addresses = [{ address = "192.168.20.250"; prefixLength = 24; }];
+  #   routes = [{ address = "240.0.0.0"; prefixLength = 4; }];
+  # };
   services.beesd.filesystems = {
     nixos = {
       spec = "/";
