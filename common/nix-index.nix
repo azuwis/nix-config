f40e26c291101716e8228d648034da6b73dc0ce0
Zhong Jianxin <azuwis@gmail.com>
2022-11-15 09:14:54 +0800

    update-src: Auto update common/nix-index.nix

--- common/nix-index.nix
+++ common/nix-index.nix
@@ -3,12 +3,12 @@
 let
   indexCache = {
     darwin = pkgs.fetchurl {
-      url = "https://github.com/Mic92/nix-index-database/releases/download/2022-10-16/index-x86_64-darwin";
-      sha256 = "09rzyjjw5kahgywjxczn2zyyz1y93rg08mafi30iaga1n9z8nh4w";
+      url = "https://github.com/Mic92/nix-index-database/releases/download/2022-11-06/index-x86_64-darwin";
+      sha256 = "0wq0f746fqng3d7cj1nk71zmm5rzb302qry5lyly03ffaws08f9n";
     };
     linux = pkgs.fetchurl {
-      url = "https://github.com/Mic92/nix-index-database/releases/download/2022-10-16/index-x86_64-linux";
-      sha256 = "1f7hil8q2iig5742sipzskgv7anchl84i74hayqxz80w0mkjp6i7";
+      url = "https://github.com/Mic92/nix-index-database/releases/download/2022-11-06/index-x86_64-linux";
+      sha256 = "0a7sn64hlriah7br4lyjwf3l3n5rnwc9hb79lk870czl2rhdr706";
     };
   }.${pkgs.stdenv.hostPlatform.parsed.kernel.name};
 in
