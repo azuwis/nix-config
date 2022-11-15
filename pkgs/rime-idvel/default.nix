50c03a3c747f673acb872cefc0f3bee4f1f01f4f
Zhong Jianxin <azuwis@gmail.com>
2022-11-15 09:15:08 +0800

    update-src: Auto update pkgs/rime-idvel/default.nix

--- pkgs/rime-idvel/default.nix
+++ pkgs/rime-idvel/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "rime-idvel";
-  version = "unstable-2022-10-19";
+  version = "unstable-2022-11-11";
 
   src = fetchFromGitHub {
     owner = "iDvel";
     repo = "rime-ice";
-    rev = "424eae48baff4028b7aa92cb4c1433f6333977b5";
-    sha256 = "0y3vamj4fbrc4crcvizazf9zw4r168p7akmz66p1rk7cimr1z247";
+    rev = "a248857be0f750f90bc4f9c4e0c76ebff678e20e";
+    sha256 = "1vdyh3dbf0mjlxqrvb1mnqfh63pxd7cq8h1askp18qg6chnxbzvy";
   };
 
   installPhase = ''
