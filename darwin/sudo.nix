336b84302083f09e95baed66fe5bf4e64d8cbb87
Zhong Jianxin <azuwis@gmail.com>
2022-11-25 10:51:06 +0800

    darwin: Use security.pam.enableSudoTouchIdAuth

--- darwin/sudo.nix
+++ darwin/sudo.nix
@@ -4,16 +4,5 @@
   environment.etc."sudoers.d/custom".text = ''
     Defaults timestamp_timeout=300
   '';
-  system.patches = [
-    (pkgs.writeText "pam-sudo.patch" ''
-      --- a/etc/pam.d/sudo
-      +++ b/etc/pam.d/sudo
-      @@ -1,4 +1,5 @@
-       # sudo: auth account password session
-      +auth       sufficient     pam_tid.so
-       auth       sufficient     pam_smartcard.so
-       auth       required       pam_opendirectory.so
-       account    required       pam_permit.so
-    '')
-  ];
+  security.pam.enableSudoTouchIdAuth = true;
 }
