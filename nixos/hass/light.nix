dea3594dab1813f646b9577e043a9765e94ddf24
Zhong Jianxin <azuwis@gmail.com>
2022-12-18 00:15:02 +0800

    hass: Update lights

--- nixos/hass/light.nix
+++ nixos/hass/light.nix
@@ -4,21 +4,42 @@
   services.home-assistant.config = {
     logger.logs."homeassistant.components.yeelight" = "critical";
     yeelight.devices = {
-      "light_living_room.lan" = {
+      "yeelink-light-ceiling12_mibtEA04.lan" = {
         name = "Living room";
-        model = "ceiling4";
-        transition = 1000;
+        model = "ceiling13";
       };
-      "light_bedroom.lan" = {
-        name = "Bedroom";
-        model = "ceiling3";
-        transition = 1000;
+      "yeelink-light-ceiling13_mibtF035.lan" = {
+        name = "Primary bedroom";
+        model = "ceiling13";
       };
-      "light_kitchen.lan" = {
-        name = "Kitchen";
+      "yeelink-light-ceiling14_mibtE345.lan" = {
+        name = "Secondary bedroom";
+        model = "ceiling13";
+      };
+      "yeelink-light-ceiling13_mibt8675.lan" = {
+        name = "Kids room";
+        model = "ceiling13";
+      };
+      "yeelink-light-panel1_mibt8793.lan" = {
+        name = "Kitchen door";
         model = "mono1";
-        transition = 1000;
+      };
+      "yeelink-light-panel1_mibt813A.lan" = {
+        name = "Kitchen window";
+        model = "mono1";
+      };
+      "yeelink-light-panel1_mibt8400.lan" = {
+        name = "Bathroom";
+        model = "mono1";
       };
     };
+    light = [{
+      platform = "group";
+      name = "Kitchen";
+      entities = [
+        "light.kitchen_door"
+        "light.kitchen_window"
+      ];
+    }];
   };
 }
