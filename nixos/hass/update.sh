d64aef42783308bfdb2067c36d9cdf48a6266d39
Zhong Jianxin <azuwis@gmail.com>
2022-12-19 00:19:08 +0800

    hass: Add update.sh

---
+++ nixos/hass/update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -u
+
+update_fetchgit() {
+  file="$1"
+  echo "Update $file"
+  nix run nixpkgs#update-nix-fetchgit -- "$file"
+  if git commit -m "hass: Auto update $file" "$file" 1>/dev/null
+  then
+    git --no-pager show
+  fi
+}
+
+update_fetchgit zhibot.nix
+update_fetchgit xiaomi_miot.nix
