c9de229c98cbddf733fe32f4c050586c510854a0
Zhong Jianxin <azuwis@gmail.com>
2023-01-05 23:03:57 +0800

    hass: Add lovelace-resource.nix helper

--- nixos/hass/zigbee2mqtt-networkmap.nix
+++ nixos/hass/zigbee2mqtt-networkmap.nix
@@ -1,25 +1,8 @@
-{ config, lib, pkgs, ... }:
-
-let
-  js = pkgs.fetchurl {
-    url = "https://github.com/azuwis/zigbee2mqtt-networkmap/releases/download/v0.7.0/zigbee2mqtt-networkmap.js";
-    sha256 = "sha256-OB2MOzFtSwzmW1b1wxBrWvW2GXdzf80vD/p+t5TN2jE=";
-  };
-in
-
-{
-  services.nginx.virtualHosts.hass = {
-    locations."= /local/zigbee2mqtt-networkmap.js" = {
-      alias = js;
-    };
-  };
-
-  services.home-assistant.config = {
-    lovelace.resources = [{
-      url = "/local/zigbee2mqtt-networkmap.js?v=${builtins.hashFile "md5" js}";
-      type = "module";
-    }];
-    mqtt.sensor = [{
+import ./lovelace-resource.nix {
+  url = "https://github.com/azuwis/zigbee2mqtt-networkmap/releases/download/v0.7.0/zigbee2mqtt-networkmap.js";
+  sha256 = "sha256-OB2MOzFtSwzmW1b1wxBrWvW2GXdzf80vD/p+t5TN2jE=";
+  cfg = {
+    services.home-assistant.config.mqtt.sensor = [{
       name = "Zigbee2mqtt Networkmap";
       state_topic = "zigbee2mqtt/bridge/response/networkmap";
       value_template = ''{{ now().strftime("%Y-%m-%d %H:%M:%S") }}'';
