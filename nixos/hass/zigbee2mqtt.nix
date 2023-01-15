ca1e0233afe788718bacb681892bd8f9a321c503
Zhong Jianxin <azuwis@gmail.com>
2023-01-15 11:39:22 +0800

    hass: Add motion autos

--- nixos/hass/zigbee2mqtt.nix
+++ nixos/hass/zigbee2mqtt.nix
@@ -73,4 +73,21 @@ in
       proxyWebsockets = true;
     };
   };
+
+  hass.automations = ''
+    - alias: Lights bathroom on when sensor on
+      trigger:
+        platform: state
+        entity_id: binary_sensor.motion_bathroom_occupancy
+        from: "off"
+        to: "on"
+      condition:
+        condition: state
+        entity_id: light.bathroom
+        state: "off"
+      action:
+        service: light.turn_on
+        data:
+          entity_id: light.bathroom
+  '';
 }
