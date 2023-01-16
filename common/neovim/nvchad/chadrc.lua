6ef2989417c9e1ec8ea2ac58675c2a40465e3423
Zhong Jianxin <azuwis@gmail.com>
2023-01-16 10:30:45 +0800

    neovim: Add sudo save

--- common/neovim/nvchad/chadrc.lua
+++ common/neovim/nvchad/chadrc.lua
@@ -9,6 +9,7 @@ M.mappings = {
     n = {
       ["<leader>fq"] = { "<cmd> :wq <CR>", "Save and quit" },
       ["<leader>fs"] = { "<cmd> :update <CR>", "Save" },
+      ["<leader>fS"] = { "<cmd> :execute ':silent w !sudo tee % > /dev/null' | :edit! <CR>", "Sudo save" },
       ["<leader>gg"] = { "<cmd> :Neogit <CR>", "Neogit" },
       ["<leader><leader>"] = { "<cmd> :Telescope custom find <CR>", "Find" },
     },
