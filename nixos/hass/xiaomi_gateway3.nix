1d19b5a98725bead86a841e6f1e71a9cfb45ee92
Zhong Jianxin <azuwis@gmail.com>
2023-02-12 22:15:17 +0800

    hass: Switch to linptech sensor for bathroom

--- nixos/hass/xiaomi_gateway3.nix
+++ nixos/hass/xiaomi_gateway3.nix
@@ -23,4 +23,36 @@ in
   services.home-assistant.config = {
     # logger.logs."custom_components.xiaomi_gateway3" = "debug";
   };
+
+  hass.automations = ''
+    - alias: Lights bathroom on when sensor on
+      trigger:
+        platform: state
+        entity_id: binary_sensor.dced8387eef4_occupancy
+        from: "off"
+        to: "on"
+      condition:
+        condition: state
+        entity_id: light.bathroom
+        state: "off"
+      action:
+        service: light.turn_on
+        data:
+          entity_id: light.bathroom
+
+    - alias: Lights bathroom off when sensor off
+      trigger:
+        platform: state
+        entity_id: binary_sensor.dced8387eef4_occupancy
+        from: "on"
+        to: "off"
+      condition:
+        condition: state
+        entity_id: light.bathroom
+        state: "on"
+      action:
+        service: light.turn_off
+        data:
+          entity_id: light.bathroom
+  '';
 }
