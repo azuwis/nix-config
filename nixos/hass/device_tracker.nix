46a3cbb97ee641e1d7fa925870a1d96797731959
Zhong Jianxin <azuwis@gmail.com>
2023-03-02 19:43:17 +0800

    hass: Add movie scene autos

--- nixos/hass/device_tracker.nix
+++ nixos/hass/device_tracker.nix
@@ -10,4 +10,51 @@
     consider_home = 20;
     new_device_defaults.track_new_devices = false;
   }];
+
+  hass.automations = ''
+    - alias: Primary bedroom movie on
+      trigger:
+        - platform: state
+          entity_id: device_tracker.epson_projector
+          from: "not_home"
+          to: "home"
+      action:
+        - service: scene.create
+          data:
+            scene_id: primary_bedroom_before
+            snapshot_entities:
+              - cover.lumi_hmcn01_7c8c_curtain
+              - light.primary_bedroom
+        - if:
+          - condition: state
+            entity_id: light.primary_bedroom
+            state: "on"
+          then:
+          - service: light.turn_on
+            data:
+              entity_id: light.primary_bedroom
+              brightness: 3
+        - service: media_player.turn_on
+          data:
+            entity_id: media_player.edifier_r2000db
+        - delay: 2
+        - service: media_player.select_source
+          data:
+            entity_id: media_player.edifier_r2000db
+            source: Bluetooth
+
+    - alias: Primary bedroom movie off
+      trigger:
+        - platform: state
+          entity_id: device_tracker.epson_projector
+          from: "home"
+          to: "not_home"
+      action:
+        - service: scene.turn_on
+          target:
+            entity_id: scene.primary_bedroom_before
+        - service: media_player.turn_off
+          data:
+            entity_id: media_player.edifier_r2000db
+  '';
 }
