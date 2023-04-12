95009ebe97d2363b0464bab5ec156ad6b378286b
Zhong Jianxin <azuwis@gmail.com>
2023-04-12 10:13:29 +0800

    darwin: Disable Keychain for pinentry-mac

---
+++ darwin/gnupg/default.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Open Keychain Access, search for "GnuPG" and delete the entry
+  system.defaults.CustomUserPreferences = {
+    "org.gpgtools.pinentry-mac" = {
+      "UseKeychain" = false;
+    };
+  };
+}
