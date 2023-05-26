74426c526d4265b1b04e7bc915f013232fc0e971
Zhong Jianxin <azuwis@gmail.com>
2023-05-26 23:26:13 +0800

    fcitx5: fcitx5 is in PATH, no need to use /nix/store path

--- nixos/fcitx5/default.nix
+++ nixos/fcitx5/default.nix
@@ -1,12 +1,5 @@
 { config, lib, pkgs, ... }:
 
-let
-  addons = with pkgs; [
-    fcitx5-chinese-addons
-  ];
-  fcitx5 = pkgs.fcitx5-with-addons.override { inherit addons; };
-in
-
 if builtins.hasAttr "hm" lib then
 
 {
@@ -14,14 +7,14 @@ if builtins.hasAttr "hm" lib then
   #   ${fcitx5}/bin/fcitx5-remote -r || true
   # '';
   xdg.configFile.fcitx5.source = ./config;
-  wayland.windowManager.sway.config.startup = [{
-    command = "${fcitx5}/bin/fcitx5";
-  }];
+  wayland.windowManager.sway.config.startup = [{ command = "fcitx5"; }];
 }
 
 else
 
 {
   i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = addons;
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    fcitx5-chinese-addons
+  ];
 }
