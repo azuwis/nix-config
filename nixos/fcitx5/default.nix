e17ec631a664bd593e8b65f6605bcb8e12e09d2f
Zhong Jianxin <azuwis@gmail.com>
2023-05-26 20:38:08 +0800

    fcitx5: Use sway.config.startup to start fcitx5

--- nixos/fcitx5/default.nix
+++ nixos/fcitx5/default.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
+let
+  addons = with pkgs; [
+    fcitx5-chinese-addons
+  ];
+  fcitx5 = pkgs.fcitx5-with-addons.override { inherit addons; };
+in
+
 if builtins.hasAttr "hm" lib then
 
 {
@@ -7,13 +14,14 @@ if builtins.hasAttr "hm" lib then
   #   ${fcitx5}/bin/fcitx5-remote -r || true
   # '';
   xdg.configFile.fcitx5.source = ./config;
+  wayland.windowManager.sway.config.startup = [{
+    command = "${fcitx5}/bin/fcitx5";
+  }];
 }
 
 else
 
 {
   i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = with pkgs; [
-    fcitx5-chinese-addons
-  ];
+  i18n.inputMethod.fcitx5.addons = addons;
 }
