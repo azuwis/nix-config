26b7de2e20db5cf82fb6a9288f6e0421cd4c8a6a
Zhong Jianxin <azuwis@gmail.com>
2023-05-28 20:49:15 +0800

    nixos: Add seatd module

---
+++ modules/nixos/seatd/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.services.seatd;
+in {
+  options.services.seatd = {
+    enable = lib.mkEnableOption (lib.mdDoc "seatd");
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.groups.seatd = {};
+
+    systemd.services.seatd = {
+      enable = true;
+      description = "Seat management daemon";
+      script = "${pkgs.seatd}/bin/seatd -g seatd";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "1";
+      };
+      wantedBy = ["multi-user.target"];
+    };
+  };
+}
