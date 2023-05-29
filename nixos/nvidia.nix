8adab80e43b9ed6ecd88ceab76eac9743af6a6d1
Zhong Jianxin <azuwis@gmail.com>
2023-05-29 20:49:28 +0800

    nvidia: Config sway managed by home-manager

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,5 +1,20 @@
 { config, lib, pkgs, ... }:
 
+if builtins.hasAttr "hm" lib
+
+then
+
+{
+  wayland.windowManager.sway = {
+    extraOptions = [ "--unsupported-gpu" ];
+    extraSessionCommands = ''
+      export WLR_NO_HARDWARE_CURSORS=1
+    '';
+  };
+}
+
+else
+
 let
   rev = "b240ee53ac2f9035ee810b7f9b0b455b3876086f";
   hash = "sha256-VY0XtLc0jfqoeSLD5l90tCoKpcHBkpmgNNKH6bmnVc4=";
