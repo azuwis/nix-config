5a5eb8f1b98d09f33ffd5150585a0d2d268f2ed8
Zhong Jianxin <azuwis@gmail.com>
2023-06-05 21:00:23 +0800

    sway: Add xdgAutostart option

--- modules/nixos/sway/default.nix
+++ modules/nixos/sway/default.nix
@@ -1,19 +1,26 @@
 { config, lib, pkgs, ... }:
 
 let
-  inherit (lib) mdDoc mkEnableOption mkIf;
+  inherit (lib) mdDoc mkEnableOption mkIf mkMerge;
   cfg = config.my.sway;
 
 in {
   options.my.sway = {
     enable = mkEnableOption (mdDoc "sway");
+    xdgAutostart = mkEnableOption (mdDoc "xdgAutostart");
   };
 
-  config = mkIf cfg.enable {
-    programs.sway = {
-      enable = true;
-      package = null;
-    };
-    # services.xserver.desktopManager.runXdgAutostartIfNone = true;
-  };
+  config = mkIf cfg.enable (mkMerge [
+    ({
+      programs.sway = {
+        enable = true;
+        package = null;
+      };
+    })
+
+    (mkIf cfg.xdgAutostart {
+      services.xserver.desktopManager.runXdgAutostartIfNone = true;
+    })
+
+  ]);
 }
--- modules/nixos/sway/home.nix
+++ modules/nixos/sway/home.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 let
-  inherit (lib) mdDoc mkEnableOption mkDefault mkIf;
+  inherit (lib) mdDoc mkEnableOption mkDefault mkIf mkMerge;
   cfg = config.my.sway;
   fuzzelWrapped = with pkgs; runCommand "fuzzel" { buildInputs = [ makeWrapper ]; } ''
     options="--lines=8 --no-icons --font=monospace:pixelsize=20 --background-color=2E3440FF --text-color=D8DEE9FF --selection-color=4C566AFF --selection-text-color=E8DEE9FF --terminal=footclient --log-level=error"
@@ -13,102 +13,109 @@ let
 in {
   options.my.sway = {
     enable = mkEnableOption (mdDoc "sway");
+    xdgAutostart = mkEnableOption (mdDoc "xdgAutostart");
   };
 
-  config = mkIf cfg.enable {
-    my.cliphist.enable = true;
-    my.foot.enable = true;
-    my.swayidle.enable = mkDefault true;
-    my.yambar.enable = true;
+  config = mkIf cfg.enable (mkMerge [
+    ({
+      my.cliphist.enable = true;
+      my.foot.enable = true;
+      my.swayidle.enable = mkDefault true;
+      my.yambar.enable = true;
 
-    home.packages = with pkgs; [
-      fuzzelWrapped
-      pulsemixer
-      qt5.qtwayland
-      swappy
-      sway-contrib.grimshot
-    ];
+      home.packages = with pkgs; [
+        fuzzelWrapped
+        pulsemixer
+        qt5.qtwayland
+        swappy
+        sway-contrib.grimshot
+      ];
 
-    wayland.windowManager.sway = {
-      enable = true;
-      wrapperFeatures.gtk = true;
-      systemdIntegration = false;
-      extraSessionCommands = ''
-        export SDL_VIDEODRIVER=wayland
-        export QT_QPA_PLATFORM=wayland-egl
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-        export _JAVA_AWT_WM_NONREPARENTING=1
-      '';
-      config = {
-        # Apps
-        # swaymsg -t get_tree
-        assigns = {
-          "2" = [{ app_id = "^firefox$"; }];
-        };
-        floating.criteria = [
-          { app_id = "^mpv$"; }
-        ];
-        # Border
-        gaps.smartBorders = "no_gaps";
-        window.hideEdgeBorders = "both";
-        # Keybindings
-        menu = "fuzzel";
-        keybindings = let mod = config.wayland.windowManager.sway.config.modifier; in lib.mkOptionDefault {
-          "${mod}+Tab" = "workspace back_and_forth";
-          # stop graphical-session.target so services like foot will not try to restart itself
-          "${mod}+Shift+e" = "exec swaynag -t warning -m 'Do you really want to exit sway?' -b 'Yes, exit sway' 'systemctl --user stop graphical-session.target; swaymsg exit'";
-          "${mod}+Shift+p" = "exec passmenu";
-          "${mod}+c" = "floating enable; move absolute position center";
-          "${mod}+Shift+l" = mkDefault "exec swaylock";
-          "Print" = "grimshot save - | swappy -f -";
-          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+";
-          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%-";
-          "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-        };
-        # Inputs/outputs
-        input = {
-          "*" = {
-            natural_scroll = "enabled";
-          };
-          "type:keyboard" = {
-            repeat_delay = "300";
-            repeat_rate = "33";
-          };
-          "type:touchpad" = {
-            accel_profile = "adaptive";
-            drag_lock = "enabled";
-            dwt = "enabled";
-            pointer_accel = "0.28";
-            tap = "enabled";
-            scroll_method = "edge";
-            scroll_factor = "0.6";
-          };
-        };
-        output."*".bg = "#2E3440 solid_color";
-        startup = [
-          { command = "swaylock"; }
-        ];
-        # startup = [{
-        #   command = "systemctl --user start xdg-autostart-if-no-desktop-manager.target";
-        # }];
-      };
-      swaynag = {
+      wayland.windowManager.sway = {
         enable = true;
-        settings."<config>".edge = "bottom";
+        wrapperFeatures.gtk = true;
+        systemdIntegration = false;
+        extraSessionCommands = ''
+          export SDL_VIDEODRIVER=wayland
+          export QT_QPA_PLATFORM=wayland-egl
+          export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+          export _JAVA_AWT_WM_NONREPARENTING=1
+        '';
+        config = {
+          # Apps
+          # swaymsg -t get_tree
+          assigns = {
+            "2" = [{ app_id = "^firefox$"; }];
+          };
+          floating.criteria = [
+            { app_id = "^mpv$"; }
+          ];
+          # Border
+          gaps.smartBorders = "no_gaps";
+          window.hideEdgeBorders = "both";
+          # Keybindings
+          menu = "fuzzel";
+          keybindings = let mod = config.wayland.windowManager.sway.config.modifier; in lib.mkOptionDefault {
+            "${mod}+Tab" = "workspace back_and_forth";
+            # stop graphical-session.target so services like foot will not try to restart itself
+            "${mod}+Shift+e" = "exec swaynag -t warning -m 'Do you really want to exit sway?' -b 'Yes, exit sway' 'systemctl --user stop graphical-session.target; swaymsg exit'";
+            "${mod}+Shift+p" = "exec passmenu";
+            "${mod}+c" = "floating enable; move absolute position center";
+            "${mod}+Shift+l" = mkDefault "exec swaylock";
+            "Print" = "grimshot save - | swappy -f -";
+            "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+";
+            "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%-";
+            "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          };
+          # Inputs/outputs
+          input = {
+            "*" = {
+              natural_scroll = "enabled";
+            };
+            "type:keyboard" = {
+              repeat_delay = "300";
+              repeat_rate = "33";
+            };
+            "type:touchpad" = {
+              accel_profile = "adaptive";
+              drag_lock = "enabled";
+              dwt = "enabled";
+              pointer_accel = "0.28";
+              tap = "enabled";
+              scroll_method = "edge";
+              scroll_factor = "0.6";
+            };
+          };
+          output."*".bg = "#2E3440 solid_color";
+          startup = [
+            { command = "swaylock"; }
+          ];
+        };
+        swaynag = {
+          enable = true;
+          settings."<config>".edge = "bottom";
+        };
       };
-    };
 
-    programs.swaylock = {
-      enable = true;
-      settings = {
-        color = "2E3440";
-        font-size = 24;
-        ignore-empty-password = true;
-        indicator-idle-visible = true;
-        indicator-radius = 100;
-        show-failed-attempts = true;
+      programs.swaylock = {
+        enable = true;
+        settings = {
+          color = "2E3440";
+          font-size = 24;
+          ignore-empty-password = true;
+          indicator-idle-visible = true;
+          indicator-radius = 100;
+          show-failed-attempts = true;
+        };
       };
-    };
 
-  };
+    })
+
+    (mkIf cfg.xdgAutostart {
+      wayland.windowManager.sway.config.startup = [{
+        command = "systemctl --user start xdg-autostart-if-no-desktop-manager.target";
+      }];
+    })
+
+  ]);
 }
