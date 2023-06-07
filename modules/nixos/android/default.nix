bbc27c253b049c9de34447f4b8fcd4f68892356e
Zhong Jianxin <azuwis@gmail.com>
2023-06-07 22:46:14 +0800

    android: mdDoc shoud not be used here

--- modules/nixos/android/default.nix
+++ modules/nixos/android/default.nix
@@ -7,11 +7,11 @@ let
 in {
   options.my.android = {
     enable = mkEnableOption (mdDoc "android");
-    adbusers = mkEnableOption (mdDoc ''
+    adbusers = mkEnableOption ''
       Whether to add my.user to adbusers group.
 
       Adb udev rules add uaccess tag to android devices, so logined user will have permission to access Android phones, and don't need to enable this option.
-    '');
+    '';
   };
 
   config = mkIf cfg.enable (mkMerge [
