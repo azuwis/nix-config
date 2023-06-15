27ef26a5b5fd8560730f549da004e3ea43954e3e
Zhong Jianxin <azuwis@gmail.com>
2023-06-15 22:49:58 +0800

    common/alacritty: Refactor

--- darwin/default.nix
+++ darwin/default.nix
@@ -7,13 +7,13 @@
   ] ++ lib.my.getModules [ ../modules/darwin ./. ];
 
   hm.imports = [
-    ../common/alacritty.nix
     ../common/emacs
     ../common/rime
   ] ++ lib.my.getHmModules [ ./. ];
 
   my.firefox.enable = true;
 
+  hm.my.alacritty.enable = true;
   hm.my.mpv.enable = true;
   hm.my.zsh-ssh-agent.enable = true;
 }
