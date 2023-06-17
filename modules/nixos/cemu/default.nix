c2af1321af923e8950b0d043da6efe6c328fb75b
Zhong Jianxin <azuwis@gmail.com>
2023-06-17 20:15:46 +0800

    cemu: Init module

---
+++ modules/nixos/cemu/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mdDoc mkEnableOption mkIf;
+  cfg = config.my.cemu;
+in
+{
+  options.my.cemu = {
+    enable = mkEnableOption (mdDoc "cemu");
+  };
+
+  config = mkIf cfg.enable {
+    # cemu/sdl use hidraw to support game controller motion, after
+    # https://github.com/libsdl-org/SDL/pull/7697 merged, this can be disabled
+    my.steam-devices.enable = true;
+
+    hm.my.cemu.enable = true;
+  };
+}
