07850355f2b10d1794ad6a096b5453b64629a342
Zhong Jianxin <azuwis@gmail.com>
2023-06-19 20:19:50 +0800

    flakes: Export packages in overlay

--- flakes/overlays.nix
+++ flakes/overlays.nix
@@ -1,11 +1,21 @@
 { self, ... }: {
   flake.overlays.default = import ../overlay.nix;
 
-  perSystem = { system, ... }: {
-    _module.args.pkgs = import self.inputs.nixpkgs {
-      inherit system;
-      overlays = [ self.overlays.default self.inputs.nvidia-patch.overlay ];
-      config.allowUnfree = true;
+  perSystem = { lib, system, ... }:
+    let
+      pkgs = import self.inputs.nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default self.inputs.nvidia-patch.overlay ];
+        config.allowUnfree = true;
+      };
+    in
+    {
+      _module.args.pkgs = pkgs;
+
+      packages = lib.filterAttrs
+        (_: value: value ? type && value.type == "derivation")
+        (builtins.mapAttrs
+          (name: _: pkgs.${name})
+          (self.overlays.default { } { }));
     };
-  };
 }
