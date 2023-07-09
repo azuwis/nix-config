f03ba889292324696b5cb2a9c362b290169ceab2
Zhong Jianxin <azuwis@gmail.com>
2023-07-09 20:06:41 +0800

    moonlight-cemuhook: Allow config package

--- nixos/moonlight-cemuhook/default.nix
+++ nixos/moonlight-cemuhook/default.nix
@@ -1,18 +1,19 @@
 { config, lib, pkgs, ... }:
 
 let
-  inherit (lib) mdDoc mkEnableOption mkIf;
+  inherit (lib) mdDoc mkEnableOption mkIf mkPackageOptionMD;
   cfg = config.my.moonlight-cemuhook;
 in
 {
   options.my.moonlight-cemuhook = {
     enable = mkEnableOption (mdDoc "moonlight-cemuhook");
+    package = mkPackageOptionMD pkgs "moonlight-cemuhook" { };
   };
 
   config = mkIf cfg.enable {
     my.steam-devices.enable = true;
 
-    environment.systemPackages = [ pkgs.moonlight-cemuhook ];
+    environment.systemPackages = [ cfg.package ];
     networking.firewall.allowedUDPPorts = [ 26760 ];
   };
 }
