6905967da573b2773cbbdd6d80b5323364bd56b2
Zhong Jianxin <azuwis@gmail.com>
2023-07-29 21:47:39 +0800

    intel-gpu: Add intel_gpu_top wrapper

--- nixos/intel-gpu/default.nix
+++ nixos/intel-gpu/default.nix
@@ -13,6 +13,14 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.intel-gpu-tools ];
 
+    security.wrappers.intel_gpu_top = {
+      source = "${pkgs.intel-gpu-tools}/bin/intel_gpu_top";
+      owner = "root";
+      group = "wheel";
+      permissions = "0750";
+      capabilities = "cap_perfmon=ep";
+    };
+
     hardware.opengl.extraPackages = [ pkgs.intel-vaapi-driver ];
   };
 }
