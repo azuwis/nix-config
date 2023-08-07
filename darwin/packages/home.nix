392d8fa886b6f638a66a9de1bfeeb21334cc636d
Zhong Jianxin <azuwis@gmail.com>
2023-08-07 19:45:43 +0800

    darwin: Install telnet from inetutils

--- darwin/packages/home.nix
+++ darwin/packages/home.nix
@@ -19,6 +19,11 @@
       fi
       darwin-rebuild switch --flake ~/.config/nixpkgs "''$@"
     '')
+    (pkgs.runCommand "telnet-0.0.0" { } ''
+      mkdir -p $out/bin $out/share/man/man1/
+      ln -s ${pkgs.inetutils}/bin/telnet $out/bin/
+      ln -s ${pkgs.inetutils}/share/man/man1/telnet.1.gz $out/share/man/man1/
+    '')
     android-file-transfer
     android-tools
     coreutils-full
