cb84c736dfd17949d989ceedebd6480816d50244
Zhong Jianxin <azuwis@gmail.com>
2023-08-14 20:18:45 +0800

    fcitx5: Set theme to Nord-Dark

--- nixos/fcitx5/home.nix
+++ nixos/fcitx5/home.nix
@@ -4,6 +4,14 @@ let
   inherit (lib) mdDoc mkEnableOption mkIf;
   cfg = config.my.fcitx5;
 
+  fcitx5-nord = pkgs.fetchFromGitHub rec {
+    name = "${repo}-${rev}";
+    owner = "tonyfettes";
+    repo = "fcitx5-nord";
+    rev = "bdaa8fb723b8d0b22f237c9a60195c5f9c9d74d1";
+    hash = "sha256-qVo/0ivZ5gfUP17G29CAW0MrRFUO0KN1ADl1I/rvchE=";
+  };
+
 in
 {
   options.my.fcitx5 = {
@@ -15,6 +23,7 @@ in
     #   ${fcitx5}/bin/fcitx5-remote -r || true
     # '';
     xdg.configFile.fcitx5.source = ./config;
+    xdg.dataFile."fcitx5/themes".source = fcitx5-nord;
     wayland.windowManager.sway.config.startup = [{ command = "fcitx5"; }];
     xsession.windowManager.i3.config.startup = [{ command = "fcitx5"; }];
   };
