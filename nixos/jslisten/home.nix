12cf49e84b0940e87bae94f606a50e3d84270b9e
Zhong Jianxin <azuwis@gmail.com>
2023-08-21 20:59:13 +0800

    jslisten: `program` only accept 100 chars, shorten them by wrapping

--- nixos/jslisten/home.nix
+++ nixos/jslisten/home.nix
@@ -14,50 +14,57 @@ in
 
     settings = mkOption {
       type = ini.type;
-      default = {
-        # PS+L
-        Left = {
-          button1 = 10;
-          button2 = 4;
-          program = if sway then "wtype -k left &" else "xdotool key Left &";
+      default =
+        let
+          # `program` only accept 100 chars, shorten them by wrapping
+          sway-run = args: "${pkgs.writeShellScript (builtins.elemAt (builtins.split " " args) 0) ''
+            exec ${./scripts}/sway-run ${args}
+          ''} &";
+        in
+        {
+          # PS+L
+          Left = {
+            button1 = 10;
+            button2 = 4;
+            program = if sway then "wtype -k left &" else "xdotool key Left &";
+          };
+          # PS+R
+          Right = {
+            button1 = 10;
+            button2 = 5;
+            program = if sway then "wtype -k right &" else "xdotool key Right &";
+          };
+          # PS+ZR
+          Space = {
+            button1 = 10;
+            button2 = 7;
+            program = if sway then "wtype -k space &" else "xdotool key space &";
+          };
+          # PS+△
+          TotK = {
+            button1 = 10;
+            button2 = 2;
+            program = sway-run "TotK class=yuzu yuzu -f -g $HOME/Games/Switch/TotK.nsp";
+          };
+          # PS+□
+          Yuzu = {
+            button1 = 10;
+            button2 = 3;
+            program = sway-run "Yuzu class=yuzu yuzu";
+          };
+          # PS+○
+          BotW = {
+            button1 = 10;
+            button2 = 1;
+            program = if sway then sway-run "BotW app_id=info.cemu.Cemu cemu --fullscreen --title-id 00050000101c9300" else "${./scripts}/i3-botw &";
+          };
+          # PS+X
+          NieR = {
+            button1 = 10;
+            button2 = 0;
+            program = sway-run "NieR class=yuzu yuzu -f -g $HOME/Games/Switch/NieR.nsp";
+          };
         };
-        # PS+R
-        Right = {
-          button1 = 10;
-          button2 = 5;
-          program = if sway then "wtype -k right &" else "xdotool key Right &";
-        };
-        # PS+ZR
-        Space = {
-          button1 = 10;
-          button2 = 7;
-          program = if sway then "wtype -k space &" else "xdotool key space &";
-        };
-        # PS+△
-        TotK = {
-          button1 = 10;
-          button2 = 2;
-          program = "${./scripts}/sway-run TotK class=yuzu yuzu -f -g $HOME/Games/Switch/TotK.nsp &";
-        };
-        # PS+□
-        Yuzu = {
-          button1 = 10;
-          button2 = 3;
-          program = "${./scripts}/sway-run Yuzu class=yuzu yuzu &";
-        };
-        # PS+○
-        BotW = {
-          button1 = 10;
-          button2 = 1;
-          program = if sway then "${./scripts}/sway-botw BotW app_id=info.cemu.Cemu cemu --fullscreen --title-id 00050000101c9300 &" else "${./scripts}/i3-botw &";
-        };
-        # PS+X
-        NieR = {
-          button1 = 10;
-          button2 = 0;
-          program = "${./scripts}/sway-run NieR class=yuzu yuzu -f -g $HOME/Games/Switch/NieR.nsp &";
-        };
-      };
     };
   };
 
