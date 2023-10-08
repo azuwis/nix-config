a30a8dd8322386212537f6c10b27633d96c74c16
Zhong Jianxin <azuwis@gmail.com>
2023-10-08 19:43:34 +0800

    sketchybar: Use networksetup to get wifi essid
    To make it work for macOS 14

--- darwin/sketchybar/scripts/wifi.sh
+++ darwin/sketchybar/scripts/wifi.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-if [ -n "$INFO" ]
-then
-  WIFI_ICON="􀙇"
-  WIFI_PADDING=6
-else
-  WIFI_ICON="􀙈"
-  WIFI_PADDING=0
-fi
+get_wifi() {
+  local wifi test
+  wifi=$(networksetup -getairportnetwork en0)
+  read -r _ _ test WIFI_LABEL <<< "$wifi"
+  if [ "$test" = "Network:" ]
+  then
+      WIFI_ICON="􀙇"
+      WIFI_PADDING=6
+  else
+      WIFI_LABEL=""
+      WIFI_ICON="􀙈"
+      WIFI_PADDING=0
+  fi
+}
 
-sketchybar --set wifi icon="$WIFI_ICON" icon.padding_right="$WIFI_PADDING" label="$INFO"
+get_wifi
+
+sketchybar --set wifi icon="$WIFI_ICON" icon.padding_right="$WIFI_PADDING" label="$WIFI_LABEL"
