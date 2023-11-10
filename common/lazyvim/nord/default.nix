edaf20a984e5e50fc91cc7f9c698d5f2d07e0ece
Zhong Jianxin <azuwis@gmail.com>
2023-11-10 20:48:41 +0800

    lazyvim: Use gbprod/nord.nvim for better neogit support

--- common/lazyvim/nord/default.nix
+++ common/lazyvim/nord/default.nix
@@ -3,6 +3,18 @@
 let
   inherit (lib) mkEnableOption;
   cfg = config.my.lazyvim.nord;
+
+  nord-nvim = pkgs.vimUtils.buildVimPlugin {
+    pname = "nord.nvim";
+    version = "2023-11-10";
+    src = pkgs.fetchFromGitHub {
+      owner = "gbprod";
+      repo = "nord.nvim";
+      rev = "74b5032dd7a600b849eab769b52401bccb6d8d41";
+      sha256 = "sha256-njZRvB6pcRQcrfGrcOAX5z/VqGtdIKgzEBUjEbvG6pI=";
+    };
+    meta.homepage = "https://github.com/gbprod/nord.nvim/";
+  };
 in
 {
   options.my.lazyvim.nord = {
@@ -10,7 +22,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    my.lazyvim.extraPlugins = with pkgs.vimPlugins; [
+    my.lazyvim.extraPlugins = [
       nord-nvim
     ];
 
