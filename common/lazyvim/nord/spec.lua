c2faf36677d61d1bdd261c7c75dd46ce1d960ebf
Zhong Jianxin <azuwis@gmail.com>
2023-11-17 20:01:30 +0800

    neovim/lazyvim: Set nord TreesitterContextBottom highlight

--- common/lazyvim/nord/spec.lua
+++ common/lazyvim/nord/spec.lua
@@ -1,6 +1,14 @@
 return {
   {
     "gbprod/nord.nvim",
+    opts = {
+      on_highlights = function(highlights, colors)
+        highlights.TreesitterContextBottom = {
+          underline = true,
+          sp = colors.polar_night.brightest,
+        }
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
