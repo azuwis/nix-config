258546aa8c16dd458a4eedd123c08214771a3a4a
Zhong Jianxin <azuwis@gmail.com>
2023-11-17 20:39:48 +0800

    lazyvim: Override wrap_spell augroup

---
+++ common/lazyvim/custom/config/autocmds.lua
@@ -0,0 +1,14 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- override wrap_spell, remove markdown
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "NeogitCommitMessage" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
