8909fadf5f156ce5be3a797b131f32701a5a4155
Zhong Jianxin <azuwis@gmail.com>
2023-11-27 20:19:37 +0800

    lazyvim: Enable luasnip snipmate

--- common/lazyvim/custom/spec.lua
+++ common/lazyvim/custom/spec.lua
@@ -25,6 +25,14 @@ return {
   },
 
   {
+    "L3MON4D3/LuaSnip",
+    optional = true,
+    config = function()
+      require("luasnip.loaders.from_snipmate").lazy_load()
+    end,
+  },
+
+  {
     "echasnovski/mini.indentscope",
     optional = true,
     opts = {
