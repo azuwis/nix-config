dbea11b893dac87c97bcebfc7e891ccd5b1b2815
Zhong Jianxin <azuwis@gmail.com>
2023-11-28 20:53:30 +0800

    lazyvim: Simplify keymaps.lua

--- common/lazyvim/custom/config/keymaps.lua
+++ common/lazyvim/custom/config/keymaps.lua
@@ -1,7 +1,4 @@
-vim.keymap.set("n", "<leader>fs", "<cmd>update<cr>", { desc = "Save file if modified" })
-vim.keymap.set(
-  "n",
-  "<leader>fS",
-  "<cmd>execute ':silent w !sudo tee % > /dev/null' | :edit!<cr>",
-  { desc = "Sudo save" }
-)
+local map = vim.keymap.set
+
+map("n", "<leader>fs", "<cmd>update<cr>", { desc = "Save file if modified" })
+map("n", "<leader>fS", "<cmd>execute ':silent w !sudo tee % > /dev/null' | :edit!<cr>", { desc = "Sudo save" })
