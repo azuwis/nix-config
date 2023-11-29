6bc412af55f8a4e5987896a7c2da169738c9fbae
Zhong Jianxin <azuwis@gmail.com>
2023-11-29 20:47:06 +0800

    lazyvim: Auto import */default.nix

--- common/lazyvim/home.nix
+++ common/lazyvim/home.nix
@@ -1,16 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    ./base
-    ./ansible
-    ./bash
-    ./custom
-    ./helm
-    ./neogit
-    ./nix
-    ./nord
-    ./terraform
-    ./update-nix-fetchgit
-  ];
+  imports = lib.my.getModules [ ./. ];
 }
