259f3faf068589a7303978a7965a8f4982882919
Zhong Jianxin <azuwis@gmail.com>
2023-12-03 19:34:59 +0800

    hass: Use customLovelaceModules to setup simple-thermostat

--- nixos/hass/simple-thermostat.nix
+++ nixos/hass/simple-thermostat.nix
@@ -1,4 +1,30 @@
-import ./lovelace-resource.nix {
-  url = "https://github.com/nervetattoo/simple-thermostat/releases/download/v2.5.0/simple-thermostat.js";
-  sha256 = "sha256-mC7/6MsVrLkNgkls6VDAaCgHTzw5noYV+VOeCy6y+Xo=";
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.my.hass;
+
+  module = with pkgs; stdenv.mkDerivation (finalAttrs: {
+    pname = "simple-thermostat";
+    version = "2.5.0";
+    src = fetchurl {
+      url = "https://github.com/nervetattoo/simple-thermostat/releases/download/v${finalAttrs.version}/simple-thermostat.js";
+      hash = "sha256-mC7/6MsVrLkNgkls6VDAaCgHTzw5noYV+VOeCy6y+Xo=";
+    };
+    dontUnpack = true;
+    installPhase = ''
+      mkdir $out
+      cp $src $out/simple-thermostat.js
+    '';
+    passthru.entrypoint = "simple-thermostat.js";
+  });
+in
+{
+  options.my.hass = {
+    simple-thermostat = mkEnableOption "hass" // { default = true; };
+  };
+
+  config = mkIf (cfg.enable && cfg.mini-media-player) {
+    services.home-assistant.customLovelaceModules = [ module ];
+  };
 }
