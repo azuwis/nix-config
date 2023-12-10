88c9e7da5bc7c6d96d6b20077d2bf5728ff7ea69
Zhong Jianxin <azuwis@gmail.com>
2023-12-10 19:29:24 +0800

    hass: Use customLovelaceModules to setup zigbee2mqtt-networkmap

--- nixos/hass/zigbee2mqtt-networkmap.nix
+++ nixos/hass/zigbee2mqtt-networkmap.nix
@@ -1,13 +1,15 @@
-import ./lovelace-resource.nix {
-  url = "https://github.com/azuwis/zigbee2mqtt-networkmap/releases/download/v0.7.0/zigbee2mqtt-networkmap.js";
-  sha256 = "sha256-OB2MOzFtSwzmW1b1wxBrWvW2GXdzf80vD/p+t5TN2jE=";
-  cfg = {
-    services.home-assistant.config.mqtt.sensor = [{
-      name = "Zigbee2mqtt Networkmap";
-      state_topic = "zigbee2mqtt/bridge/response/networkmap";
-      value_template = ''{{ now().strftime("%Y-%m-%d %H:%M:%S") }}'';
-      json_attributes_topic = "zigbee2mqtt/bridge/response/networkmap";
-      json_attributes_template = "{{ value_json.data.value | tojson }}";
-    }];
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.my.hass;
+in
+{
+  options.my.hass = {
+    zigbee2mqtt-networkmap = mkEnableOption "hass" // { default = true; };
+  };
+
+  config = mkIf (cfg.enable && cfg.zigbee2mqtt-networkmap) {
+    services.home-assistant.customLovelaceModules = [ pkgs.home-assistant-custom-lovelace-modules.zigbee2mqtt-networkmap ];
   };
 }
