16bab6a96f6e1124d1de2ef74ce037a66535d4a5
Zhong Jianxin <azuwis@gmail.com>
2023-12-10 19:50:53 +0800

    hass: Use customComponents to setup smartir

--- nixos/hass/smartir.nix
+++ nixos/hass/smartir.nix
@@ -3,14 +3,6 @@
 let
   inherit (lib) mdDoc mkEnableOption mkIf;
   cfg = config.my.hass;
-
-  component = pkgs.fetchFromGitHub rec {
-    name = "${owner}-${repo}-${rev}";
-    owner = "smartHomeHub";
-    repo = "SmartIR";
-    rev = "1f8e95b8a6e062038e5698ed438ecee4321b496f";
-    sha256 = "sha256-/jnEkpN2TH8QhkwN4WmaGFjfY4hWzHTZ29vhnaD22RM=";
-  };
 in
 {
   options.my.hass = {
@@ -18,15 +10,7 @@ in
   };
 
   config = mkIf (cfg.enable && cfg.smartir) {
-    hass.file."custom_components/smartir" = {
-      source = "${component}/custom_components/smartir";
-      recursive = true;
-    };
-    hass.file."custom_components/smartir/codes".source = "${component}/codes";
-
-    services.home-assistant.extraPackages = ps: with ps; [
-      broadlink
-    ];
+    services.home-assistant.customComponents = [ pkgs.smartir ];
 
     services.home-assistant.config = {
       smartir.check_updates = false;
