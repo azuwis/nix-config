634e09a506248f74630616bf4705fde79dde57f9
Zhong Jianxin <azuwis@gmail.com>
2023-12-21 19:40:00 +0800

    hass: Add missing config.mqtt.sensor to zigbee2mqtt-networkmap

--- nixos/hass/zigbee2mqtt-networkmap.nix
+++ nixos/hass/zigbee2mqtt-networkmap.nix
@@ -10,6 +10,14 @@ in
   };
 
   config = mkIf (cfg.enable && cfg.zigbee2mqtt-networkmap) {
+    services.home-assistant.config.mqtt.sensor = [{
+      name = "Zigbee2mqtt Networkmap";
+      state_topic = "zigbee2mqtt/bridge/response/networkmap";
+      value_template = ''{{ now().strftime("%Y-%m-%d %H:%M:%S") }}'';
+      json_attributes_topic = "zigbee2mqtt/bridge/response/networkmap";
+      json_attributes_template = "{{ value_json.data.value | tojson }}";
+    }];
+
     services.home-assistant.customLovelaceModules = [ pkgs.home-assistant-custom-lovelace-modules.zigbee2mqtt-networkmap ];
   };
 }
