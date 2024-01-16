e32a5b0a257bb0865dd481451b332909d19eb42f
Zhong Jianxin <azuwis@gmail.com>
2024-01-16 21:59:35 +0800

    nixos: mkNixos allow override nixpkgs

--- flakes/nixos.nix
+++ flakes/nixos.nix
@@ -1,31 +1,37 @@
 { self, withSystem, ... }:
 
 let
-  mkNixos = { system ? "x86_64-linux", config ? { }, overlays ? [ ], modules ? [ ] }:
+  mkNixos =
+    { system ? "x86_64-linux"
+    , nixpkgs ? self.inputs.nixpkgs
+    , config ? { }
+    , overlays ? [ ]
+    , modules ? [ ]
+    }:
     withSystem system ({ lib, pkgs, system, ... }:
-      let
-        customPkgs = import self.inputs.nixpkgs (lib.recursiveUpdate
-          {
-            inherit system;
-            overlays = [ self.overlays.default ] ++ overlays;
-            config.allowUnfree = true;
-          }
-          {
-            inherit config;
-          }
-        );
-      in
-      self.inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit lib;
-          inputs = self.inputs;
-          pkgs = if (overlays != [ ] || config != { }) then customPkgs else pkgs;
-        };
-        modules = [
-          ../nixos
-        ] ++ modules;
-      });
+    let
+      customPkgs = import nixpkgs (lib.recursiveUpdate
+        {
+          inherit system;
+          overlays = [ self.overlays.default ] ++ overlays;
+          config.allowUnfree = true;
+        }
+        {
+          inherit config;
+        }
+      );
+    in
+    nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit lib;
+        inputs = self.inputs;
+        pkgs = if (overlays != [ ] || config != { }) then customPkgs else pkgs;
+      };
+      modules = [
+        ../nixos
+      ] ++ modules;
+    });
 in
 {
   flake.nixosConfigurations = {
