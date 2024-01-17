d619e2626c401b3bc8d3bc0c9468a313b1a057df
Zhong Jianxin <azuwis@gmail.com>
2024-01-17 19:51:13 +0800

    nixos: Add nix-builder module

---
+++ nixos/nix-builder/default.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.my.nix-builder;
+in
+{
+  options.my.nix-builder = {
+    enable = mkEnableOption "nix-builder";
+  };
+
+  config = mkIf cfg.enable {
+    nix.settings.trusted-users = [ "nix-ssh" ];
+    nix.sshServe = {
+      enable = true;
+      write = true;
+      keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSWc7PHn11yf+fuveTjzhcQpwgeWavw3YB4FVbj/d9t" ];
+      protocol = "ssh-ng";
+    };
+  };
+}
