8977158f1e7215e1d430af9d496aacd462257ac7
Zhong Jianxin <azuwis@gmail.com>
2024-01-31 19:59:56 +0800

    grub-reboot-menu: Use id to select entry

--- pkgs/grub-reboot-menu/grub-reboot-menu.sh
+++ pkgs/grub-reboot-menu/grub-reboot-menu.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
-menuentry='menuentry "(.*)"'
-submenu='submenu "(.*)"'
+menuentry="menuentry [\"'](.*)[\"']"
+submenu="submenu [\"'](.*)[\"']"
 
+id=()
 title=()
 open="false"
+last="-1"
 while read -r line
 do
   if [[ "$line" =~ $menuentry ]]
   then
     open="true"
+    id+=("$((last+1))")
     title+=("${BASH_REMATCH[1]}")
-    (IFS='>'; echo "${title[*]}")
+    (IFS='>'; echo "${id[*]} ${title[*]}")
   elif [[ "$line" =~ $submenu ]]
   then
     open="true"
+    id+=("$((last+1))")
+    last="-1"
     title+=("${BASH_REMATCH[1]}")
   elif [[ "$line" =~ "}" ]] && [ "$open" = "true" ]
   then
+    last="${id[-1]}"
+    unset "id[-1]"
     unset "title[-1]"
   fi
-done </boot/grub/grub.cfg | fzf | xargs --max-lines=1 --delimiter='\n' --no-run-if-empty --verbose grub-reboot
+done </boot/grub/grub.cfg | fzf --bind 'enter:become(echo {1})' | xargs --max-lines=1 --delimiter='\n' --no-run-if-empty --verbose grub-reboot
