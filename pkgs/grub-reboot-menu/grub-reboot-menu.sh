1cef3b4cc00109b582af739a2f11af5e21a0d4f9
Zhong Jianxin <azuwis@gmail.com>
2024-03-12 19:56:34 +0800

    grub-reboot-menu: Better var name `bracket`

--- pkgs/grub-reboot-menu/grub-reboot-menu.sh
+++ pkgs/grub-reboot-menu/grub-reboot-menu.sh
@@ -5,23 +5,23 @@ submenu="submenu [\"'](.*)[\"']"
 
 id=()
 title=()
-open="false"
+bracket="close"
 last="-1"
 while read -r line
 do
   if [[ "$line" =~ $menuentry ]]
   then
-    open="true"
+    bracket="open"
     id+=("$((last+1))")
     title+=("${BASH_REMATCH[1]}")
     (IFS='>'; echo "${id[*]} ${title[*]}")
   elif [[ "$line" =~ $submenu ]]
   then
-    open="true"
+    bracket="open"
     id+=("$((last+1))")
     last="-1"
     title+=("${BASH_REMATCH[1]}")
-  elif [[ "$line" =~ "}" ]] && [ "$open" = "true" ]
+  elif [[ "$line" =~ "}" ]] && [ "$bracket" = "open" ]
   then
     last="${id[-1]}"
     unset "id[-1]"
