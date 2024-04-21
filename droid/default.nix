4a6f175fe2a84504209834f90481c059d18e4900
Zhong Jianxin <azuwis@gmail.com>
2024-04-21 19:58:50 +0800

    droid: Remove workaround, patch accepted upstream

--- droid/default.nix
+++ droid/default.nix
@@ -8,7 +8,4 @@
   hm.imports = lib.my.getHmModules [ ./. ];
 
   hm.my.zsh-ssh-agent.enable = true;
-
-  # https://github.com/nix-community/nix-on-droid/pull/316
-  home-manager.extraSpecialArgs = { lib = import (home-manager-path + "/modules/lib/stdlib-extended.nix") lib; };
 }
