8acadb01f3775578103f864865780e4a2ff361d1
Zhong Jianxin <azuwis@gmail.com>
2024-04-23 19:04:27 +0800

    lazyvim: Disable some extra terraform plugins

--- common/lazyvim/terraform/spec.lua
+++ common/lazyvim/terraform/spec.lua
@@ -1,3 +1,5 @@
 return {
   { import = "lazyvim.plugins.extras.lang.terraform" },
+  { "ANGkeith/telescope-terraform-doc.nvim", enabled = false },
+  { "cappyzawa/telescope-terraform.nvim", enabled = false },
 }
