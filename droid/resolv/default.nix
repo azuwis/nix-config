fa94100d8ddb847c5856c1e3a3c14f2fc2e8e9e1
Zhong Jianxin <azuwis@gmail.com>
2024-05-05 20:25:04 +0800

    droid: Add resolv module

---
+++ droid/resolv/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  # https://github.com/termux/termux-packages/issues/1174
+  environment.etc."resolv.conf".enable = false;
+  environment.packages = [
+    (pkgs.runCommand "resolvconf" { } ''
+      mkdir -p $out/bin
+      install -m 0755 ${./resolvconf} $out/bin/resolvconf
+    '')
+  ];
+  hm.programs.zsh.initExtra = ''
+    resolvconf
+  '';
+}
--- droid/system/default.nix
+++ droid/system/default.nix
@@ -7,8 +7,6 @@
 
 {
   environment.etcBackupExtension = ".bak";
-  # https://github.com/termux/termux-packages/issues/1174
-  environment.etc."resolv.conf".enable = false;
   environment.motd = "";
   nix.extraOptions = ''
     experimental-features = nix-command flakes
