c7858dde81ce1abcee36f39710f7ed205b014a1e
Zhong Jianxin <azuwis@gmail.com>
2024-05-16 21:02:54 +0800

    registry: Guard nixos module using my.registry.enable and disable it by default
    nixos already did similar things by default, see <nixpkgs>/nixos/modules/misc/nixpkgs-flake.nix

--- common/registry/default.nix
+++ common/registry/default.nix
@@ -7,6 +7,9 @@
 }:
 
 let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.my.registry;
+
   flakes = lib.filterAttrs (
     name: value:
     builtins.elem name [
@@ -16,13 +19,19 @@ let
   ) inputs;
 in
 {
-  # https://github.com/gytis-ivaskevicius/flake-utils-plus/blob/master/lib/options.nix
-  environment.etc = lib.mapAttrs' (name: value: {
-    name = "nix/inputs/${name}";
-    value = {
-      source = value.outPath;
-    };
-  }) flakes;
-  nix.nixPath = [ "/etc/nix/inputs" ];
-  nix.registry = builtins.mapAttrs (name: value: { flake = value; }) flakes;
+  options.my.registry = {
+    enable = mkEnableOption "registry";
+  };
+
+  config = mkIf cfg.enable {
+    # https://github.com/gytis-ivaskevicius/flake-utils-plus/blob/master/lib/options.nix
+    environment.etc = lib.mapAttrs' (name: value: {
+      name = "nix/inputs/${name}";
+      value = {
+        source = value.outPath;
+      };
+    }) flakes;
+    nix.nixPath = [ "/etc/nix/inputs" ];
+    nix.registry = builtins.mapAttrs (name: value: { flake = value; }) flakes;
+  };
 }
