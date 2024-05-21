9e044698efe887ef9c03b07577ae051b7f3ca0ba
Zhong Jianxin <azuwis@gmail.com>
2024-05-21 19:59:24 +0800

    my: Inherit inputs.my domain

--- common/my/default.nix
+++ common/my/default.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   lib,
   pkgs,
@@ -19,6 +20,7 @@ with lib;
   };
   config = {
     my = {
+      inherit (inputs.my) domain;
       user = "azuwis";
       name = "Zhong Jianxin";
       email = "azuwis@gmail.com";
@@ -26,24 +28,6 @@ with lib;
       keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBa4stB3iashVWVH3XA4CgciZ7UvnvMCNyc7nkKqpiqMs1TQNg+dayNv+rX6zJh+WEWL4bZ5SNr+cnlHpF6wCcZlicFh7vli4FKK0DgmcASJPRHqN98dBQ9aH+4ZIUcjuPaAPeA/0EO+o8RVNTCnOD3QtJjNPh6Kr1FW2THE+AMpdotnS5pUpH4hrFaaEhRkvOwUeaJ/kxs+KYqPTyq4pNyoSuG6aTzjwCpTJB1tbma+7/t8809Cpy6q48jh2VhB6Sb6sxcFi3KMo/i6I7SpmhlgPTEBDDtloQaP7uhgtkP7UTCldUU2UV8BrAXIRFHBccan1La2MJ3Z5vrr67zSkb"
       ];
-      domain =
-        let
-          domain = "tube.demon.xyz";
-        in
-        concatMapStrings (x: elemAt (stringToCharacters domain) x) [
-          7
-          8
-          13
-          10
-          5
-          12
-          9
-          1
-          4
-          9
-          6
-          0
-        ];
       ca = ./ca.crt;
     };
   };
