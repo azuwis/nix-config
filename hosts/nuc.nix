782697d0e432126d3251b4bf214463d5346d481d
Zhong Jianxin <azuwis@gmail.com>
2024-05-21 20:05:27 +0800

    yambar: Refactor using programs.yambar from home-manager

--- hosts/nuc.nix
+++ hosts/nuc.nix
@@ -51,7 +51,7 @@
   hm.my.swayidle.enable = false;
 
   # workaround for yambar crash, https://codeberg.org/dnkl/yambar/issues/300
-  hm.my.yambar.package = pkgs.yambar.overrideAttrs (old: {
+  hm.programs.yambar.package = pkgs.yambar.overrideAttrs (old: {
     src = old.src.override {
       rev = "0bea49b75e2cf3fe347bce3447e9dfbaaaaf2c8d";
       hash = "sha256-hX+qCDhSQ7DKxsiUp5RZoArDE7M/MVckKHIPS5QmVhs=";
--- nixos/yambar/home.nix
+++ nixos/yambar/home.nix
@@ -6,28 +6,162 @@
 }:
 
 let
-  inherit (lib) mkEnableOption mkPackageOption mkIf;
+  inherit (lib) mkEnableOption mkIf;
   cfg = config.my.yambar;
+
   # ifaces = builtins.attrNames config.networking.interfaces;
   # ens = builtins.filter (name: (builtins.match "^e.*" name) != null) ifaces;
   # en = if ens != [] then builtins.elemAt ens 0 else null;
-  configFile = pkgs.substituteAll {
-    src = ./config.yml;
-    name = "yambar.yml";
-    scripts = ./scripts;
-    msgcmd = if config.my.sway.enable then "swaymsg" else "i3-msg";
-  };
+  msgcmd = if config.my.sway.enable then "swaymsg" else "i3-msg";
 in
 {
   options.my.yambar = {
     enable = mkEnableOption "yambar";
-    package = mkPackageOption pkgs "yambar" { };
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
+    programs.yambar.enable = true;
 
-    xdg.configFile."yambar/config.yml".source = configFile;
+    programs.yambar.settings.bar = {
+      location = "top";
+      height = 26;
+      background = "2E3440FF";
+      font = "monospace:pixelsize=16";
+      spacing = 6;
+      margin = 12;
+      left = [
+        {
+          i3 = {
+            sort = "ascending";
+            content."".map =
+              let
+                default = {
+                  text = "{name}";
+                  margin = 6;
+                  on-click = "${msgcmd} --quiet workspace {name}";
+                };
+                focused.foreground = "EBCB8BFF";
+                urgent = {
+                  foreground = "000000FF";
+                  deco.stack = [ { background.color = "BF616AFF"; } ];
+                };
+                underline.underline = {
+                  size = 2;
+                  color = "81A1C1FF";
+                };
+              in
+              {
+                default.string = default;
+                conditions = {
+                  "state == focused".string = default // focused;
+                  "state == urgent".string = default // urgent;
+                };
+              };
+          };
+        }
+      ];
+      center = [
+        {
+          foreign-toplevel.content.map.conditions = {
+            "~activated".empty = { };
+            activated.string.text = "{app-id}";
+          };
+        }
+      ];
+      right = [
+        {
+          network = {
+            poll-interval = 10000;
+            content.map.conditions."name == eno1 || name == wlo1" = [
+              {
+                string = {
+                  text = "";
+                  right-margin = 4;
+                };
+              }
+              {
+                map = {
+                  default.string.text = "{dl-speed:kib:.0}K";
+                  conditions."dl-speed >= 1048576".string.text = "{dl-speed:mib:.1}M";
+                };
+              }
+              {
+                string = {
+                  text = "";
+                  left-margin = 6;
+                  right-margin = 4;
+                };
+              }
+              {
+                map = {
+                  default.string.text = "{ul-speed:kib:.0}K";
+                  conditions."ul-speed >= 1048576".string.text = "{ul-speed:mib:.1}M";
+                };
+              }
+            ];
+          };
+        }
+        {
+          cpu = {
+            poll-interval = 10000;
+            content.map.conditions."id < 0" = [
+              {
+                string = {
+                  text = "󰓅";
+                  right-margin = 4;
+                };
+              }
+              { string.text = "{cpu}%"; }
+            ];
+          };
+        }
+        {
+          mem = {
+            poll-interval = 10000;
+            content = [
+              {
+                string = {
+                  text = "󰍛";
+                  right-margin = 4;
+                };
+              }
+              { string.text = "{free:gb}G"; }
+            ];
+          };
+        }
+        {
+          pulse.content = [
+            {
+              map.conditions = {
+                sink_muted.string = {
+                  text = "󰖁";
+                  right-margin = 4;
+                };
+                "~sink_muted".string = {
+                  text = "󰕾";
+                  right-margin = 4;
+                };
+              };
+            }
+            { string.text = "{sink_percent}"; }
+          ];
+        }
+        {
+          clock = {
+            date-format = "%a %m-%d";
+            content = [
+              {
+                string = {
+                  text = "";
+                  right-margin = 4;
+                };
+              }
+              { string.text = "{date} {time}"; }
+            ];
+          };
+        }
+      ];
+    };
 
     wayland.windowManager.sway.config = {
       bars = [ ];
