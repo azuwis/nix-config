714c058db44ecfa37d7459d05aff6b23c5890a93
Zhong Jianxin <azuwis@gmail.com>
2024-05-22 20:29:25 +0800

    registry: Follow nixos to set nix.nixPath and nix.registry

--- common/registry/default.nix
+++ common/registry/default.nix
@@ -9,32 +9,20 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.my.registry;
-
-  flakes = lib.filterAttrs (
-    name: value:
-    builtins.elem name [
-      "nixpkgs"
-      "home-manager"
-    ]
-  ) inputs;
 in
 {
   options.my.registry = {
     enable = mkEnableOption "registry" // {
-      # https://github.com/NixOS/nixpkgs/pull/254405/commits/e456032addae76701eb17e6c03fc515fd78ad74f
-      default = !(config.nixpkgs ? flake && config.nixpkgs.flake.source == null);
+      default = true;
     };
   };
 
   config = mkIf cfg.enable {
-    # https://github.com/gytis-ivaskevicius/flake-utils-plus/blob/master/lib/options.nix
-    environment.etc = lib.mapAttrs' (name: value: {
-      name = "nix/inputs/${name}";
-      value = {
-        source = value.outPath;
-      };
-    }) flakes;
-    nix.nixPath = [ "/etc/nix/inputs" ];
-    nix.registry = builtins.mapAttrs (name: value: { flake = value; }) flakes;
+    # <nixpkgs>/nixos/modules/misc/nixpkgs-flake.nix
+    nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
+    nix.registry.nixpkgs.to = {
+      type = "path";
+      path = inputs.nixpkgs.outPath;
+    };
   };
 }
