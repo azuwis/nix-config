1d0e2724a72af18a5c0aac2ddcc3a5f287ffe521
Zhong Jianxin <azuwis@gmail.com>
2024-05-22 20:50:45 +0800

    vimfx: 0.26.4 -> 0.27.2

--- pkgs/by-name/vi/vimfx/package.nix
+++ pkgs/by-name/vi/vimfx/package.nix
@@ -2,10 +2,10 @@
 
 buildFirefoxXpiAddon rec {
   pname = "vimfx";
-  version = "0.26.4";
+  version = "0.27.2";
   addonId = "VimFx-unlisted@akhodakivskiy.github.com";
   url = "https://github.com/akhodakivskiy/VimFx/releases/download/v${version}/VimFx.xpi";
-  sha256 = "sha256-8uVuk/oqOY6zE640GQ7nzBLGcxLvCHToqPLjuxdS428=";
+  sha256 = "sha256-IiFnbp5nrfSgpyfnXt68F335cgpAvR8lXTsIos4D7Mk=";
   meta = {
     homepage = "https://github.com/akhodakivskiy/VimFx";
     description = "Vim keyboard shortcuts for Firefox";
