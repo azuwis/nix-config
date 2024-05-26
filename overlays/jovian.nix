fa65482085e514146d619ff09fe1b501e3f08021
Zhong Jianxin <azuwis@gmail.com>
2024-05-26 20:02:10 +0800

    jovian: Add vimPlugins overlay

--- overlays/jovian.nix
+++ overlays/jovian.nix
@@ -1,4 +1,20 @@
 self: super: {
+  # Workaround for vimPlugins.nvim-snippets not in jovia/nixpkgs yet.
+  # Remove it when jovian upgrades next time.
+  vimPlugins = super.vimPlugins // {
+    nvim-snippets = self.vimUtils.buildVimPlugin {
+      pname = "nvim-snippets";
+      version = "2024-05-25";
+      src = self.fetchFromGitHub {
+        owner = "garymjr";
+        repo = "nvim-snippets";
+        rev = "343c687b463ff0c71febd4582076fa5b96214475";
+        sha256 = "16r9vl52yf6r85x80jb53sx810vb7z08rxq2bq6qsjwh18ya8z0n";
+      };
+      meta.homepage = "https://github.com/garymjr/nvim-snippets/";
+    };
+  };
+
   # Steam (electron) have issues with variable noto cjk fonts
   # https://github.com/NixOS/nixpkgs/issues/178121
   # noto-fonts-cjk-sans = super.noto-fonts-cjk-sans.overrideAttrs (old: {
