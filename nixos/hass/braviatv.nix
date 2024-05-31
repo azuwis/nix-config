49adf89e86ac27995b5b29ce7ea2cb9e0584c71e
Zhong Jianxin <azuwis@gmail.com>
2024-05-31 23:45:48 +0800

    hass: Also check nightlight for TV auto

--- nixos/hass/braviatv.nix
+++ nixos/hass/braviatv.nix
@@ -24,12 +24,28 @@ in
         trigger:
           - platform: state
             entity_id: light.living_room
+            from: "on"
+            to: "off"
+          - platform: state
+            entity_id: light.living_room_nightlight
+            from: "on"
             to: "off"
         condition:
           - condition: sun
             after: sunset
             before: sunrise
         action:
+          - service: homeassistant.update_entity
+            data:
+              entity_id:
+                - light.living_room
+                - light.living_room_nightlight
+          - condition: state
+            entity_id: light.living_room
+            state: "off"
+          - condition: state
+            entity_id: light.living_room_nightlight
+            state: "off"
           - service: media_player.turn_off
             data:
               entity_id: media_player.sony_kdl_55w800b
