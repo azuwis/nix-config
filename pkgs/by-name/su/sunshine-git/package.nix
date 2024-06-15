317b1a6a517e400e127340b87f7d9b17b14c7449
Zhong Jianxin <azuwis@gmail.com>
2024-06-15 22:25:15 +0800

    sunshine-git: Update to git HEAD, and use boost185

--- pkgs/by-name/su/sunshine-git/package.nix
+++ pkgs/by-name/su/sunshine-git/package.nix
@@ -1,26 +1,35 @@
 {
+  lib,
   sunshine,
+  boost185,
   nodejs,
   cudaSupport ? false,
   nix-update-script,
 }:
 
-(sunshine.override { inherit cudaSupport; }).overrideAttrs (old: {
-  pname = "sunshine-git";
-  version = "2024.615.30110-unstable-2024-06-14";
+(sunshine.override {
+  inherit cudaSupport;
+  boost = boost185;
+}).overrideAttrs
+  (old: {
+    pname = "sunshine-git";
+    version = "2024.616.13035-unstable-2024-06-16";
 
-  src = old.src.override {
-    rev = "a66dd260cc8caa1f81fd7365af44c235bb9dc77e";
-    sha256 = "sha256-Rk4+lEHpHrP9aXmJ89RacWvrjGibjGbgrSrK4zoGC9Q=";
-  };
+    src = old.src.override {
+      rev = "42aec263058f2ab59502ea4b55aae27e46c81de6";
+      sha256 = "sha256-WN88q3p1K3YmMYJ0qMvKuDsTYvGxt3PlPTwX0jazktI=";
+    };
 
-  buildInputs = old.buildInputs ++ [ nodejs ];
+    buildInputs = old.buildInputs ++ [ nodejs ];
 
-  passthru.updateScript = nix-update-script {
-    extraArgs = [
-      "--version"
-      "branch=a66dd260cc8caa1f81fd7365af44c235bb9dc77e"
-      # "branch=pull/2606/head"
-    ];
-  };
-})
+    cmakeFlags = old.cmakeFlags ++ [ (lib.cmakeFeature "BOOST_USE_STATIC" "OFF") ];
+
+    passthru.updateScript = nix-update-script {
+      extraArgs = [
+        "--version"
+        "branch"
+        # "branch=42aec263058f2ab59502ea4b55aae27e46c81de6"
+        # "branch=pull/2606/head"
+      ];
+    };
+  })
