2597940e1b29b00933c880f9061379118d710a8c
Zhong Jianxin <azuwis@gmail.com>
2024-06-19 23:18:09 +0800

    lazyvim: Use updated vimdoc treesitter parser

--- common/lazyvim/base/default.nix
+++ common/lazyvim/base/default.nix
@@ -158,6 +158,8 @@ in
           csv
           jsonc
           regex
+          # builtin `vimdoc` has error: vimdoc(highlights): .../query.lua:252: Query error at 2:4. Invalid node type "delimiter":
+          vimdoc
         ]
       )).dependencies;
   };
