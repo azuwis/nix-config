3b590732603409fab3e35cab50c5972f6c3b82fa
Zhong Jianxin <azuwis@gmail.com>
2024-06-24 21:04:17 +0800

    jovian: Use overlay to fix CJK font problem of gamescope-session

--- overlays/jovian.nix
+++ overlays/jovian.nix
@@ -1,4 +1,14 @@
 self: super: {
+  # https://github.com/Jovian-Experiments/Jovian-NixOS/pull/376
+  steamPackages = super.steamPackages.overrideScope (
+    _: scopeSuper: {
+      steam-fhsenv = scopeSuper.steam-fhsenv.override (prev: {
+        extraPkgs =
+          pkgs: (if prev ? extraPkgs then prev.extraPkgs pkgs else [ ]) ++ [ pkgs.noto-fonts-cjk-sans ];
+      });
+    }
+  );
+
   # Workaround for vimPlugins.* not in jovia/nixpkgs yet.
   # Remove it when jovian upgrades next time.
   # vimPlugins = super.vimPlugins // {
