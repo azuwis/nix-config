1f1671d0a21379147ead310cdf70c102566586e0
Zhong Jianxin <azuwis@gmail.com>
2024-06-26 20:42:51 +0800

    rime-ice: 2024.05.21-unstable-2024-06-09 -> 2024.05.21-unstable-2024-06-23

--- pkgs/by-name/ri/rime-ice/package.nix
+++ pkgs/by-name/ri/rime-ice/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "rime-ice";
-  version = "2024.05.21-unstable-2024-06-09";
+  version = "2024.05.21-unstable-2024-06-23";
 
   src = fetchFromGitHub {
     owner = "iDvel";
     repo = "rime-ice";
-    rev = "0b59306d619a29cd345e3277aa5cd0b0d5653eda";
-    sha256 = "sha256-dElBA0baKaMN04sSXvMgfey+I60Zq4b+i+KoYzpxZMQ=";
+    rev = "fdb6141a18ed6479fd82616c8a82befea2ae30df";
+    sha256 = "sha256-O1oR85+0qo3GFluV6Z5rYuycUCErhZHOc1rrdD2Yh5k=";
   };
 
   installPhase = ''
