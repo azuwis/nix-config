db5df493f388d4c066d6eb706afedf7a27440948
Zhong Jianxin <azuwis@gmail.com>
2024-07-04 23:22:34 +0800

    os: Add `hardware` subcommand

--- hosts/update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2024
-
-test -e /etc/NIXOS || exit
-cd "${0%/*}" || exit
-sudo nixos-generate-config --show-hardware-config > "hardware-$(hostname).nix"
