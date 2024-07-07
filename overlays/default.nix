b1bdf7fb22a57a7110308bb1ef02529db39b4357
Zhong Jianxin <azuwis@gmail.com>
2024-07-07 20:26:18 +0800

    overlays: Add workaround for mpv broken on darwin

--- overlays/default.nix
+++ overlays/default.nix
@@ -18,6 +18,53 @@ self: super: {
   fcitx5-configtool = self.writeShellScriptBin "fcitx5-config-qt" ''
     echo "fcitx-config-qt dummy command"
   '';
+  # https://github.com/NixOS/nixpkgs/issues/320900
+  # https://github.com/NixOS/nixpkgs/pull/79344
+  mpv-unwrapped =
+    (super.mpv-unwrapped.override {
+      swift =
+        let
+          CommandLineTools = "/Library/Developer/CommandLineTools";
+        in
+        super.stdenv.mkDerivation {
+          name = "swift-CommandLineTools-0.0.0";
+          phases = [
+            "installPhase"
+            "fixupPhase"
+          ];
+
+          propagatedBuildInputs = [ self.darwin.DarwinTools ];
+
+          installPhase = ''
+            mkdir -p $out/bin $out/lib
+            ln -s ${CommandLineTools}/usr/bin/swift $out/bin
+            ln -s ${CommandLineTools}/usr/lib/swift $out/lib
+            ln -s ${CommandLineTools}/SDKs $out
+          '';
+
+          setupHook = builtins.toFile "hook" ''
+            addCommandLineTools() {
+                echo >&2
+                echo "WARNING: this is impure and unreliable, make sure the CommandLineTools are installed!" >&2
+                echo "  $ xcode-select --install" >&2
+                echo >&2
+                [ -d ${CommandLineTools} ]
+                export NIX_LDFLAGS+=" -L@out@/lib/swift/macosx"
+                export SWIFT=swift
+                export SWIFT_LIB_DYNAMIC=@out@/lib/swift/macosx
+                export MACOS_SDK_VERSION=$(sw_vers -productVersion | awk -F. '{print $1}')
+                export MACOS_SDK=@out@/SDKs/MacOSX$MACOS_SDK_VERSION.sdk
+            }
+
+            prePhases+=" addCommandLineTools"
+          '';
+
+          __impureHostDeps = [ CommandLineTools ];
+        };
+    }).overrideAttrs
+      (old: {
+        preConfigure = "";
+      });
   nixos-option =
     let
       prefix = ''(builtins.getFlake \\\"/etc/nixos\\\").nixosConfigurations.\$(hostname)'';
