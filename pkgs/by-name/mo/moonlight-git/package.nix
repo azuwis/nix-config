58f6cf8555e05f7bcacc168d72272a324aca7055
Zhong Jianxin <azuwis@gmail.com>
2024-07-13 19:55:38 +0800

    moonlight-git: 6.0.1-unstable-2024-07-06 -> 6.0.1-unstable-2024-07-13

--- pkgs/by-name/mo/moonlight-git/package.nix
+++ pkgs/by-name/mo/moonlight-git/package.nix
@@ -6,11 +6,11 @@
 
 moonlight-qt.overrideAttrs (old: {
   pname = "moonlight-git";
-  version = "6.0.1-unstable-2024-07-06";
+  version = "6.0.1-unstable-2024-07-13";
 
   src = old.src.override {
-    rev = "d085722911ef100e8de5debfdf53fca4675d606e";
-    sha256 = "sha256-LuROMq/eIxs+PaeGutPcSa6qftV3vanAG1+4MGm5ivc=";
+    rev = "3580286807a09d9737d9a95160d3d96f3a6aa5d6";
+    sha256 = "sha256-+6YKxVRjna8/4jyatr1NtGiFIFhXE3urxAmXxmCtuRo=";
   };
 
   patches = [ ];
