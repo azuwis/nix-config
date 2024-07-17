8a98f2f0106ebd0ebfa185811aa37cff8f484996
Zhong Jianxin <azuwis@gmail.com>
2024-07-17 21:27:48 +0800

    xiaomi_miio_airconditioningcompanion: Init at 2023.12.0.0

---
+++ pkgs/by-name/xi/xiaomi_miio_airconditioningcompanion/package.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildHomeAssistantComponent,
+  fetchFromGitHub,
+  home-assistant,
+  nix-update-script,
+}:
+
+buildHomeAssistantComponent rec {
+  owner = "syssi";
+  domain = "xiaomi_miio_airconditioningcompanion";
+  version = "2023.12.0.0";
+
+  src = fetchFromGitHub {
+    owner = "syssi";
+    repo = "xiaomi_airconditioningcompanion";
+    rev = version;
+    hash = "sha256-wJYjeQjkUwhvGEZTcXUWKTu5DIIyGI4rP6RU7L4EcoM=";
+  };
+
+  propagatedBuildInputs = with home-assistant.python.pkgs; [ python-miio ];
+
+  dontBuild = true;
+
+  dontCheckManifest = true;
+
+  # passthru.updateScript = nix-update-script { };
+
+  meta = {
+    changelog = "https://github.com/syssi/xiaomi_airconditioningcompanion/releases/tag/${version}";
+    description = "Xiaomi Mi and Aqara Air Conditioning Companion integration for Home Assistant";
+    homepage = "https://github.com/syssi/xiaomi_airconditioningcompanion";
+    maintainers = with lib.maintainers; [ azuwis ];
+    license = lib.licenses.asl20;
+  };
+}
