4682ec35db543a4f838eef0a30e5ad3ded922ae5
Zhong Jianxin <azuwis@gmail.com>
2024-07-18 23:22:28 +0800

    foot: Use themes from nixpkgs.foot

--- nixos/foot/home.nix
+++ nixos/foot/home.nix
@@ -22,10 +22,7 @@ in
       settings = {
         main = {
           font = lib.mkDefault "monospace:pixelsize=20";
-          include = "${pkgs.fetchurl {
-            url = "https://codeberg.org/dnkl/foot/raw/commit/a1796ba5cd1cc7b6ef03021d7db57503e445b5dd/themes/nord";
-            sha256 = "sha256-YBoRdklYm2nK9xdypxNZFTloJ3xhKH0d4MvykGUP3i0=";
-          }}";
+          include = "${lib.getOutput "themes" pkgs.foot}/share/foot/themes/nord";
           term = "xterm-256color";
         };
       };
