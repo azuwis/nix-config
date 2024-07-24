57fc7494dd6de152c536c5438799916f5d65d587
Zhong Jianxin <azuwis@gmail.com>
2024-07-24 21:30:14 +0800

    gree2: 0-unstable-2024-06-26 -> 0-unstable-2024-07-22

--- pkgs/by-name/gr/gree2/package.nix
+++ pkgs/by-name/gr/gree2/package.nix
@@ -8,13 +8,13 @@
 buildHomeAssistantComponent {
   owner = "xcy1231";
   domain = "gree2";
-  version = "0-unstable-2024-06-26";
+  version = "0-unstable-2024-07-22";
 
   src = fetchFromGitHub {
     owner = "xcy1231";
     repo = "Ha-GreeCentralClimate";
-    rev = "6c3f3e906f5832adaf8b41062717bd3085dd84f0";
-    hash = "sha256-dbzevANt7ZvvICnmop97k+JLGynog94aIYIJTAGJm0w=";
+    rev = "1fb3ec02fe8eb6c4081107892837fc242d638f22";
+    hash = "sha256-YVzSBnhH51JxI4iCKzUG7GFTDwrHMkpIub+bdElbfc8=";
   };
 
   dontBuild = true;
