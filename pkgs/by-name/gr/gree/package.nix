fc53113ffb06f25d14160ae172ec1c161ae7c845
Zhong Jianxin <azuwis@gmail.com>
2024-07-25 21:36:59 +0800

    gree: Ignore from update.nix

--- pkgs/by-name/gr/gree/package.nix
+++ pkgs/by-name/gr/gree/package.nix
@@ -22,7 +22,10 @@ buildHomeAssistantComponent rec {
 
   dontBuild = true;
 
-  passthru.updateScript = nix-update-script { };
+  # buildPythonPackage always set passthru.updateScript and can not be disabled,
+  # override it with command `echo` to workaround
+  passthru.updateScript = "echo";
+  # passthru.updateScript = nix-update-script { };
 
   meta = with lib; {
     description = "Custom Gree climate component written in Python3 for Home Assistant. Controls AC's supporting the Gree protocol.";
