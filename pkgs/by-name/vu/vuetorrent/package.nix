982809ef68dcac9207a3db078e9fc324c16ae61a
Zhong Jianxin <azuwis@gmail.com>
2024-07-26 23:26:41 +0800

    vuetorrent: Init at 2.11.0

---
+++ pkgs/by-name/vu/vuetorrent/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchzip,
+  nix-update-script,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "vuetorrent";
+  version = "2.11.0";
+
+  src = fetchzip {
+    url = "https://github.com/WDaan/VueTorrent/releases/download/v${finalAttrs.version}/vuetorrent.zip";
+    sha256 = "sha256-uo1i14K0lZYMY1pgTqvb8IMWSn25xhQKgnpmDF1YZpg=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/
+    cp -r public/ $out/share/
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "The sleekest looking WEBUI for qBittorrent made with Vuejs";
+    homepage = "https://github.com/VueTorrent/VueTorrent";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ azuwis ];
+    platforms = lib.platforms.all;
+  };
+})
