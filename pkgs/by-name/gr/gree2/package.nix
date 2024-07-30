4aba02e7ceb9d7639dca2054a7e566ffc3db42d7
Zhong Jianxin <azuwis@gmail.com>
2024-07-30 19:59:08 +0800

    Revert "gree2: 0-unstable-2024-06-26 -> 0-unstable-2024-07-22"
    This reverts commit 57fc7494dd6de152c536c5438799916f5d65d587.
    
    This version is broken:
    
    ```
      File "/var/lib/hass/custom_components/gree2/climate.py", line 45, in async_setup_platform
        server = FakeServer(fake_server, 1812, 'dis.gree.com')
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ```

--- pkgs/by-name/gr/gree2/package.nix
+++ pkgs/by-name/gr/gree2/package.nix
@@ -8,13 +8,13 @@
 buildHomeAssistantComponent {
   owner = "xcy1231";
   domain = "gree2";
-  version = "0-unstable-2024-07-22";
+  version = "0-unstable-2024-06-26";
 
   src = fetchFromGitHub {
     owner = "xcy1231";
     repo = "Ha-GreeCentralClimate";
-    rev = "1fb3ec02fe8eb6c4081107892837fc242d638f22";
-    hash = "sha256-YVzSBnhH51JxI4iCKzUG7GFTDwrHMkpIub+bdElbfc8=";
+    rev = "6c3f3e906f5832adaf8b41062717bd3085dd84f0";
+    hash = "sha256-dbzevANt7ZvvICnmop97k+JLGynog94aIYIJTAGJm0w=";
   };
 
   dontBuild = true;
