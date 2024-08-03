261478792099e95751b06c9bbf0b62e623dd4a2a
Zhong Jianxin <azuwis@gmail.com>
2024-08-03 23:49:41 +0800

    overlays: Use recommented keyword `final` `prev`
    See https://wiki.nixos.org/wiki/Overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,8 @@
 # https://discourse.nixos.org/t/in-overlays-when-to-use-self-vs-super/2968/12
 
-self: super: {
+final: prev: {
   # pkgs
-  # cemu = super.cemu.overrideAttrs (old: {
+  # cemu = prev.cemu.overrideAttrs (old: {
   #   postPatch = (old.postPatch or "") + ''
   #     sed -i '/\/\/ already connected\?/,+2 d' src/input/api/DSU/DSUControllerProvider.cpp
   #   '';
@@ -10,9 +10,9 @@ self: super: {
 
   # override
   # https://github.com/NixOS/nixpkgs/issues/267536
-  borgbackup = super.borgbackup.overrideAttrs (
+  borgbackup = prev.borgbackup.overrideAttrs (
     old:
-    self.lib.optionalAttrs (self.stdenv.isDarwin && self.stdenv.isx86_64) {
+    final.lib.optionalAttrs (final.stdenv.isDarwin && final.stdenv.isx86_64) {
       disabledTests = (old.disabledTests or [ ]) ++ [
         "test_overwrite"
         "test_sparse_file"
@@ -20,27 +20,27 @@ self: super: {
     }
   );
   # disable fcitx5-configtool
-  libsForQt5 = super.libsForQt5.overrideScope (
-    _: scopeSuper: {
-      fcitx5-with-addons = scopeSuper.fcitx5-with-addons.override { withConfigtool = false; };
+  libsForQt5 = prev.libsForQt5.overrideScope (
+    qt5final: qt5prev: {
+      fcitx5-with-addons = qt5prev.fcitx5-with-addons.override { withConfigtool = false; };
     }
   );
   # https://github.com/NixOS/nixpkgs/issues/320900
   # https://github.com/NixOS/nixpkgs/pull/79344
   mpv-unwrapped =
-    (super.mpv-unwrapped.override {
+    (prev.mpv-unwrapped.override {
       swift =
         let
           CommandLineTools = "/Library/Developer/CommandLineTools";
         in
-        self.stdenv.mkDerivation {
+        final.stdenv.mkDerivation {
           name = "swift-CommandLineTools-0.0.0";
           phases = [
             "installPhase"
             "fixupPhase"
           ];
 
-          propagatedBuildInputs = [ self.darwin.DarwinTools ];
+          propagatedBuildInputs = [ final.darwin.DarwinTools ];
 
           installPhase = ''
             mkdir -p $out/bin $out/lib
@@ -69,18 +69,18 @@ self: super: {
         };
     }).overrideAttrs
       (old: {
-        preConfigure = if self.stdenv.isDarwin then "" else old.preConfigure;
+        preConfigure = if final.stdenv.isDarwin then "" else old.preConfigure;
       });
   # https://github.com/nix-community/nix-zsh-completions/pull/52
-  nix-zsh-completions = super.nix-zsh-completions.overrideAttrs (old: {
+  nix-zsh-completions = prev.nix-zsh-completions.overrideAttrs (old: {
     src = old.src.override {
       rev = "496b2e66aa10bdcb6f033bf8118f1972203ee7b0";
       hash = "sha256-OncfatdtdEavVF5Y5hLITgx9wz1Z29bX4P/uxmojvDI=";
     };
   });
   # https://github.com/NixOS/nixpkgs/pull/313497
-  nixos-option = self.callPackage "${
-    self.fetchFromGitHub {
+  nixos-option = final.callPackage "${
+    final.fetchFromGitHub {
       owner = "NixOS";
       repo = "nixpkgs";
       rev = "172f04cac31ef53e7e6c1a96e0db5514993f7208";
@@ -89,14 +89,14 @@ self: super: {
       nonConeMode = true;
     }
   }/pkgs/tools/nix/nixos-option" { };
-  # python3 = super.python3.override {
-  #   packageOverrides = pyself: pysuper: {
-  #     pysonybraviapsk = self.python3.pkgs.callPackage ../pkgs/python/pysonybraviapsk { };
+  # python3 = prev.python3.override {
+  #   packageOverrides = pyfinal: pyprev: {
+  #     pysonybraviapsk = final.python3.pkgs.callPackage ../pkgs/python/pysonybraviapsk { };
   #   };
   # };
-  # python3Packages = self.python3.pkgs;
-  # sway-unwrapped = super.sway-unwrapped.override {
-  #   wlroots = self.wlroots_0_16.overrideAttrs (old: {
+  # python3Packages = final.python3.pkgs;
+  # sway-unwrapped = prev.sway-unwrapped.override {
+  #   wlroots = final.wlroots_0_16.overrideAttrs (old: {
   #     postPatch =
   #       (old.postPatch or "") + ''
   #         substituteInPlace render/gles2/renderer.c --replace-fail "glFlush();" "glFinish();"
--- overlays/jovian.nix
+++ overlays/jovian.nix
@@ -1,21 +1,21 @@
-self: super: {
+final: prev: {
   # https://github.com/Jovian-Experiments/Jovian-NixOS/pull/376
-  steamPackages = super.steamPackages.overrideScope (
-    _: scopeSuper: {
-      steam-fhsenv = scopeSuper.steam-fhsenv.override (prev: {
+  steamPackages = prev.steamPackages.overrideScope (
+    steamfinal: steamprev: {
+      steam-fhsenv = steamprev.steam-fhsenv.override (old: {
         extraPkgs =
-          pkgs: (if prev ? extraPkgs then prev.extraPkgs pkgs else [ ]) ++ [ pkgs.noto-fonts-cjk-sans ];
+          pkgs: (if old ? extraPkgs then old.extraPkgs pkgs else [ ]) ++ [ pkgs.noto-fonts-cjk-sans ];
       });
     }
   );
 
   # Workaround for vimPlugins.* not in jovia/nixpkgs yet.
   # Remove it when jovian upgrades next time.
-  # vimPlugins = super.vimPlugins // {
-  #   neogit = self.vimUtils.buildVimPlugin {
+  # vimPlugins = prev.vimPlugins // {
+  #   neogit = final.vimUtils.buildVimPlugin {
   #     pname = "neogit";
   #     version = "2024-05-26";
-  #     src = self.fetchFromGitHub {
+  #     src = final.fetchFromGitHub {
   #       owner = "NeogitOrg";
   #       repo = "neogit";
   #       rev = "70ad95be902ee69b56410a5cfc690dd03104edb3";
