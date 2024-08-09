183145376f90b7db5195f40cfc09598b8392a3ee
Zhong Jianxin <azuwis@gmail.com>
2024-08-09 21:30:33 +0800

    CI/update: Echo all the comments

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -11,7 +11,7 @@ update_package() {
 
   if [ "$(gh pr list --head "$update_branch" | wc -l)" -gt 0 ]
   then
-    # PR exists
+    echo "PR exists"
     git checkout "$update_branch"
     git clean -df
     if ! git merge-base --is-ancestor "$default_branch" "$update_branch"
@@ -23,23 +23,23 @@ update_package() {
     ./scripts/update -c "$package" || true
     if [ "$(git rev-parse HEAD)" != "$saved_commit" ]
     then
-      # Upstream newer than the PR, reset the branch and update again
+      echo "Upstream newer than the PR, reset the branch and update again"
       git reset --hard "$default_branch"
       if ./scripts/update -c "$package"
       then
-        # Update success, update the PR
+        echo "Update success, update the PR"
         gh pr edit "$update_branch" --title "$(git show -s --format=%B)" --body "$(./scripts/update -i "$package")"
       fi
     fi
     git push --force origin "$update_branch"
   else
-    # PR not exists
+    echo "PR not exists"
     git checkout -B "$update_branch" "$default_branch"
     git clean -df
     ./scripts/update -c "$package" || true
     if [ "$(git rev-list --count "${default_branch}..")" -gt 0 ]
     then
-      # Update success, create PR
+      echo "Update success, create PR"
       git push --force origin "$update_branch"
       gh pr create --title "$(git show -s --format=%B)" --body "$(./scripts/update -i "$package")"
     fi
