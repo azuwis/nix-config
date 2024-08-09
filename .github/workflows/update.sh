447605cb2127a9f91c152c7b4ede61c7f17c3416
Zhong Jianxin <azuwis@gmail.com>
2024-08-09 21:32:00 +0800

    CI/update: Handle PR base branch changes

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -12,18 +12,24 @@ update_package() {
     # PR exists
     git checkout "$update_branch"
     git clean -df
+    if ! git merge-base --is-ancestor "$default_branch" "$update_branch"
+    then
+      git reset --hard "$default_branch"
+      git cherry-pick "origin/$update_branch"
+    fi
+    saved_commit=$(git rev-parse HEAD)
     ./scripts/update -c "$package" || true
-    if [ "$(git rev-list --count "origin/${update_branch}..")" -gt 0 ]
+    if [ "$(git rev-parse HEAD)" != "$saved_commit" ]
     then
       # Upstream newer than the PR, reset the branch and update again
       git reset --hard "$default_branch"
       if ./scripts/update -c "$package"
       then
         # Update success, update the PR
-        git push --force origin "$update_branch"
         gh pr edit "$update_branch" --title "$(git show -s --format=%B)"
       fi
     fi
+    git push --force origin "$update_branch"
   else
     # PR not exists
     git checkout -B "$update_branch" "$default_branch"
