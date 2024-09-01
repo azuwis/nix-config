ec7b6fb3111412513eea899cd4fc787d72fe3c11
Zhong Jianxin <azuwis@gmail.com>
2024-09-01 23:12:31 +0800

    yambar-git: Update to PR 403

--- pkgs/by-name/ya/yambar-git/package.nix
+++ pkgs/by-name/ya/yambar-git/package.nix
@@ -2,17 +2,17 @@
 
 yambar.overrideAttrs (old: {
   pname = "yambar-git";
-  version = "1.11.0-unstable-2024-08-21";
+  version = "1.11.0-unstable-2024-09-01";
 
   # yambar crash, https://codeberg.org/dnkl/yambar/issues/300
   # tag: add 'b' formatter, https://codeberg.org/dnkl/yambar/issues/392
   src = old.src.override {
-    rev = "8bd02d4a4f3f7bd1a271ac10757771969075912b";
-    hash = "sha256-CTQkqvpjCxT6vUvkeP9eK79zepuKkzwu+7WhBiDWAl4=";
+    rev = "642854047301351b032beca4deb8e7ed061cce1c";
+    hash = "sha256-eWwYnYEulacFO1+sksManQc3gLfSThabIx84ziZ6yhg=";
   };
 
-  # https://codeberg.org/dnkl/yambar/pulls/401
+  # https://codeberg.org/dnkl/yambar/pulls/403
   passthru.updateScript = nix-update-script {
-    extraArgs = [ "--version=branch=8bd02d4a4f3f7bd1a271ac10757771969075912b" ];
+    extraArgs = [ "--version=branch=642854047301351b032beca4deb8e7ed061cce1c" ];
   };
 })
