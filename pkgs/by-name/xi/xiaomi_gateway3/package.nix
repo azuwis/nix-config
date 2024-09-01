e82793caef32d196c15ecbaefb1440897b48f0ee
Zhong Jianxin <azuwis@gmail.com>
2024-09-01 22:45:49 +0800

    xiaomi_gateway3: Update to PR 1436

--- pkgs/by-name/xi/xiaomi_gateway3/package.nix
+++ pkgs/by-name/xi/xiaomi_gateway3/package.nix
@@ -1,13 +1,14 @@
-{ lib, home-assistant-custom-components }:
+{ home-assistant-custom-components, nix-update-script }:
 
-home-assistant-custom-components.xiaomi_gateway3.overridePythonAttrs (old: rec {
-  version = "4.0.4";
+home-assistant-custom-components.xiaomi_gateway3.overridePythonAttrs (old: {
+  version = "4.0.5-unstable-2024-09-01";
 
   src = old.src.override {
-    rev = "v${version}";
-    hash = "sha256-MQ/yxxXt2BXUAHEHGOaqansgon22oQ0byCQcUcVZdOQ=";
+    rev = "b5c40bd96fdc60023ea8e69af740814f8a42549d";
+    hash = "sha256-BkylAqIqI22jHeq5CnZxgIepK7Al9ABgvJOqutceD9k=";
   };
 
   passthru.isHomeAssistantComponent = true;
-  passthru.skipUpdate = true;
+  # https://github.com/AlexxIT/XiaomiGateway3/pull/1436
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch=pull/1436/head" ]; };
 })
