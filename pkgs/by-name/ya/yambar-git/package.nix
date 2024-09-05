75ac664e8ce68c11d213db1413645908ccbb5480
Zhong Jianxin <azuwis@gmail.com>
2024-09-05 22:02:24 +0800

    yambar-git: 1.11.0-unstable-2024-09-01 -> 1.11.0-unstable-2024-09-05
    Also setup nix-update to use latest commit

--- pkgs/by-name/ya/yambar-git/package.nix
+++ pkgs/by-name/ya/yambar-git/package.nix
@@ -2,17 +2,14 @@
 
 yambar.overrideAttrs (old: {
   pname = "yambar-git";
-  version = "1.11.0-unstable-2024-09-01";
+  version = "1.11.0-unstable-2024-09-05";
 
   # yambar crash, https://codeberg.org/dnkl/yambar/issues/300
-  # tag: add 'b' formatter, https://codeberg.org/dnkl/yambar/issues/392
+  # tag: add '/N' formatter, https://codeberg.org/dnkl/yambar/pulls/403
   src = old.src.override {
-    rev = "642854047301351b032beca4deb8e7ed061cce1c";
-    hash = "sha256-eWwYnYEulacFO1+sksManQc3gLfSThabIx84ziZ6yhg=";
+    rev = "060586dbbeb9134ee0a8810d611fc08310da490a";
+    hash = "sha256-ZRRxY6nPhZ/CD+oy2lNfxEHEWc8DGe3wmDosjILAt5Q=";
   };
 
-  # https://codeberg.org/dnkl/yambar/pulls/403
-  passthru.updateScript = nix-update-script {
-    extraArgs = [ "--version=branch=642854047301351b032beca4deb8e7ed061cce1c" ];
-  };
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 })
