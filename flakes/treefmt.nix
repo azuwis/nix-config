1d7a03f3d7dc1757e44e7500cc99dd1b17b15156
Zhong Jianxin <azuwis@gmail.com>
2024-09-24 21:28:37 +0800

    treefmt: Fix for `nixpkgs.config.allowAliases = false`

--- flakes/treefmt.nix
+++ flakes/treefmt.nix
@@ -2,25 +2,32 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem.treefmt = {
-    projectRootFile = "flake.nix";
-    settings.global.excludes = [ "hosts/hardware-*.nix" ];
+  perSystem.treefmt =
+    { pkgs, ... }:
+    {
+      # For `nixpkgs.config.allowAliases = false`
+      package = pkgs.treefmt2;
+      projectRootFile = "flake.nix";
+      settings.global.excludes = [ "hosts/hardware-*.nix" ];
 
-    programs.nixfmt.enable = true;
-    programs.shfmt = {
-      enable = true;
-      # Use settings in .editorconfig
-      indent_size = null;
+      programs.nixfmt.enable = true;
+
+      programs.shfmt = {
+        enable = true;
+        # Use settings in .editorconfig
+        indent_size = null;
+      };
+      settings.formatter.shfmt = {
+        includes = [
+          ".githooks/*"
+          "pkgs/by-name/sc/scripts/bin/*"
+          "scripts/os"
+          "scripts/update"
+        ];
+      };
+
+      programs.stylua.enable = true;
+
+      programs.yamlfmt.enable = true;
     };
-    settings.formatter.shfmt = {
-      includes = [
-        ".githooks/*"
-        "pkgs/by-name/sc/scripts/bin/*"
-        "scripts/os"
-        "scripts/update"
-      ];
-    };
-    programs.stylua.enable = true;
-    programs.yamlfmt.enable = true;
-  };
 }
