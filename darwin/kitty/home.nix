00a23248f01d6d867a8fa25d74f594f355b1a822
Zhong Jianxin <azuwis@gmail.com>
2024-09-25 19:52:59 +0800

    kitty: Remove programs.zsh.initExtra, already enabled by home-manager

--- darwin/kitty/home.nix
+++ darwin/kitty/home.nix
@@ -30,13 +30,4 @@
   #   symbol_map U+E5FA-U+E62B,U+E700-U+E7C5,U+F000-U+F2E0,U+E200-U+E2A9,U+F500-U+FD46,U+E300-U+E3EB,U+F400-U+F4A8,U+2665,U+26A1,U+F27C,U+E0A3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D2,U+E0D4,U+23FB-U+23FE,U+2B58,U+F300-U+F313,U+E000-U+E00D JetBrainsMono Nerd Font
   #   symbol_map U+100000-U+10FFFF SF Pro
   # '';
-  programs.zsh.initExtra = ''
-    # kitty shell integration
-    if [[ -n $KITTY_INSTALLATION_DIR ]]; then
-      export KITTY_SHELL_INTEGRATION="enabled"
-      autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-      kitty-integration
-      unfunction kitty-integration
-    fi
-  '';
 }
