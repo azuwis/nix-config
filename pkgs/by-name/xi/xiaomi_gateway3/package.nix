155b6414ac68a86611f9613744254be63492300a
Zhong Jianxin <azuwis@gmail.com>
2024-09-28 20:53:52 +0800

    xiaomi_gateway3: Remove package
    See previous commit

--- pkgs/by-name/xi/xiaomi_gateway3/package.nix
+++
@@ -1,9 +0,0 @@
-{ home-assistant-custom-components }:
-
-home-assistant-custom-components.xiaomi_gateway3.overridePythonAttrs (old: rec {
-  version = "4.0.6";
-  src = old.src.override {
-    rev = "v${version}";
-    hash = "sha256-E3BekX0Xbp1R36+dYmOlsI2BTrdGjFsMoYYRIiBi1qU=";
-  };
-})
