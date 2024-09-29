a6b7d8e2546e28d09895c69e2bc942dbf03e9bc8
Zhong Jianxin <azuwis@gmail.com>
2024-09-29 21:39:58 +0800

    droid: Enable nixpkgs.config.allowAliases

--- flakes/droid.nix
+++ flakes/droid.nix
@@ -21,8 +21,15 @@ let
   };
 in
 {
-  flake.nixOnDroidConfigurations.default = mkDroid { };
+  flake.nixOnDroidConfigurations.default = mkDroid {
+    # `pkgs.system` is used and breaks `config.allowaliases = false`
+    # https://github.com/nix-community/nix-on-droid/blob/5d88ff2519e4952f8d22472b52c531bb5f1635fc/flake.nix#L82
+    config.allowAliases = true;
+  };
 
   # for CI
-  flake.nixOnDroidConfigurations.droid = mkDroid { system = "x86_64-linux"; };
+  flake.nixOnDroidConfigurations.droid = mkDroid {
+    config.allowAliases = true;
+    system = "x86_64-linux";
+  };
 }
