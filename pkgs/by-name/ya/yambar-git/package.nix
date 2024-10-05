0c1e17f6639aa47139f9fe319b600840510c3858
Zhong Jianxin <azuwis@gmail.com>
2024-10-05 23:05:43 +0800

    yambar-git: Update to HEAD from pulls/405

--- pkgs/by-name/ya/yambar-git/package.nix
+++ pkgs/by-name/ya/yambar-git/package.nix
@@ -2,18 +2,17 @@
 
 yambar.overrideAttrs (old: {
   pname = "yambar-git";
-  version = "1.11.0-unstable-2024-09-25";
+  version = "1.11.0-unstable-2024-09-30";
 
   # yambar crash, https://codeberg.org/dnkl/yambar/issues/300
   # tag: add '/N' formatter, https://codeberg.org/dnkl/yambar/pulls/403
   # Add niri-workspaces and niri-language modules https://codeberg.org/dnkl/yambar/pulls/405
   src = old.src.override {
-    owner = "azuwis";
-    rev = "992125aaf9a3db0a8c06dbebf52b3cda12b96aa5";
+    rev = "b9b82d3876ca9b740affe20191cd84801181e9c0";
     hash = "sha256-tOjlRzQKg+rAso/COqiLTyLmLbhzffRBfSgJLLdW+gc=";
   };
 
   passthru.updateScript = nix-update-script {
-    extraArgs = [ "--version=branch=992125aaf9a3db0a8c06dbebf52b3cda12b96aa5" ];
+    extraArgs = [ "--version=branch=b9b82d3876ca9b740affe20191cd84801181e9c0" ];
   };
 })
