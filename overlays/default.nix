1cfadd459eefeb59b893e8d2f8aac5a8a2ca1014
Zhong Jianxin <azuwis@gmail.com>
2024-10-05 23:12:37 +0800

    overlays/mpv: Drop patches and postInstall overrides
    Fixed upstream

--- overlays/default.nix
+++ overlays/default.nix
@@ -47,41 +47,25 @@ final: prev: {
   luaPackages = final.lua.pkgs;
 
   # https://github.com/NixOS/nixpkgs/issues/320900
+  # https://github.com/NixOS/nixpkgs/issues/327836
   # https://github.com/NixOS/nixpkgs/pull/79344
-  mpv-unwrapped =
-    (prev.mpv-unwrapped.override {
-      # For `--vo=gpu-next` on macOS, useful for DobbyVision 10Bit videos
-      vulkanSupport = true;
-      swift =
-        (import
-          (builtins.fetchTarball {
-            # nixpkgs-24.05-darwin
-            url = "https://github.com/NixOS/nixpkgs/archive/ced0da1e7e7d50f1352bc6bdd25af8ae55eb3934.tar.gz";
-            sha256 = "01wqw1jsngicri7b09npg70xdzyrfq787kka9xx1q1h3p1jwnsag";
-          })
-          {
-            inherit (final.stdenv) system;
-          }
-        ).swift;
-    }).overrideAttrs
-      (
-        old:
-        final.lib.optionalAttrs final.stdenv.isDarwin {
-          patches = (old.patches or [ ]) ++ [ ../patches/mpv-darwin.patch ];
-          postInstall = ''
-            # Use a standard font
-            mkdir -p $out/share/mpv
-            ln -s ${final.freefont_ttf}/share/fonts/truetype/FreeSans.ttf $out/share/mpv/subfont.ttf
-
-            pushd ../TOOLS
-            cp mpv_identify.sh umpv $out/bin/
-            popd
-
-            mkdir -p $out/Applications
-            cp -r mpv.app $out/Applications
-          '';
+  mpv-unwrapped = prev.mpv-unwrapped.override {
+    # For `--vo=gpu-next` on macOS, useful for DobbyVision 10bit videos
+    vulkanSupport = true;
+    inherit
+      (import
+        (builtins.fetchTarball {
+          # nixpkgs-24.05-darwin
+          url = "https://github.com/NixOS/nixpkgs/archive/ced0da1e7e7d50f1352bc6bdd25af8ae55eb3934.tar.gz";
+          sha256 = "01wqw1jsngicri7b09npg70xdzyrfq787kka9xx1q1h3p1jwnsag";
+        })
+        {
+          inherit (final.stdenv) system;
         }
-      );
+      )
+      swift
+      ;
+  };
 
   # https://github.com/Mic92/nix-update/pull/269
   # nix-update = prev.nix-update.overridePythonAttrs (old: {
