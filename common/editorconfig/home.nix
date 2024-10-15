b35d40ff4ff6c26f9e865565a27864fdb48f9bdc
Zhong Jianxin <azuwis@gmail.com>
2024-10-15 20:50:48 +0800

    editorconfig: Just use .editorconfig as ~/.editorconfig

--- common/editorconfig/home.nix
+++ common/editorconfig/home.nix
@@ -15,23 +15,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    editorconfig.enable = true;
-    editorconfig.settings = {
-      # https://github.com/LazyVim/LazyVim/blob/main/stylua.toml
-      "*.lua" = {
-        indent_style = "space";
-        indent_size = 2;
-        max_line_length = 120;
-        sort_requires = true;
-      };
-    };
-    # `toINIWithGlobalSection` will escape `[shell]` to `\[shell\]`, use home.file.<name>.txt here
-    home.file.".editorconfig".text = ''
-      # man shfmt
-      [[shell]]
-      indent_style = space
-      indent_size = 2
-      max_line_length = 120
-    '';
+    home.file.".editorconfig".source = ../../.editorconfig;
   };
 }
