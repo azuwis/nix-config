09ca13d7c3bace507cffc10ff7f3c02578d7d316
Zhong Jianxin <azuwis@gmail.com>
2024-10-20 21:08:14 +0800

    Back out "jujutsu: Use `--stat` instead of adding log_detailed template"
    This backs out commit 40f03fcb256af0bde3fd5a2cf8018b4341be195e.
    
    `jj log --stat --template builtin_log_detailed` lacks a new line compared
    to custom `log_detailed` template

--- common/jujutsu/home.nix
+++ common/jujutsu/home.nix
@@ -23,9 +23,8 @@ in
         aliases = {
           ld = [
             "log"
-            "--stat"
             "--template"
-            "builtin_log_detailed"
+            "log_detailed"
           ];
           ls = [
             "log"
@@ -72,6 +71,7 @@ in
               )
             )
           '';
+          log_detailed = ''concat(builtin_log_detailed, diff.stat(120), "\n")'';
           log_stat = "concat(builtin_log_compact, diff.summary())";
         };
         templates = {
