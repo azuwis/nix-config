91338ed83c88bd3d527798f65d865bb0cf7f85c0
Zhong Jianxin <azuwis@gmail.com>
2024-10-22 19:42:53 +0800

    nixos: Add comment about files to backup

--- nixos/default.nix
+++ nixos/default.nix
@@ -7,6 +7,13 @@
 }:
 
 {
+  # Files to backup
+  # /var/lib/acme/
+  # /var/lib/bluetooth/
+  # /var/lib/hass/
+  # /var/lib/qbittorrent/
+  # /var/lib/torrent-ratio/
+  # /var/lib/zigbee2mqtt/
   imports = [
     inputs.agenix.nixosModules.default
     inputs.home-manager.nixosModules.home-manager
