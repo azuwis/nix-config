e92a42d5c31b675b5daa232da11091a313383ea5
Zhong Jianxin <azuwis@gmail.com>
2024-11-01 19:20:42 +0800

    nix-search: 0.3.1-unstable-2024-08-24 -> 0.4.0
    Also let updateScript use stable version, since upstream makes git tags
    now.

--- pkgs/by-name/ni/nix-search/package.nix
+++ pkgs/by-name/ni/nix-search/package.nix
@@ -5,15 +5,15 @@
   nix-update-script,
 }:
 
-buildGoModule {
+buildGoModule rec {
   pname = "nix-search";
-  version = "0.3.1-unstable-2024-08-24";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "diamondburned";
     repo = "nix-search";
-    rev = "e616ac1c82a616fa6e6d8c94839c5052eb8c808d";
-    hash = "sha256-h9yYOjL9i/m0r5NbqMcLMFNnwSKsIgfUr5qk+47pOtc=";
+    rev = "v${version}";
+    hash = "sha256-dOdcXKfSwi0THOjtgP3O/46SWoUY+T7LL9nGwOXXJfw=";
   };
 
   vendorHash = "sha256-bModWDH5Htl5rZthtk/UTw/PXT+LrgyBjsvE6hgIePY=";
@@ -23,14 +23,14 @@ buildGoModule {
     "-w"
   ];
 
-  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+  passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
-    description = "A Nix-channel-compatible package search";
+  meta = {
+    description = "Nix-channel-compatible package search";
     homepage = "https://github.com/diamondburned/nix-search";
-    license = licenses.gpl3Only;
+    license = lib.licenses.gpl3Only;
     mainProgram = "nix-search";
-    maintainers = with maintainers; [ azuwis ];
-    platforms = platforms.all;
+    maintainers = with lib.maintainers; [ azuwis ];
+    platforms = lib.platforms.all;
   };
 }
