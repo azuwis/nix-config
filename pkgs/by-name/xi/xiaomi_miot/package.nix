9363bd3688d4fef72afe9e42d562e753e6b32910
Zhong Jianxin <azuwis@gmail.com>
2024-11-18 20:47:47 +0800

    xiaomi_miot: 0.7.22 -> 0.7.23, use same dependences as upstream

--- pkgs/by-name/xi/xiaomi_miot/package.nix
+++ pkgs/by-name/xi/xiaomi_miot/package.nix
@@ -1,9 +1,14 @@
-{ home-assistant-custom-components }:
+{ home-assistant, home-assistant-custom-components }:
 
 home-assistant-custom-components.xiaomi_miot.overridePythonAttrs (old: rec {
-  version = "0.7.22";
+  version = "0.7.23";
   src = old.src.override {
     rev = "v${version}";
-    hash = "sha256-KRjhGvK69NTsH0AkmFAZD10g9CpGQtjAXLlOm40Y4k8=";
+    hash = "sha256-PTjkKuK+DAOmKREr0AHjFXzy4ktguD4ZOHcWuLedLH0=";
   };
+  propagatedBuildInputs = with home-assistant.python.pkgs; [
+    construct
+    micloud
+    python-miio
+  ];
 })
