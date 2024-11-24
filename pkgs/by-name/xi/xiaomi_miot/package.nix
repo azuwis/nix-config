9c5f5f746e21f2f8731d5104b60c09f781d57cb2
Zhong Jianxin <azuwis@gmail.com>
2024-11-24 21:00:49 +0800

    xiaomi_miot: Sync with xiaomi_miot in nixpkgs

--- pkgs/by-name/xi/xiaomi_miot/package.nix
+++ pkgs/by-name/xi/xiaomi_miot/package.nix
@@ -2,13 +2,13 @@
 
 home-assistant-custom-components.xiaomi_miot.overridePythonAttrs (old: rec {
   version = "0.7.23";
+
   src = old.src.override {
     rev = "v${version}";
     hash = "sha256-PTjkKuK+DAOmKREr0AHjFXzy4ktguD4ZOHcWuLedLH0=";
   };
-  propagatedBuildInputs = with home-assistant.python.pkgs; [
-    construct
-    micloud
-    python-miio
-  ];
+
+  passthru.skipUpdate = true;
+
+  meta.changelog = "https://github.com/al-one/hass-xiaomi-miot/releases/tag/v${version}";
 })
