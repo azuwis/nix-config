4ca47ce72c9e681912a6bb21f54484f32ba7322e
Zhong Jianxin <azuwis@gmail.com>
2024-12-04 20:51:35 +0800

    default.nix: Use style from https://github.com/nix-community/flake-compat

--- default.nix
+++ default.nix
@@ -1,26 +1,36 @@
 { ... }:
+
 let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  flake-compat = fetchTarball {
-    url =
-      lock.nodes.flake-compat.locked.url
-        or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash;
+  flake-compat =
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      inherit (lock.nodes.${lock.nodes.${lock.root}.inputs.flake-compat}.locked)
+        owner
+        repo
+        rev
+        narHash
+        ;
+    in
+    fetchTarball {
+      url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+      sha256 = narHash;
+    };
+  flake = import flake-compat {
+    src = ./.;
   };
-  self = import flake-compat { src = ./.; };
-  nixpkgs = self.defaultNix.inputs.nixpkgs.outPath;
+  nixpkgs = flake.defaultNix.inputs.nixpkgs.outPath;
   packages = import nixpkgs {
     overlays = [
       # For `builtins.unsafeGetAttrPos "src"` to work in scripts/update.nix,
-      # `self.defaultNix.overlays.default` can not be used here.
+      # `flake.defaultNix.overlays.default` can not be used here.
       # Update this if `flake.overlays.default` changed in `flakes/overlay.nix`.
-      self.defaultNix.inputs.agenix.overlays.default
+      flake.defaultNix.inputs.agenix.overlays.default
       (import "${nixpkgs}/pkgs/top-level/by-name-overlay.nix" ./pkgs/by-name)
-      self.defaultNix.overlays.yuzu
+      flake.defaultNix.overlays.yuzu
       (import ./overlays/default.nix)
       (import ./overlays/lix.nix)
     ];
   };
 in
 # nix-update expect nixpkgs-like repo https://discourse.nixos.org/t/25274
-packages // self.defaultNix
+packages // flake.defaultNix
