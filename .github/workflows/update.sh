3e8800083f11824032eb66b87b3e1e0fe9f57c84
Zhong Jianxin <azuwis@gmail.com>
2024-12-04 20:30:27 +0800

    CI/update: Remove pr comments to reduce notifications

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -49,13 +49,11 @@ update_package() {
       git reset --hard "$default_branch"
       if try_update "$package"; then
         echo "::notice::$package: Update package succeeded, update the PR"
-        gh pr comment "$update_branch" --body "Update package succeeded."
         gh pr edit "$update_branch" --title "$(git show -s --format=%B)" --body "$(generate_body "$package")"
         git push --force origin "$update_branch"
       fi
     elif [ "$(git rev-parse HEAD)" = "$(git rev-parse "$default_branch")" ]; then
       echo "::notice::$package: Close the PR, seems cherry-picked in $default_branch."
-      gh pr comment "$update_branch" --body "Close, seems cherry-picked in $default_branch."
       git push --delete origin "$update_branch"
     elif [ "$(git rev-parse HEAD)" != "$(git rev-parse "origin/$update_branch")" ]; then
       git push --force origin "$update_branch"
@@ -70,7 +68,6 @@ update_package() {
       git push --force origin "$update_branch"
       gh pr create --title "$(git show -s --format=%B)" --body "$(generate_body "$package")"
       echo "Force push to trigger CI"
-      gh pr comment "$update_branch" --body "Force push to trigger CI."
       git commit --amend --no-edit
       git push --force origin "$update_branch"
     fi
