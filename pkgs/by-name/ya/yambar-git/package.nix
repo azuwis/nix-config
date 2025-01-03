9298220aff08e0799cc0d353ab63b6a5c1fc5d3d
Zhong Jianxin <azuwis@gmail.com>
2025-01-03 23:56:21 +0800

    yambar-git: 1.11.0-unstable-2024-10-24 -> 1.11.0-unstable-2025-01-01

--- pkgs/by-name/ya/yambar-git/package.nix
+++ pkgs/by-name/ya/yambar-git/package.nix
@@ -2,19 +2,19 @@
 
 yambar.overrideAttrs (old: {
   pname = "yambar-git";
-  version = "1.11.0-unstable-2024-10-24";
+  version = "1.11.0-unstable-2025-01-01";
 
   # yambar crash, https://codeberg.org/dnkl/yambar/issues/300
   # tag: add '/N' formatter, https://codeberg.org/dnkl/yambar/pulls/403
   # Add niri-workspaces and niri-language modules https://codeberg.org/dnkl/yambar/pulls/405
   src = old.src.override {
-    rev = "0a698558526d60997ae615b640826c003b5e95ce";
-    hash = "sha256-IHJgblYwCfhqSzM9C/PalK4xNmV0usWgGaHGDIQUVAg=";
+    rev = "fc24ea225d0cb66c1e6949cecd584c21ad1af9d7";
+    hash = "sha256-LfA5qWucGnldNqVRIJ8c3X6Odt4fD3e9JffmkPhF0Bw=";
   };
 
   passthru = (old.passthru or { }) // {
     updateScript = nix-update-script {
-      extraArgs = [ "--version=branch=pull/405/head" ];
+      extraArgs = [ "--version=branch" ];
     };
   };
 })
