e6b2539fe6f85d89cb7b8b07c856d8b6e59102b3
Zhong Jianxin <azuwis@gmail.com>
2025-01-03 23:23:06 +0800

    xiaomi_gateway3: 0-unstable-2024-11-29 -> 4.0.8

--- pkgs/by-name/xi/xiaomi_gateway3/package.nix
+++ pkgs/by-name/xi/xiaomi_gateway3/package.nix
@@ -3,18 +3,15 @@
   nix-update-script,
 }:
 
-home-assistant-custom-components.xiaomi_gateway3.overridePythonAttrs (old: {
-  version = "0-unstable-2024-11-29";
+home-assistant-custom-components.xiaomi_gateway3.overridePythonAttrs (old: rec {
+  version = "4.0.8";
 
   src = old.src.override {
-    owner = "CharmingCheung";
-    rev = "6518451b57325cb8c757978934308c2624b4f26b";
-    hash = "sha256-vPV298RqDehdAnTXreURzJB1N96NKKBaII0V1wxWc5c=";
+    rev = "v${version}";
+    hash = "sha256-VvuvOUldhmROTs1+YbCT7++VJ71GgGKRbHjqZxQQY0w=";
   };
 
-  passthru.enable = true;
-  passthru.isHomeAssistantComponent = true;
-  passthru.updateScript = nix-update-script {
-    extraArgs = [ "--version=branch=linp.sensor_occupy.es2" ];
+  passthru = (old.passthru or { }) // {
+    enable = true;
   };
 })
