66215d2c718cbf98c45b0e78714c4dde6335e96a
Zhong Jianxin <azuwis@gmail.com>
2025-01-05 11:58:44 +0800

    overlays: Remove some old commented overrides

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,34 +2,6 @@
 { inputs }:
 
 final: prev: {
-  # override
-  # https://github.com/NixOS/nixpkgs/issues/267536
-  # borgbackup = prev.borgbackup.overrideAttrs (
-  #   old:
-  #   final.lib.optionalAttrs (final.stdenv.isDarwin && final.stdenv.isx86_64) {
-  #     disabledTests = (old.disabledTests or [ ]) ++ [
-  #       "test_overwrite"
-  #       "test_sparse_file"
-  #     ];
-  #   }
-  # );
-
-  # cemu = prev.cemu.overrideAttrs (old: {
-  #   postPatch = (old.postPatch or "") + ''
-  #     sed -i '/\/\/ already connected\?/,+2 d' src/input/api/DSU/DSUControllerProvider.cpp
-  #   '';
-  # });
-
-  # https://github.com/intel/intel-vaapi-driver/pull/566
-  # intel-vaapi-driver = prev.intel-vaapi-driver.overrideAttrs (old: {
-  #   patches = (old.patches or [ ]) ++ [
-  #     (final.fetchpatch {
-  #       url = "https://github.com/intel/intel-vaapi-driver/commit/4206d0e15363d188f30f2f3dbcc212fef206fc1d.patch";
-  #       hash = "sha256-unCnAGM36sRcW4inaN21IqVOhHY9YB+iJYGgdFCxWQ0=";
-  #     })
-  #   ];
-  # });
-
   # disable fcitx5-configtool
   qt6Packages = prev.qt6Packages.overrideScope (
     qt6final: qt6prev: {
@@ -46,33 +18,6 @@ final: prev: {
       };
   };
   luaPackages = final.lua.pkgs;
-
-  # https://github.com/NixOS/nixpkgs/pull/79344
-  # mpv-unwrapped = prev.mpv-unwrapped.override {
-  #   inherit
-  #     (import
-  #       (builtins.fetchTarball {
-  #         # nixpkgs-24.05-darwin
-  #         url = "https://github.com/NixOS/nixpkgs/archive/ced0da1e7e7d50f1352bc6bdd25af8ae55eb3934.tar.gz";
-  #         sha256 = "01wqw1jsngicri7b09npg70xdzyrfq787kka9xx1q1h3p1jwnsag";
-  #       })
-  #       {
-  #         inherit (final.stdenv) system;
-  #       }
-  #     )
-  #     swift
-  #     ;
-  # };
-
-  # https://github.com/Mic92/nix-update/pull/269
-  # nix-update = prev.nix-update.overridePythonAttrs (old: {
-  #   version = "1.5.0-unstable-2024-08-21";
-  #   src = old.src.override {
-  #     rev = "737121eccb67542e8c004c64da833fede2e80c64";
-  #     hash = "sha256-xn0dC4M3mfItxP+s3/v3Hz/CSKp74VH/gMfufKxl9/4=";
-  #   };
-  #   passthru.updateScript = final.nix-update-script { extraArgs = [ "--version=branch" ]; };
-  # });
 
   # https://github.com/nix-community/nix-zsh-completions/pull/52
   nix-zsh-completions = prev.nix-zsh-completions.overrideAttrs (old: {
@@ -95,15 +40,6 @@ final: prev: {
   # };
   # python3Packages = final.python3.pkgs;
 
-  # sway-unwrapped = prev.sway-unwrapped.override {
-  #   wlroots = final.wlroots_0_16.overrideAttrs (old: {
-  #     postPatch =
-  #       (old.postPatch or "") + ''
-  #         substituteInPlace render/gles2/renderer.c --replace-fail "glFlush();" "glFinish();"
-  #       '';
-  #   });
-  # };
-
   # vimPlugins =
   #   prev.vimPlugins
   #   // final.lib.packagesFromDirectoryRecursive {
@@ -120,9 +56,9 @@ final: prev: {
       default = final.wallpapers.lake;
     };
 
-  # error: 'VK_DRIVER_ID_MESA_AGXV' was not declared in this scope
   yuzu-ea = final.lib.optionalAttrs final.stdenv.isLinux (
     inputs.yuzu.packages.${final.stdenv.system}.early-access.overrideAttrs (old: {
+      # error: 'VK_DRIVER_ID_MESA_AGXV' was not declared in this scope
       postPatch =
         (old.postPatch or "")
         + ''
--- overlays/jovian.nix
+++ overlays/jovian.nix
@@ -4,20 +4,4 @@ final: prev: {
     extraPkgs =
       pkgs: (if old ? extraPkgs then old.extraPkgs pkgs else [ ]) ++ [ pkgs.noto-fonts-cjk-sans ];
   });
-
-  # Workaround for vimPlugins.* not in jovia/nixpkgs yet.
-  # Remove it when jovian upgrades next time.
-  # vimPlugins = prev.vimPlugins // {
-  #   neogit = final.vimUtils.buildVimPlugin {
-  #     pname = "neogit";
-  #     version = "2024-05-26";
-  #     src = final.fetchFromGitHub {
-  #       owner = "NeogitOrg";
-  #       repo = "neogit";
-  #       rev = "70ad95be902ee69b56410a5cfc690dd03104edb3";
-  #       sha256 = "sha256-hkb33SOJJMqPXj8xJ1epZBDyH/OCX+MQom8jVPJEXyw=";
-  #     };
-  #     meta.homepage = "https://github.com/NeogitOrg/neogit/";
-  #   };
-  # };
 }
