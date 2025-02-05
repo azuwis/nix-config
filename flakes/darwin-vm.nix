b527326f0e514f97aa87a3fed7d42982dfc6397e
Zhong Jianxin <azuwis@gmail.com>
2025-02-05 19:12:47 +0800

    darwin: Workaround vm-mfoc key override problem

--- flakes/darwin-vm.nix
+++ flakes/darwin-vm.nix
@@ -1,35 +1,47 @@
 { withSystem, ... }:
 
 {
+  # Need `nix.linux-builder.enable = true`
   flake.packages.aarch64-darwin = withSystem "aarch64-darwin" (
     { pkgs, ... }:
     {
-      vm-mfoc = pkgs.darwin.linux-builder.override {
-        modules = [
-          {
-            environment.systemPackages = with pkgs; [
-              libnfc
-              mfoc-hardnested
+      vm-mfoc =
+        let
+          builder = pkgs.darwin.linux-builder.override {
+            modules = [
+              (
+                {
+                  pkgs,
+                  ...
+                }:
+                {
+                  environment.systemPackages = with pkgs; [
+                    libnfc
+                    mfoc-hardnested
+                  ];
+                  security.sudo = {
+                    execWheelOnly = true;
+                    wheelNeedsPassword = false;
+                  };
+                  users.users.builder.extraGroups = [ "wheel" ];
+                  virtualisation.darwin-builder = {
+                    hostPort = 31023;
+                    workingDirectory = "/var/lib/vm/mfoc";
+                  };
+                  # Use `system_profiler SPUSBDataType` to get vendor/product IDs
+                  # WIP: USB passthrough does not work
+                  virtualisation.qemu.options = [
+                    "-device usb-host,vendorid=0x067b,productid=0x2303"
+                  ];
+                }
+              )
             ];
-            security.sudo = {
-              execWheelOnly = true;
-              wheelNeedsPassword = false;
-            };
-            users.users.builder.extraGroups = [ "wheel" ];
-            virtualisation.darwin-builder = {
-              hostPort = 31023;
-              workingDirectory = "/var/lib/vm/mfoc";
-            };
-            # Use `system_profiler SPUSBDataType` to get vendor/product IDs
-            # WIP:
-            # 1) USB passthrough does not work
-            # 2) Multiple linux-builder override /etc/nix/builder_ed25519.pub file
-            virtualisation.qemu.options = [
-              "-device usb-host,vendorid=0x067b,productid=0x2303"
-            ];
-          }
-        ];
-      };
+          };
+        in
+        pkgs.writeShellScriptBin "create-builder" ''
+          ln -snf /var/lib/darwin-builder/keys /var/lib/vm/mfoc/keys
+          ${pkgs.lib.getExe builder};
+        '';
     }
   );
 }
