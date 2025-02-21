23a8af95394f6418aae1de1983c05b279c87ce12
Zhong Jianxin <azuwis@gmail.com>
2025-02-21 20:48:08 +0800

    rime-ice: Use stable version
    Upstream makes regular releases now

--- pkgs/by-name/ri/rime-ice/package.nix
+++ pkgs/by-name/ri/rime-ice/package.nix
@@ -1,19 +1,18 @@
 {
-  lib,
   stdenvNoCC,
   fetchFromGitHub,
   nix-update-script,
 }:
 
-stdenvNoCC.mkDerivation {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "rime-ice";
-  version = "2024.12.12-unstable-2025-02-11";
+  version = "2024.12.12";
 
   src = fetchFromGitHub {
     owner = "iDvel";
     repo = "rime-ice";
-    rev = "b8eb8d7bfee4cf55c761e809d49958eff3ba1687";
-    sha256 = "sha256-Z0Mjh/FNS4ZwAvfFTp7m5uOrJzor5EfkTOnFYIf508I=";
+    tag = finalAttrs.version;
+    sha256 = "sha256-2QZdlLGZwWIesbjYTE/2yhM1hHGVVp7jR02bR0oqxV0=";
   };
 
   installPhase = ''
@@ -22,10 +21,10 @@ stdenvNoCC.mkDerivation {
     sed '/^\.\.\.$/q' rime_ice.dict.yaml > $out/rime_ice.dict.yaml
   '';
 
-  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+  passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
+  meta = {
     description = "Rime pinyin dict from iDvel";
     homepage = "https://github.com/iDvel/rime-ice";
   };
-}
+})
