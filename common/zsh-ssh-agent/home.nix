453d790055671253db2a6176b02a7050814a5f82
Zhong Jianxin <azuwis@gmail.com>
2025-03-03 19:30:30 +0800

    zsh-ssh-agent: Compatible with POSIX shell

--- common/zsh-ssh-agent/home.nix
+++ common/zsh-ssh-agent/home.nix
@@ -18,17 +18,13 @@ in
     programs.zsh.initExtra = ''
       # ssh-agent
       start_ssh_agent() {
-        local socket="$HOME/.ssh/ssh-agent.socket"
-        if [ "$SSH_AUTH_SOCK" != "$socket" ]
-        then
-          if [ -e "$socket" ]
-          then
+        socket="$HOME/.ssh/ssh-agent.socket"
+        if [ "$SSH_AUTH_SOCK" != "$socket" ]; then
+          if [ -e "$socket" ]; then
             export SSH_AUTH_SOCK="$socket"
-            local code output
             output="$(ssh-add -l 2>/dev/null)"
             code="$?"
-            if [[ "$code" -eq 0 || ( "$code" -eq 1 && "$output" = "The agent has no identities." ) ]]
-            then
+            if [ "$code" -eq 0 ] || { [ "$code" -eq 1 ] && [ "$output" = "The agent has no identities." ]; }; then
               :
             else
               rm "$socket"
@@ -40,7 +36,7 @@ in
         fi
       }
       start_ssh_agent
-      unfunction start_ssh_agent
+      unset -f start_ssh_agent
     '';
   };
 }
