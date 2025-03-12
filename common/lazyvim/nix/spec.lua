b6d724e7a9199ab80b2a21f5c8d2b0d3708bd4c8
Zhong Jianxin <azuwis@gmail.com>
2025-03-13 00:04:35 +0800

    lazyvim/nix: Disable nixd options

--- common/lazyvim/nix/spec.lua
+++ common/lazyvim/nix/spec.lua
@@ -1,11 +1,11 @@
 local uv = vim.uv
 
-local dir
-if uv.fs_stat("/etc/nixos") then
-  dir = "/etc/nixos"
-else
-  dir = vim.fn.expand("~/.config/nixpkgs")
-end
+-- local dir
+-- if uv.fs_stat("/etc/nixos") then
+--   dir = "/etc/nixos"
+-- else
+--   dir = vim.fn.expand("~/.config/nixpkgs")
+-- end
 
 return {
   {
@@ -18,17 +18,23 @@ return {
               formatting = {
                 command = { "nixfmt" },
               },
-              options = {
-                nixos = {
-                  expr = string.format("(import %s { }).nixosConfigurations.nuc.options", dir),
-                },
-                ["nix-darwin"] = {
-                  expr = string.format("(import %s { }).darwinConfigurations.mbp.options", dir),
-                },
-                ["home-manager"] = {
-                  expr = string.format("(import %s { }).homeConfigurations.azuwis.options", dir),
-                },
-              },
+              -- Disable for now, use lots of memory, the default exprs shoud be enough
+              -- https://github.com/nix-community/nixd/blob/065dcb4cb2f8269d6d15d2b2491a79cff47f9550/nixd/lib/Controller/LifeTime.cpp#L26-L37
+              -- options = {
+              --   nixos = {
+              --     expr = string.format("(import %s { }).nixosConfigurations.nuc.options", dir),
+              --   },
+              --   ["nix-darwin"] = {
+              --     expr = string.format("(import %s { }).darwinConfigurations.mbp.options", dir),
+              --   },
+              --   ["home-manager"] = {
+              --     expr = string.format("(import %s { }).homeConfigurations.azuwis.options", dir),
+              --     expr = string.format(
+              --       'let inputs = import %s/inputs; pkgs = import inputs.nixpkgs { }; hm = import "${inputs.home-manager}/modules" { inherit pkgs; configuration.home = { stateVersion = "24.11"; username = "foo"; homeDirectory = "/home/foo"; }; }; in hm.options',
+              --       dir
+              --     ),
+              --   },
+              -- },
             },
           },
         },
