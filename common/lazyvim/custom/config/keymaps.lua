f85c3e8427aface732329040cc8abe9c0b1236f4
Zhong Jianxin <azuwis@gmail.com>
2025-03-15 16:30:59 +0800

    lazyvim: Remove terminal keymap overrides
    LazyVim now use Snacks.terminal, and don't have the ctrl_hjkl option.
    
    <C-hjkl> to switch windows is actually useful

--- common/lazyvim/custom/config/keymaps.lua
+++ common/lazyvim/custom/config/keymaps.lua
@@ -1,14 +1,4 @@
-local Util = require("lazyvim.util")
 local map = vim.keymap.set
 
 map("n", "<leader>fs", "<cmd>update<cr>", { desc = "Save file if modified" })
 map("n", "<leader>fS", "<cmd>execute ':silent w !sudo tee % > /dev/null' | :edit!<cr>", { desc = "Sudo save" })
-
-local lazyterm = function()
-  Util.terminal(nil, { cwd = Util.root(), ctrl_hjkl = false })
-end
-map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function()
-  Util.terminal(nil, { ctrl_hjkl = false })
-end, { desc = "Terminal (cwd)" })
-map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
