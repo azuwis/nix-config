4b0dc45ac9568378d115a209a3664ab0f3500397
Zhong Jianxin <azuwis@gmail.com>
2025-03-15 21:52:43 +0800

    pkgs: Allow passing any args to nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,15 +1,15 @@
-{
-  # Required by nixpkgs-hammering
-  overlays ? [ ],
-  ...
-}:
+{ ... }@args:
 
 let
   inputs = import ../inputs;
-  pkgs = import "${inputs.nixpkgs.outPath}" {
-    config = import ../config.nix;
-    overlays = (import ../overlays { }) ++ overlays;
-  };
+  pkgs = import "${inputs.nixpkgs.outPath}" (
+    args
+    // {
+      config = import ../config.nix // args.config or { };
+      # Required by nixpkgs-hammering
+      overlays = (import ../overlays { }) ++ args.overlays or [ ];
+    }
+  );
 in
 # nix-update expect nixpkgs-like repo
 # https://discourse.nixos.org/t/25274
