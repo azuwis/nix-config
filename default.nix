0d9ebcc82acf886ae013f822ec6ea3fcf03bb218
Zhong Jianxin <azuwis@gmail.com>
2025-03-15 23:50:43 +0800

    nixos: Add comments about why no nixpkgs/pkgs arg

--- default.nix
+++ default.nix
@@ -6,6 +6,8 @@ let
   mkNixos =
     host:
     import "${nixpkgs}/nixos/lib/eval-config.nix" {
+      # No need to pass nixpkgs or pkgs, eval-config.nix will import the same
+      # nixpkgs that contains it, see nixpkgs/nixos/modules/misc/nixpkgs.nix
       system = null;
       modules = [
         (./hosts + "/${host}.nix") # compatible syntax with nix 2.3
