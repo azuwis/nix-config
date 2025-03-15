7911729309ef6330088e1c34144cd33c39b8f407
Zhong Jianxin <azuwis@gmail.com>
2025-03-15 16:35:30 +0800

    lazyvim: Add <leader>fq keymap

--- common/lazyvim/custom/config/keymaps.lua
+++ common/lazyvim/custom/config/keymaps.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
 
 map("n", "<leader>fs", "<cmd>update<cr>", { desc = "Save file if modified" })
+map("n", "<leader>fq", "<cmd>wq<cr>", { desc = "Save and quit" })
 map("n", "<leader>fS", "<cmd>execute ':silent w !sudo tee % > /dev/null' | :edit!<cr>", { desc = "Sudo save" })
