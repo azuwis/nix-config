cdc98cd9e3d9e8ac2648d9e50768a84ed989755d
Zhong Jianxin <azuwis@gmail.com>
2025-03-21 10:49:54 +0800

    flake: Drop home-manager

--- flake.nix
+++ flake.nix
@@ -12,10 +12,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     };
