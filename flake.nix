860af63d9930373aa7900fd0a33970fa1f81915e
Zhong Jianxin <azuwis@gmail.com>
2025-03-24 22:33:45 +0800

    flakes: Drop

--- flake.nix
+++ flake.nix
@@ -1,27 +1,3 @@
 {
-  inputs = {
-    agenix = {
-      url = "github:ryantm/agenix";
-      flake = false;
-    };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
-  };
-
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-      imports = [ ./flakes ];
-    }
-    // import ./default.nix { };
+  outputs = _: import ./default.nix { };
 }
--- flakes/default.nix
+++
@@ -1,7 +0,0 @@
-{ inputs, ... }:
-
-{
-  imports = [
-    ./overlays.nix
-  ];
-}
