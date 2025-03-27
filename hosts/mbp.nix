2f96cf6d106d8ea765b9c46f793a2857ec433df4
Zhong Jianxin <azuwis@gmail.com>
2025-03-27 20:45:12 +0800

    mbp: Fix comment after refactor

--- hosts/mbp.nix
+++ hosts/mbp.nix
@@ -13,7 +13,7 @@
   networking.hostName = "mbp";
   my.desktop.enable = true;
 
-  # For `flakes/darwin-vm.nix`
+  # For `devshells/darwin-vm-pn532.nix`
   # nix.linux-builder.enable = true;
   # Workaround `sandbox-exec: pattern serialization length <number> exceeds maximum (65535)`
   # nix.settings.extra-sandbox-paths = [ "/nix/store" ];
