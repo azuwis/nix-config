cf6882f0c403b40396078a6c693612a55cc139c6
Zhong Jianxin <azuwis@gmail.com>
2025-03-27 20:08:14 +0800

    torrent-ratio: Use finalAttrs pattern

--- pkgs/by-name/to/torrent-ratio/package.nix
+++ pkgs/by-name/to/torrent-ratio/package.nix
@@ -4,18 +4,18 @@
   nix-update-script,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "torrent-ratio";
   version = "0.8";
 
   src = fetchFromGitHub {
     owner = "azuwis";
     repo = "torrent-ratio";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-UYlR4ArbY8oDq4dHUwGanLOmQS6QfMYwcBmlV6tzJ5s=";
   };
 
   vendorHash = "sha256-yDaALsAg+j9gQOTx4kdeCDE85talRsbbXzo/btdryYc=";
 
   passthru.updateScript = nix-update-script { };
-}
+})
