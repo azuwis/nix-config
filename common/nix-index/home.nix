b7284e87f6ab0b382f9c2355348fbf18350da5b1
Zhong Jianxin <azuwis@gmail.com>
2025-03-30 19:30:13 +0800

    common/nix-index: Wrap nix-locate to use full database
    And keep command-not-found to use small database

--- common/nix-index/home.nix
+++ common/nix-index/home.nix
@@ -7,20 +7,40 @@
 
 let
   inherit (lib) mkEnableOption mkIf;
-  cfg = config.my.nix-index;
+  inherit (lib.hm.shell) mkBashIntegrationOption mkZshIntegrationOption;
   inputs = import ../../inputs;
+  cfg = config.my.nix-index;
 
   nix-index-database = import inputs.nix-index-database.outPath { inherit pkgs; };
+
+  shellInit = ''
+    # nix-index
+    source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
+  '';
+
+  # Wrap nix-locate to use full database
+  nix-index-full = pkgs.runCommand "nix-index-full" { buildInputs = [ pkgs.makeWrapper ]; } ''
+    mkdir -p $out/share/misc
+    ln -s ${nix-index-database.nix-index-database} $out/share/misc/files
+    makeWrapper ${pkgs.nix-index}/bin/nix-locate $out/bin/nix-locate \
+      --set NIX_INDEX_DATABASE $out/share/misc
+  '';
 in
 {
   options.my.nix-index = {
     enable = mkEnableOption "nix-index";
+    enableBashIntegration = mkBashIntegrationOption { inherit config; };
+    enableZshIntegration = mkZshIntegrationOption { inherit config; };
   };
 
   config = mkIf cfg.enable {
-    programs.nix-index.enable = true;
-
+    # Use small database for command-not-found
     home.file."${config.xdg.cacheHome}/nix-index/files".source =
       nix-index-database.nix-index-small-database;
+
+    programs.bash.initExtra = mkIf cfg.enableBashIntegration shellInit;
+    programs.zsh.initExtra = mkIf cfg.enableZshIntegration shellInit;
+
+    home.packages = [ nix-index-full ];
   };
 }
