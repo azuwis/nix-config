6aecb50534b0b6f0f5b0c9ba4233f83fd3435457
Zhong Jianxin <azuwis@gmail.com>
2025-04-08 19:47:19 +0800

    apps/devShell: More docs

--- apps/default.nix
+++ apps/default.nix
@@ -2,6 +2,9 @@
   pkgs ? import ../pkgs { },
 }:
 
+# nix run -f apps <name>
+# nix run ./flakes#<name>
+# nix run 'github:azuwis/nix-config?dir=flakes#<name>'
 builtins.removeAttrs (pkgs.lib.packagesFromDirectoryRecursive {
   inherit (pkgs) callPackage;
   directory = ./.;
--- shell.nix
+++ shell.nix
@@ -4,6 +4,8 @@ let
   lib = import ./lib;
 in
 
+# nix-shell -A <name>
+# nix develop ./flakes#<name>
 # nix-shell --option tarball-ttl 1 https://github.com/azuwis/nix-config/archive/refs/heads/master.zip -A <name>
 # nix develop 'github:azuwis/nix-config?dir=flakes#<name>'
 lib.packagesFromDirectoryRecursive {
