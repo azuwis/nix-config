c327b6b654b7f9c539dc902f0062edb74fda5443
Zhong Jianxin <azuwis@gmail.com>
2025-05-14 20:30:40 +0800

    yambar-git: Switch to fork
    Not developed anymore upstream

--- pkgs/by-name/ya/yambar-git/package.nix
+++ pkgs/by-name/ya/yambar-git/package.nix
@@ -2,14 +2,12 @@
 
 yambar.overrideAttrs (old: {
   pname = "yambar-git";
-  version = "1.11.0-unstable-2025-03-20";
+  version = "1.11.0-unstable-2025-05-14";
 
-  # yambar crash, https://codeberg.org/dnkl/yambar/issues/300
-  # tag: add '/N' formatter, https://codeberg.org/dnkl/yambar/pulls/403
-  # Add niri-workspaces and niri-language modules https://codeberg.org/dnkl/yambar/pulls/405
   src = old.src.override {
-    rev = "43e19446071c49016d571bdb432e1a6e1e1e8778";
-    hash = "sha256-erWbwhVj97JGdxwnPICmwAM2GeWyqUteI0VPhqzKWt4=";
+    owner = "azuwis";
+    rev = "123ec40cba2423ddc5a197626844d279182e89ee";
+    hash = "sha256-FWCqhX2sqr7L6/LKNXYz8OlI0TjXfC3YGvoPsqu9G68=";
   };
 
   passthru = (old.passthru or { }) // {
