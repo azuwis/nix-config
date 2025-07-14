79b4f987d11dcad545be8dc96745847d0bade54b
Zhong Jianxin <azuwis@gmail.com>
2025-07-14 21:30:58 +0800

    jujutsu: Split static config to config.toml

--- common/jujutsu/default.nix
+++ common/jujutsu/default.nix
@@ -7,7 +7,13 @@
 }:
 
 let
-  inherit (lib) mkEnableOption mkIf mkOption;
+  inherit (lib)
+    importTOML
+    mkEnableOption
+    mkIf
+    mkMerge
+    mkOption
+    ;
   cfg = config.wrappers.jujutsu;
   tomlFormat = pkgs.formats.toml { };
   scripts = ./scripts;
@@ -26,95 +32,31 @@ in
     environment.systemPackages = [
       (wrapper {
         basePackage = pkgs.jujutsu;
-        env.JJ_CONFIG.value = tomlFormat.generate "jujutsu-config" cfg.settings;
+        env.JJ_CONFIG.value = tomlFormat.generate "jujutsu-config.toml" cfg.settings;
       })
     ];
 
-    wrappers.jujutsu.settings = {
-      aliases = {
-        fe = [
-          "git"
-          "fetch"
-        ];
-        ld = [
-          "log"
-          "--template"
-          "log_detailed"
-        ];
-        ls = [
-          "log"
-          "--template"
-          "log_stat"
-        ];
-        ni = [
-          "util"
-          "exec"
-          "--"
-          "${scripts}/ni"
-        ];
-        pr = [
-          "util"
-          "exec"
-          "--"
-          "${scripts}/pr"
-        ];
-        pu = [
-          "git"
-          "push"
-        ];
-      };
-      colors = {
-        "diff removed token" = {
-          underline = false;
+    wrappers.jujutsu.settings = mkMerge [
+      (importTOML ./config.toml)
+      {
+        aliases = {
+          ni = [
+            "util"
+            "exec"
+            "--"
+            "${scripts}/ni"
+          ];
+          pr = [
+            "util"
+            "exec"
+            "--"
+            "${scripts}/pr"
+          ];
         };
-        "diff added token" = {
-          underline = false;
+        user = {
+          inherit (config.my) email name;
         };
-      };
-      merge-tools = {
-        nvim.edit-args = [
-          "-c"
-          "DiffEditor $left $right $output"
-        ];
-      };
-      # https://jj-vcs.github.io/jj/latest/templates/
-      # https://github.com/jj-vcs/jj/blob/main/cli/src/config/templates.toml
-      # log_custom modified from builtin_log_compact
-      template-aliases = {
-        description_placeholder = ''label("description placeholder", "(no description)")'';
-        log_custom = ''
-          if(root,
-            format_root_commit(self),
-            label(if(current_working_copy, "working_copy"),
-              concat(
-                format_short_commit_header(self) ++ "\n",
-                separate(" ",
-                  if(empty, label("empty", "(empty)")),
-                  if(description,
-                    description.first_line(),
-                    if(empty, label("empty", description_placeholder), diff.summary()),
-                  ),
-                ),
-              ),
-            )
-          )
-        '';
-        log_detailed = ''concat(builtin_log_detailed, diff.stat(120), "\n")'';
-        log_stat = "concat(builtin_log_compact, diff.summary())";
-      };
-      templates = {
-        log = "log_custom";
-      };
-      ui = {
-        default-command = "log";
-        diff-editor = ":builtin";
-        # diff-editor = "nvim";
-        diff-instructions = false;
-        diff.format = "git";
-      };
-      user = {
-        inherit (config.my) email name;
-      };
-    };
+      }
+    ];
   };
 }
