73593bfba7051fdc18f958fbe9db5d662150b0b0
Zhong Jianxin <azuwis@gmail.com>
2025-07-17 20:34:56 +0800

    xiaomi_miot: Enable and update latest git commit
    Need captcha support

--- pkgs/by-name/xi/xiaomi_miot/package.nix
+++ pkgs/by-name/xi/xiaomi_miot/package.nix
@@ -1,15 +1,16 @@
 { home-assistant-custom-components, nix-update-script }:
 
 home-assistant-custom-components.xiaomi_miot.overridePythonAttrs (old: rec {
-  version = "1.0.9";
+  version = "1.0.19-unstable-2025-07-03";
 
   src = old.src.override {
-    rev = "v${version}";
-    hash = "sha256-2XCm7XEKQgoe8myAgPnYCOO+XdLTAl8NtAVCBMJGqDc=";
+    rev = "aa99f3885405ede068dd117b5b2657184586ddcb";
+    hash = "sha256-kifImeiytb7t+eyRCmHKPR+IkXkpsRKg0yikIQLX+40=";
   };
 
   passthru = (old.passthru or { }) // {
-    enable = false;
+    enable = true;
+    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
   };
 
   meta = (old.meta or { }) // {
