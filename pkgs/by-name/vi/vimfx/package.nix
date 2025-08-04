ba0f935edbe460aa8f0c275330fcebceeb17a365
Zhong Jianxin <azuwis@gmail.com>
2025-08-04 20:43:37 +0800

    vimfx: 0.27.3 -> 0.27.4

--- pkgs/by-name/vi/vimfx/package.nix
+++ pkgs/by-name/vi/vimfx/package.nix
@@ -2,10 +2,10 @@
 
 buildFirefoxXpiAddon rec {
   pname = "vimfx";
-  version = "0.27.3";
+  version = "0.27.4";
   addonId = "VimFx-unlisted@akhodakivskiy.github.com";
   url = "https://github.com/akhodakivskiy/VimFx/releases/download/v${version}/VimFx.xpi";
-  sha256 = "sha256-Tn3WPvWE18TsNipbIdjjdIXJDtpVxD+QyK4hZ8Skak8=";
+  sha256 = "sha256-X9Ax7H0UYk+WGz362dSHCrAXBDmC0/rWjqppHywj/zE=";
   passthru.updateScript = nix-update-script {
     extraArgs = [
       "--override-filename"
