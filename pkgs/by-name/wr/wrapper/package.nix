76d5b56ebff538ceffb8bc7f482ba056e442ca3f
Zhong Jianxin <azuwis@gmail.com>
2025-08-21 19:07:04 +0800

    wrapper: Support man output

--- pkgs/by-name/wr/wrapper/package.nix
+++ pkgs/by-name/wr/wrapper/package.nix
@@ -9,23 +9,44 @@
   env ? { },
 }:
 
+let
+  hasMan = builtins.hasAttr "man" package;
+in
+
 runCommand package.name
   {
-    inherit (package) pname version meta;
+    inherit (package) pname version;
 
     nativeBuildInputs = [
       makeWrapper
     ];
+
+    outputs = [
+      "out"
+    ]
+    ++ (lib.optional hasMan "man");
+
+    meta = (package.meta or { }) // {
+      outputsToInstall = [
+        "out"
+      ]
+      ++ (lib.optional hasMan "man");
+    };
   }
-  ''
-    mkdir -p "$out"
-    for file in "${package}"/*; do
-      basename=$(basename "$file")
-      if [ "$basename" != "bin" ]; then
-        ln -sv "$file" "$out/"
-      fi
-    done
-    exe="${lib.getExe package}"
-    makeWrapper "$exe" "$out/bin/$(basename "$exe")" \
-      ${lib.concatMapAttrsStringSep " " (name: value: "--set ${name} ${value}") env}
-  ''
+  (
+    ''
+      mkdir -p "$out"
+      for file in "${package}"/*; do
+        basename=$(basename "$file")
+        if [ "$basename" != "bin" ]; then
+          ln -sv "$file" "$out/"
+        fi
+      done
+      exe="${lib.getExe package}"
+      makeWrapper "$exe" "$out/bin/$(basename "$exe")" \
+        ${lib.concatMapAttrsStringSep " " (name: value: "--set ${name} ${value}") env}
+    ''
+    + lib.optionalString hasMan ''
+      ln -s ${package.man} "$man"
+    ''
+  )
