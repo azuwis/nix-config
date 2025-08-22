b130ec4d8ad657c4354723bbde57d57cf6106764
Zhong Jianxin <azuwis@gmail.com>
2025-08-22 21:19:08 +0800

    zsh: Make less exported variables of fzf

--- common/zsh/fzf.nix
+++ common/zsh/fzf.nix
@@ -22,18 +22,17 @@ in
 
     programs.zsh.interactiveShellInit = ''
       # fzf
-      if [[ $options[zle] = on ]]; then
-        eval "$(fzf --zsh)"
-      fi
-      export FZF_COMPLETION_TRIGGER='*'
-      export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
-      export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+      FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
+      FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
       _fzf_compgen_path() {
         fd --hidden --follow --exclude '.git' . "$1"
       }
       _fzf_compgen_dir() {
         fd --type d --hidden --follow --exclude '.git' . "$1"
       }
+      if [[ $options[zle] = on ]]; then
+        eval "$(fzf --zsh)"
+      fi
     '';
   };
 }
