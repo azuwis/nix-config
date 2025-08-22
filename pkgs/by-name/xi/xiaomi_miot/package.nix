a3f1611261304505f5589dcf719fcb36c1489f62
Zhong Jianxin <azuwis@gmail.com>
2025-08-22 20:41:25 +0800

    xiaomi_miot: 1.0.19-unstable-2025-07-03 -> 1.0.20

--- pkgs/by-name/xi/xiaomi_miot/package.nix
+++ pkgs/by-name/xi/xiaomi_miot/package.nix
@@ -1,16 +1,16 @@
 { home-assistant-custom-components, nix-update-script }:
 
 home-assistant-custom-components.xiaomi_miot.overridePythonAttrs (old: rec {
-  version = "1.0.19-unstable-2025-07-03";
+  version = "1.0.20";
 
   src = old.src.override {
-    rev = "aa99f3885405ede068dd117b5b2657184586ddcb";
-    hash = "sha256-kifImeiytb7t+eyRCmHKPR+IkXkpsRKg0yikIQLX+40=";
+    rev = "v${version}";
+    hash = "sha256-qn//le4zaS7URP4pWofwsA4FbB20DK7iRRUn8NWzwAI=";
   };
 
   passthru = (old.passthru or { }) // {
     enable = true;
-    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+    updateScript = nix-update-script { };
   };
 
   meta = (old.meta or { }) // {
