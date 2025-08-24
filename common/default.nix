ff7f85ef684bf68ed401f66af5dbadab44628c60
Zhong Jianxin <azuwis@gmail.com>
2025-08-24 21:16:25 +0800

    common: Sort, no-op

--- common/default.nix
+++ common/default.nix
@@ -31,22 +31,20 @@ in
   programs.direnv.nix-direnv.enable = true;
   programs.git.enable = true;
   programs.jujutsu.enable = true;
-  programs.nix-index.enable = true;
-  programs.yazi.enable = true;
-
-  programs.lazyvim.enable = true;
   programs.lazyvim.ansible.enable = true;
   programs.lazyvim.bash.enable = true;
   programs.lazyvim.custom.enable = true;
+  programs.lazyvim.enable = true;
   programs.lazyvim.helm.enable = true;
   programs.lazyvim.mini-files.enable = true;
   programs.lazyvim.neogit.enable = true;
   programs.lazyvim.nix.enable = true;
   programs.lazyvim.nord.enable = true;
   programs.lazyvim.terraform.enable = true;
-  # programs.lazyvim.yaml.enable = true;
   # programs.lazyvim.update-nix-fetchgit.enable = true;
-
+  # programs.lazyvim.yaml.enable = true;
+  programs.nix-index.enable = true;
+  programs.yazi.enable = true;
   programs.zsh.enable = true;
   programs.zsh.fzf.enable = true;
   programs.zsh.pure-prompt.enable = true;
