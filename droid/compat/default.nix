e083fe4ba685fddc05acfa71a6b48a4cb518bc2d
Zhong Jianxin <azuwis@gmail.com>
2025-08-24 19:13:52 +0800

    droid: Move nixos modules into compat/default.nix

--- droid/compat/default.nix
+++ droid/compat/default.nix
@@ -7,9 +7,26 @@
 
 let
   inherit (lib) mkOption;
+  inputs = import ../../inputs;
+  modulesPath = inputs.nixpkgs.outPath + "/nixos/modules";
 in
 
 {
+  imports = builtins.map (path: modulesPath + path) [
+    "/config/shells-environment.nix"
+    "/misc/extra-arguments.nix"
+    "/misc/meta.nix"
+    "/programs/bash/bash.nix"
+    "/programs/command-not-found/command-not-found.nix"
+    "/programs/direnv.nix"
+    "/programs/fish.nix"
+    "/programs/git.nix"
+    "/programs/nix-index.nix"
+    "/programs/xonsh.nix"
+    "/programs/yazi.nix"
+    "/programs/zsh/zsh.nix"
+  ];
+
   options = {
     documentation.man.generateCaches = mkOption { };
     environment.pathsToLink = mkOption { };
--- droid/default.nix
+++ droid/default.nix
@@ -8,33 +8,16 @@
 let
   inherit (import ../lib/my.nix) getModules;
   inherit (lib) mkAliasOptionModule;
-  inputs = import ../inputs;
-  modulesPath = inputs.nixpkgs.outPath + "/nixos/modules";
 in
 
 {
-  imports =
-    builtins.map (path: modulesPath + path) [
-      "/config/shells-environment.nix"
-      "/misc/extra-arguments.nix"
-      "/misc/meta.nix"
-      "/programs/bash/bash.nix"
-      "/programs/command-not-found/command-not-found.nix"
-      "/programs/direnv.nix"
-      "/programs/fish.nix"
-      "/programs/git.nix"
-      "/programs/nix-index.nix"
-      "/programs/xonsh.nix"
-      "/programs/yazi.nix"
-      "/programs/zsh/zsh.nix"
-    ]
-    ++ [
-      (mkAliasOptionModule [ "environment" "systemPackages" ] [ "environment" "packages" ])
-      ../common
-      ../common/nixpkgs
-      ../common/registry
-    ]
-    ++ getModules [ ./. ];
+  imports = [
+    (mkAliasOptionModule [ "environment" "systemPackages" ] [ "environment" "packages" ])
+    ../common
+    ../common/nixpkgs
+    ../common/registry
+  ]
+  ++ getModules [ ./. ];
 
   user.shell = "${pkgs.zsh}/bin/zsh";
 
