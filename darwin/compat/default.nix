6e6ffc38bf653f2ac3fb1f3cd85c3087246f30bb
Zhong Jianxin <azuwis@gmail.com>
2025-09-01 20:28:28 +0800

    darwin: Use zsh module from nixos

--- darwin/compat/default.nix
+++ darwin/compat/default.nix
@@ -11,9 +11,14 @@ let
 in
 
 {
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/doc/manual/development/replace-modules.section.md
+  # zsh module from nix-darwin are outdated, use the one from nixos
+  disabledModules = [ "programs/zsh" ];
+
   imports = builtins.map (path: modulesPath + path) [
     "/programs/command-not-found/command-not-found.nix"
     "/programs/git.nix"
     "/programs/yazi.nix"
+    "/programs/zsh/zsh.nix"
   ];
 }
