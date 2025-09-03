c600f07190a8c3ee98329cea2cfb601805059934
Zhong Jianxin <azuwis@gmail.com>
2025-09-03 20:52:55 +0800

    nvidia: nvidia-vaapi-driver already installed if nvidia enabled

--- nixos/nvidia/default.nix
+++ nixos/nvidia/default.nix
@@ -43,7 +43,6 @@ in
 
     (mkIf (cfg.firefox-fix && config.programs.firefox.enable) {
       # https://github.com/elFarto/nvidia-vaapi-driver
-      hardware.graphics.extraPackages = [ pkgs.nvidia-vaapi-driver ];
       programs.firefox.env.GDK_BACKEND = null;
       programs.firefox.env.MOZ_DISABLE_RDD_SANDBOX = "1";
       programs.firefox.settings = {
