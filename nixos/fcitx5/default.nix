06fbbb001d0def6ec5f77a80062cbf4aa269f69c
Zhong Jianxin <azuwis@gmail.com>
2025-09-08 21:22:58 +0800

    niri: Do not hardcode fcitx5 start, set it in fcitx5 module

--- nixos/fcitx5/default.nix
+++ nixos/fcitx5/default.nix
@@ -63,5 +63,8 @@ in
 
     hm.wayland.windowManager.sway.config.startup = [ { command = "fcitx5"; } ];
     hm.xsession.windowManager.i3.config.startup = [ { command = "fcitx5"; } ];
+    my.niri.extraConfig = ''
+      spawn-at-startup "fcitx5"
+    '';
   };
 }
