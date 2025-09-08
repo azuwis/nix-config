16972759238d75374d0fd9c3b75bf0b146aacbaa
Zhong Jianxin <azuwis@gmail.com>
2025-09-08 21:12:57 +0800

    fcitx5: Drop home-manager usage

--- nixos/fcitx5/default.nix
+++ nixos/fcitx5/default.nix
@@ -15,7 +15,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    hm.my.fcitx5.enable = true;
     i18n.inputMethod.enable = true;
     i18n.inputMethod.type = "fcitx5";
     i18n.inputMethod.fcitx5.addons = with pkgs; [
@@ -24,5 +23,45 @@ in
     ];
     i18n.inputMethod.fcitx5.plasma6Support = true;
     i18n.inputMethod.fcitx5.waylandFrontend = lib.mkDefault true;
+
+    i18n.inputMethod.fcitx5.settings = {
+      globalOptions = {
+        Behavior = {
+          ActiveByDefault = "True";
+        };
+        "Hotkey/EnumerateForwardKeys"."0" = "Control+Shift_L";
+      };
+      inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "keyboard-us";
+        };
+        "Groups/0/Items/0".Name = "shuangpin";
+        "Groups/0/Items/1".Name = "keyboard-us";
+      };
+      addons = {
+        classicui.globalSection = {
+          Font = "Sans 13";
+          Theme = "FluentDark-solid";
+        };
+        clipboard.globalSection = {
+          "Number of entries" = 30;
+        };
+        pinyin.globalSection = {
+          PinyinInPreedit = "True";
+          ShuangpinProfile = "Custom";
+        };
+        # punctuation.globalSection = {
+        #   HalfWidthPuncAfterLetterOrNumber = "False";
+        # };
+      };
+    };
+
+    environment.etc."xdg/fcitx5/pinyin/sp.dat".source = ./sp.dat;
+
+    hm.wayland.windowManager.sway.config.startup = [ { command = "fcitx5"; } ];
+    hm.xsession.windowManager.i3.config.startup = [ { command = "fcitx5"; } ];
   };
 }
