0e5f61800f83ef39ac33b0a1eca8b4911501ad87
Zhong Jianxin <azuwis@gmail.com>
2025-09-10 19:31:17 +0800

    cliphist: Drop home-manager usage

---
+++ nixos/cliphist/default.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.programs.cliphist;
+in
+{
+  options.programs.cliphist = {
+    enable = mkEnableOption "cliphist";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      cliphist
+      wl-clipboard
+    ];
+
+    my.wayland.startup.cliphist = [
+      "wl-paste"
+      "--watch"
+      "cliphist"
+      "store"
+    ];
+
+    my.sway.extraConfig = ''
+      bindsym $mod+p exec tmenu sh -c 'cliphist list | fzf --reverse --no-info | cliphist decode 2>/dev/null | wl-copy'
+    '';
+  };
+}
