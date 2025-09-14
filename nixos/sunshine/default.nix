30effdaa52afb44966877c11a319cb1b9e1e37c5
Zhong Jianxin <azuwis@gmail.com>
2025-09-14 21:00:20 +0800

    sunshine: Clean up comments

--- nixos/sunshine/default.nix
+++ nixos/sunshine/default.nix
@@ -74,6 +74,8 @@ in
     hardware.uinput.enable = true;
     users.users.${cfg.user}.extraGroups = [ "uinput" ];
 
+    # udevadm info -a [/sys/device/...|/dev/...]
+    # udevadm monitor --environment --property --udev
     # https://github.com/LizardByte/Sunshine/blob/master/src_assets/linux/misc/60-sunshine.rules
     # KERNEL=="hidraw*", ATTRS{name}=="Sunshine PS5 (virtual) pad", OWNER="${cfg.user}"
     services.udev.extraRules = ''
@@ -216,26 +218,5 @@ in
           ];
       };
     };
-
-    # services.seatd.enable = true;
-    # users.users.${cfg.user}.extraGroups = [ "seatd" ];
-
-    # environment.systemPackages = [ pkgs.seatd ];
-    # security.wrappers.seatd-launch = {
-    #   owner = "root";
-    #   group = "root";
-    #   source = "${pkgs.seatd}/bin/seatd-launch";
-    # };
-
-    # loginctl seat-status seat0
-    # udevadm info -a [/sys/device/...|/dev/...]
-    # udevadm monitor --environment --property --udev
-    # services.udev.extraRules = ''
-    #   TAG=="seat", SUBSYSTEMS=="drm", KERNELS=="card0", KERNEL=="card0-DP-1", ENV{ID_SEAT}="seat-sunshine"
-    #   SUBSYSTEMS=="input", ATTRS{id/product}=="4038", ATTRS{id/vendor}=="046d", ATTRS{name}=="Logitech Wireless Mouse PID:4038", TAG+="seat", TAG+="seat-sunshine", ENV{ID_SEAT}="seat-sunshine"
-    #   SUBSYSTEMS=="input", ATTRS{id/product}=="dead", ATTRS{id/vendor}=="beef", ATTRS{name}=="Touchscreen passthrough", TAG+="seat", TAG+="seat-sunshine", ENV{ID_SEAT}="seat-sunshine"
-    #   SUBSYSTEMS=="input", ATTRS{id/product}=="dead", ATTRS{id/vendor}=="beef", ATTRS{name}=="Keyboard passthrough", TAG+="seat", TAG+="seat-sunshine", ENV{ID_SEAT}="seat-sunshine"
-    #   SUBSYSTEMS=="input", ATTRS{id/product}=="028e", ATTRS{id/vendor}=="045e", ATTRS{name}=="Microsoft X-Box 360 pad", TAG+="seat", TAG+="seat-sunshine", ENV{ID_SEAT}="seat-sunshine"
-    # '';
   };
 }
