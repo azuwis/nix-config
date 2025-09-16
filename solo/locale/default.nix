08dcd60ebe74c71a31e65594aaba23f75f4adbb8
Zhong Jianxin <azuwis@gmail.com>
2025-09-16 20:55:00 +0700

    solo/locales: Only enable some locales
    Reduce closure size

--- solo/locale/default.nix
+++ solo/locale/default.nix
@@ -5,7 +5,17 @@
   ...
 }:
 
+let
+  glibcLocales = pkgs.glibcLocales.override {
+    allLocales = false;
+    locales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+    ];
+  };
+in
+
 {
   environment.variables.LANG = "en_US.UTF-8";
-  environment.variables.LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  environment.variables.LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
 }
