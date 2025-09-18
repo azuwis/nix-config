ebbfe3b5d7b2e0324c6c4c4055739a44a66aa54a
Zhong Jianxin <azuwis@gmail.com>
2025-09-18 19:32:19 +0800

    darwin/home: Use home.activate

--- darwin/home/default.nix
+++ darwin/home/default.nix
@@ -7,22 +7,6 @@
 
 {
   system.activationScripts.postActivation.text = ''
-    makeHomeEntry() {
-      source="$1"
-      target="${config.system.primaryUserHome}/$2"
-      if [ ! -e "$target" ] || { [ -L "$target" ] && [ "$(readlink -f "$target")" != "$source" ]; }; then
-        ln -sf "$source" "$target"
-      fi
-    }
-    ${lib.concatMapStringsSep "\n" (
-      homeEntry:
-      lib.escapeShellArgs [
-        "makeHomeEntry"
-        # Force local source paths to be added to the store
-        "${homeEntry.source}"
-        homeEntry.target
-      ]
-    ) (lib.attrValues config.home.file)}
-    unset -f makeHomeEntry
+    su "${config.system.primaryUser}" -c '${config.home.activate} "${config.system.primaryUserHome}" /run/current-system/sw/home' || true
   '';
 }
