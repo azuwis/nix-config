7698dcaf749d1b2829bbb0682495f2174a193261
Zhong Jianxin <azuwis@gmail.com>
2025-09-18 20:57:23 +0800

    droid: Update nix.extraOptions

--- droid/system/default.nix
+++ droid/system/default.nix
@@ -9,9 +9,15 @@
   android-integration.am.enable = true;
   environment.etcBackupExtension = ".bak";
   environment.motd = "";
+  # Need to keep sync with common/system/default.nix, since nix-on-droid does
+  # not support nix.settings
   nix.extraOptions = ''
+    accept-flake-config = false
     experimental-features = nix-command flakes
     flake-registry =
+    keep-outputs = true
+    log-lines = 25
+    tarball-ttl = 43200
   '';
   nix.package = pkgs.nix;
   system.stateVersion = "21.11";
