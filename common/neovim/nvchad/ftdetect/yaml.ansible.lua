1ccdf333ccdee91f5bc8eee154e714baaea80660
Zhong Jianxin <azuwis@gmail.com>
2025-09-19 00:14:01 +0800

    neovim/nvchad/vim: Drop, now using lazyvim

--- common/neovim/nvchad/ftdetect/yaml.ansible.lua
+++
@@ -1,8 +0,0 @@
-vim.filetype.add({
-  pattern = {
-    [".*/playbooks/.*%.yaml"] = "yaml.ansible",
-    [".*/playbooks/.*%.yml"] = "yaml.ansible",
-    [".*/roles/.*%.yaml"] = "yaml.ansible",
-    [".*/roles/.*%.yml"] = "yaml.ansible",
-  },
-})
--- common/neovim/nvchad/lua/custom/init.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.commentstring = "# %s"
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "NeogitCommitMessage" },
-  command = "exec 'norm gg' | startinsert!",
-})
