15c8ec7a32413d4f935bd0ff719a9dc51b4b5738
Zhong Jianxin <azuwis@gmail.com>
2025-09-24 23:06:02 +0800

    apps: Add some packages from solo

--- apps/default.nix
+++ apps/default.nix
@@ -3,6 +3,22 @@
   pkgs ? import ../pkgs { },
 }:
 
+let
+  eval = lib.evalModules {
+    modules = [
+      ../solo
+      {
+        nixpkgs = {
+          inherit pkgs;
+          overlays = lib.mkForce [ ];
+        };
+      }
+    ];
+  };
+
+  mkSoloPackage = name: eval.config.programs.${name}.finalPackage;
+in
+
 # nix run -f apps <name>
 # nix run ./flakes#<name>
 # nix run 'github:azuwis/nix-config?dir=flakes#<name>'
@@ -10,3 +26,7 @@ builtins.removeAttrs (lib.packagesFromDirectoryRecursive {
   inherit (pkgs) callPackage;
   directory = ./.;
 }) [ "default" ]
+// lib.genAttrs [
+  "jujutsu"
+  "lazyvim"
+] mkSoloPackage
