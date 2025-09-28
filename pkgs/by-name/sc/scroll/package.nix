58442094f789f0abecf1c52824c244744c3a4c19
Zhong Jianxin <azuwis@gmail.com>
2025-09-28 20:48:08 +0800

    scroll: Init

---
+++ pkgs/by-name/sc/scroll/package.nix
@@ -0,0 +1,25 @@
+{
+  scroll-unwrapped,
+  sway,
+  # Used by the NixOS module:
+  withBaseWrapper ? true,
+  extraSessionCommands ? "",
+  withGtkWrapper ? false,
+  extraOptions ? [ ], # E.g.: [ "--verbose" ]
+  isNixOS ? false,
+  enableXWayland ? true,
+  dbusSupport ? true,
+}:
+
+sway.override {
+  inherit
+    withBaseWrapper
+    extraSessionCommands
+    withGtkWrapper
+    extraOptions
+    isNixOS
+    enableXWayland
+    dbusSupport
+    ;
+  sway-unwrapped = scroll-unwrapped;
+}
