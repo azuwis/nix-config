69f930c9ee9683a3c7337826b3d90a5e639c379a
Zhong Jianxin <azuwis@gmail.com>
2025-10-08 20:47:56 +0800

    registry: Refactor, allow config entries

--- common/registry/default.nix
+++ common/registry/default.nix
@@ -7,44 +7,64 @@
 }:
 
 let
-  inherit (lib) mkEnableOption mkIf;
-  cfg = config.my.registry;
-in
-{
-  options.my.registry = {
-    enable = mkEnableOption "registry" // {
-      default = true;
-    };
-  };
+  cfg = config.settings.registry;
 
-  config = mkIf cfg.enable (
-    let
-      # Use toString to avoid copying nixpkgs to nix store if NPINS_OVERRIDE_nixpkgs is used
-      # nix.registry use builtins.toJSON, which will also copy paths to nix store
-      # https://nix.dev/manual/nix/2.26/language/builtins#builtins-toJSON
-      nixpkgs = toString inputs.nixpkgs.outPath;
-    in
-    {
-      # ${nixpkgs}/nixos/modules/misc/nixpkgs-flake.nix
-      # ${nixpkgs}/nixos/modules/config/nix-flakes.nix
-      environment.etc."nix/inputs/nixpkgs".source = nixpkgs;
-      nix.nixPath = [ "/etc/nix/inputs" ];
-      nix.registry = {
-        n.to = {
-          id = "nixpkgs";
-          type = "indirect";
-        };
-      }
-      // lib.optionalAttrs (lib.hasPrefix "/nix/store/" nixpkgs) {
+  inputs' =
+    lib.filterAttrs
+      (
         # Only set nixpkgs registry if in nix store
         # When NPINS_OVERRIDE_nixpkgs is used, nixpkgs registry will be something like
         # `path:/home/user/src/nixpkgs`, and then `nix run nixpkgs#foo` will copy the
         # entire nixpkgs repo into nix store
-        nixpkgs.to = {
-          type = "path";
-          path = nixpkgs;
-        };
+        name: value: builtins.elem name cfg.entries && lib.hasPrefix "/nix/store/" value
+      )
+      (
+        # Use toString to avoid copying inputs to nix store if NPINS_OVERRIDE_* is used
+        # nix.registry use builtins.toJSON, which will also copy paths to nix store
+        # https://nix.dev/manual/nix/2.26/language/builtins#builtins-toJSON
+        builtins.mapAttrs (_: value: builtins.toString value.outPath) inputs
+      );
+in
+
+{
+  options.settings.registry = {
+    enable = lib.mkEnableOption "registry" // {
+      default = true;
+    };
+
+    entries = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+  };
+
+  # ${nixpkgs}/nixos/modules/misc/nixpkgs-flake.nix
+  # ${nixpkgs}/nixos/modules/config/nix-flakes.nix
+  config = lib.mkIf cfg.enable {
+    environment.etc = lib.mapAttrs' (name: value: {
+      name = "nix/inputs/${name}";
+      value = {
+        source = value;
+      };
+    }) inputs';
+
+    nix.nixPath = [ "/etc/nix/inputs" ];
+
+    nix.registry = {
+      n.to = {
+        id = "nixpkgs";
+        type = "indirect";
       };
     }
-  );
+    // builtins.mapAttrs (_: value: {
+      to = {
+        type = "path";
+        path = value;
+      };
+    }) inputs';
+
+    settings.registry.entries = [
+      "nixpkgs"
+    ];
+  };
 }
