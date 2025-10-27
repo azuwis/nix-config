2a80f24ce2203d9ad4621a034cec972243f3cfb7
Zhong Jianxin <azuwis@gmail.com>
2025-10-27 20:12:21 +0800

    jovian: Fix `error: unused option: HYPERV`

--- overlays/jovian.nix
+++ overlays/jovian.nix
@@ -1,4 +1,19 @@
 final: prev: {
+  # Fix `error: unused option: HYPERV`, remove after nixos-25.11 release
+  linux_jovian = prev.linux_jovian.override {
+    argsOverride.structuredExtraConfig =
+      let
+        inputs = import ../inputs;
+        prevArgs = (
+          final.callPackage "${inputs.jovian-nixos.outPath}/pkgs/linux-jovian" { buildLinux = x: x; }
+        );
+      in
+      prevArgs.structuredExtraConfig
+      // {
+        HYPERV = final.lib.mkForce (final.lib.kernel.option final.lib.kernel.no);
+      };
+  };
+
   # https://github.com/Jovian-Experiments/Jovian-NixOS/pull/376
   steam = prev.steam.override (old: {
     extraPkgs =
