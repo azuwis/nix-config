d7907b3e33a86a5372731f18f8f4f5b30790ffd2
Zhong Jianxin <azuwis@gmail.com>
2025-11-09 20:45:21 +0800

    Revert "xiaomi_miot: Update to 1.1.1-unstable-2025-11-04"
    This reverts commit 4582c1ca0e31fb5aa461ef746788671ee0b75d01.
    
    Still has problem with bath heater's climate

--- pkgs/by-name/xi/xiaomi_miot/package.nix
+++ pkgs/by-name/xi/xiaomi_miot/package.nix
@@ -1,18 +1,16 @@
 { home-assistant-custom-components, nix-update-script }:
 
 home-assistant-custom-components.xiaomi_miot.overridePythonAttrs (old: rec {
-  version = "1.1.1-unstable-2025-11-04";
+  version = "1.0.22";
 
   src = old.src.override {
-    # rev = "v${version}";
-    rev = "4b5a8682b75c870eb6116bd07c3288906a6f04a7";
-    hash = "sha256-0B+rG2h2OMb363t0529/XjqZ9ORaT7XXk4qVyEAfNx8=";
+    rev = "v${version}";
+    hash = "sha256-hEBsrQXLKNIR68DYWVE8N2CQS96O70u4DFn/apvyrdk=";
   };
 
   passthru = (old.passthru or { }) // {
     enable = true;
-    # updateScript = nix-update-script { extraArgs = [ "--version-regex=^v([0-9.]+)$" ]; };
-    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+    updateScript = nix-update-script { extraArgs = [ "--version-regex=^v([0-9.]+)$" ]; };
   };
 
   meta = (old.meta or { }) // {
