e1035f77e4ed800d06ef324c209fa6b308341a3d
Zhong Jianxin <azuwis@gmail.com>
2025-11-16 21:57:02 +0800

    scinet: Use ExecStartPost/ExecStopPost to setup nftables
    So that when the service is stopped, nftables rules will be cleared, and
    won't break the network

--- nixos/scinet/default.nix
+++ nixos/scinet/default.nix
@@ -17,55 +17,66 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    networking.nftables.tables.shadowsocks-rust = {
-      family = "ip";
-      # https://en.wikipedia.org/wiki/Reserved_IP_addresses
-      content = ''
-        set local {
-          type ipv4_addr
-          flags interval
-          auto-merge
-          elements = {
-            0.0.0.0/8,
-            10.0.0.0/8,
-            100.64.0.0/10,
-            127.0.0.0/8,
-            169.254.0.0/16,
-            172.16.0.0/12,
-            192.0.0.0/24,
-            192.0.2.0/24,
-            192.88.99.0/24,
-            192.168.0.0/16,
-            198.18.0.0/15,
-            198.51.100.0/24,
-            203.0.113.0/24,
-            224.0.0.0/4,
-            240.0.0.0/4,
-            255.255.255.255/32,
-            ${builtins.replaceStrings [ "\n" ] [ "," ] (builtins.readFile pkgs.chnroutes2)}
-            }
-        }
-        chain output {
-          type nat hook output priority filter; policy accept;
-          oif lo accept
-          ip daddr @local accept
-          tcp dport { 22, 53, 80, 8000, 443, 587, 873, 993, 3000, 5222, 5228, 32200 } dnat to 127.0.0.1:7071
-        }
-      '';
-    };
-
     services.smartdns.enable = true;
 
     age.secrets."shadowsocks-rust-redir.json".file = "${inputs.my.outPath}/shadowsocks-rust-redir.json";
-    systemd.services.shadowsocks-rust = {
-      description = "shadowsocks-rust Daemon";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        DynamicUser = true;
-        LoadCredential = "config.json:${config.age.secrets."shadowsocks-rust-redir.json".path}";
-        ExecStart = "${lib.getBin cfg.package}/bin/sslocal --config \${CREDENTIALS_DIRECTORY}/config.json";
+    systemd.services.shadowsocks-rust =
+      let
+        ExecStartPost = pkgs.writeScript "shadowsocks-rust-post-start" ''
+          #! ${pkgs.nftables}/bin/nft -f
+          include "${ExecStopPost}"
+          table ip shadowsocks-rust {
+            set local {
+              type ipv4_addr
+              flags interval
+              auto-merge
+              elements = {
+                0.0.0.0/8,
+                10.0.0.0/8,
+                100.64.0.0/10,
+                127.0.0.0/8,
+                169.254.0.0/16,
+                172.16.0.0/12,
+                192.0.0.0/24,
+                192.0.2.0/24,
+                192.88.99.0/24,
+                192.168.0.0/16,
+                198.18.0.0/15,
+                198.51.100.0/24,
+                203.0.113.0/24,
+                224.0.0.0/4,
+                240.0.0.0/4,
+                255.255.255.255/32,
+                ${builtins.replaceStrings [ "\n" ] [ "," ] (builtins.readFile pkgs.chnroutes2)}
+              }
+            }
+            chain output {
+              type nat hook output priority filter; policy accept;
+              oif lo accept
+              ip daddr @local accept
+              tcp dport { 22, 53, 80, 8000, 443, 587, 873, 993, 3000, 5222, 5228, 32200 } dnat to 127.0.0.1:7071
+            }
+          }
+        '';
+        ExecStopPost = pkgs.writeScript "shadowsocks-rust-pre-stop" ''
+          #! ${pkgs.nftables}/bin/nft -f
+          table ip shadowsocks-rust
+          delete table ip shadowsocks-rust
+        '';
+      in
+      {
+        description = "shadowsocks-rust Daemon";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        # https://en.wikipedia.org/wiki/Reserved_IP_addresses
+        serviceConfig = {
+          inherit ExecStartPost ExecStopPost;
+          DynamicUser = true;
+          CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
+          AmbientCapabilities = [ "CAP_NET_ADMIN" ];
+          LoadCredential = "config.json:${config.age.secrets."shadowsocks-rust-redir.json".path}";
+          ExecStart = "${lib.getBin cfg.package}/bin/sslocal --config \${CREDENTIALS_DIRECTORY}/config.json";
+        };
       };
-    };
   };
 }
