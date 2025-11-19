0917261f5cbfb893ee52c2b35729e7e9a0c37cb7
Zhong Jianxin <azuwis@gmail.com>
2025-11-19 20:05:55 +0800

    yazi: Override optionalDeps to []
    This remove chafa/ffmpeg/resvg/... from solo environment, reduce 600M closure size.
    
    For desktop environment, tools like ffmpeg are installed, and still
    retain thoese yazi functions.

--- common/yazi/default.nix
+++ common/yazi/default.nix
@@ -5,18 +5,27 @@
   ...
 }:
 
+let
+  cfg = config.programs.yazi;
+in
+
 {
-  programs.yazi.settings = {
-    yazi = {
-      mgr.ratio = [
-        1
-        3
-        4
-      ];
-      preview = {
-        cache_dir = "~/.cache/yazi";
-        max_height = 1200;
-        max_width = 800;
+  config = lib.mkIf cfg.enable {
+    programs.yazi.package = pkgs.yazi.override {
+      optionalDeps = [ ];
+    };
+    programs.yazi.settings = {
+      yazi = {
+        mgr.ratio = [
+          1
+          3
+          4
+        ];
+        preview = {
+          cache_dir = "~/.cache/yazi";
+          max_height = 1200;
+          max_width = 800;
+        };
       };
     };
   };
