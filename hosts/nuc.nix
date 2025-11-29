f82f7154abe3ee4364165b51d79b01fc28d0fc49
Zhong Jianxin <azuwis@gmail.com>
2025-11-29 20:18:25 +0800

    nuc: Override niri mouse setting

--- hosts/nuc.nix
+++ hosts/nuc.nix
@@ -58,6 +58,9 @@
 
   programs.swayidle.enable = false;
 
+  # Niri does not have per input device setting yet, override `natural-scroll` for
+  # Microsoft All-in-One Media Keyboard
+  programs.niri.settings.input.mouse = [ ];
   programs.niri.extraConfig = ''
     output "HDMI-A-1" {
       mode "1920x1080@60.000"
