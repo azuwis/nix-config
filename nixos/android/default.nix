94ddee916deee4d248f8a92146c4c0440bb2d81d
Zhong Jianxin <azuwis@gmail.com>
2025-11-30 23:41:57 +0800

    android: Remove adbusers option, no longer needed

--- nixos/android/default.nix
+++ nixos/android/default.nix
@@ -12,16 +12,10 @@ in
 {
   options.programs.android = {
     enable = mkEnableOption "android";
-    adbusers = mkEnableOption ''
-      Whether to add my.user to adbusers group.
-
-      Adb udev rules add uaccess tag to android devices, so logined user will have permission to access Android phones, and don't need to enable this option.
-    '';
   };
 
   config = mkIf cfg.enable {
     programs.adb.enable = true;
     environment.systemPackages = [ pkgs.android-file-transfer ];
-    users.users.${config.my.user}.extraGroups = mkIf cfg.adbusers [ "adbusers" ];
   };
 }
