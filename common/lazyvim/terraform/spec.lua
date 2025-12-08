f24c3d78d98a18e992e8df8b70a58b4f20f0d81d
Zhong Jianxin <azuwis@gmail.com>
2025-12-08 21:24:43 +0800

    lazyvim/terraform: Don't need to disable this 2 plugins now
    telescope.nvim is optional

--- common/lazyvim/terraform/spec.lua
+++ common/lazyvim/terraform/spec.lua
@@ -1,5 +1,3 @@
 return {
   { import = "lazyvim.plugins.extras.lang.terraform" },
-  { "ANGkeith/telescope-terraform-doc.nvim", enabled = false },
-  { "cappyzawa/telescope-terraform.nvim", enabled = false },
 }
