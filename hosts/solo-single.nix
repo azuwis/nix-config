0186f725274899db6f745afdbbfaaf901c670e4f
Zhong Jianxin <azuwis@gmail.com>
2025-12-10 22:29:32 +0800

    solo-single: Add notes about nix install and setup

--- hosts/solo-single.nix
+++ hosts/solo-single.nix
@@ -1,3 +1,21 @@
+# Nix single user installation:
+# ```
+# curl -sL https://nixos.org/nix/install | sed '/trap cleanup/d' > install
+# bash install --no-channel-add --no-daemon --no-modify-profile
+# ```
+# Use `--no-modify-profile` to prevent adding nix profile loading script to
+# ~/.bash_profile, this file is for login shell, adding nix profile will
+# shallow coreutils, etc, may cause problem for original system, such as, Steam
+# won't start in desktop mode on Steam Deck
+#
+# For interactive shell use only, manually add this to ~/.bashrc:
+# ```
+# if [[ $- = *i* ]]; then
+#   if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi
+#   exec -l zsh
+# fi
+# ```
+
 {
   config,
   lib,
