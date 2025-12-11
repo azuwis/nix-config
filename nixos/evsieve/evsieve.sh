bdde6b96d9e5665d2b2cc45a52d242e031a85bf3
Zhong Jianxin <azuwis@gmail.com>
2025-12-11 22:41:40 +0800

    evsieve: Replace mode+select for mangohud
    The previous key combo was for toggle sway pointer_accel in sunshine, no
    longer needed

--- nixos/evsieve/evsieve.sh
+++ nixos/evsieve/evsieve.sh
@@ -4,12 +4,13 @@ IFS='-' read -r id kernel <<<"$1"
 
 case "$id" in
 054c0ce6) # dualsense
+  # rightshift+f10: MangoHud cycle between presets
   exec evsieve --input "/dev/input/$kernel" \
     --hook btn:mode btn:north exec-shell="gamefzf" \
     --hook btn:mode btn:east send-key=key:esc@kb \
     --hook btn:mode btn:west send-key=key:space@kb \
     --hook btn:mode btn:south send-key=key:enter@kb \
-    --hook btn:mode btn:select send-key=key:leftmeta@kb send-key=key:leftshift@kb send-key=key:m@kb \
+    --hook btn:mode btn:select send-key=key:rightshift@kb send-key=key:f10@kb \
     --hook btn:mode btn:start send-key=key:backspace@kb \
     --hook btn:mode btn:tl send-key=key:up@kb \
     --hook btn:mode btn:tr send-key=key:down@kb \
