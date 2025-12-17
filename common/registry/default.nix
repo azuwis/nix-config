ff0bb323dab3ad4b7b219a31fccb3c9cbcec4d6a
Zhong Jianxin <azuwis@gmail.com>
2025-12-17 20:45:19 +0800

    registry: Update comment

--- common/registry/default.nix
+++ common/registry/default.nix
@@ -13,13 +13,13 @@ let
     lib.filterAttrs
       (
         # Only set nixpkgs registry if in nix store
-        # When NPINS_OVERRIDE_nixpkgs is used, nixpkgs registry will be something like
+        # When NIXLOCK_OVERRIDE_nixpkgs is used, nixpkgs registry will be something like
         # `path:/home/user/src/nixpkgs`, and then `nix run nixpkgs#foo` will copy the
         # entire nixpkgs repo into nix store
         name: value: builtins.elem name cfg.entries && lib.hasPrefix builtins.storeDir value
       )
       (
-        # Use toString to avoid copying inputs to nix store if NPINS_OVERRIDE_* is used
+        # Use toString to avoid copying inputs to nix store if NIXLOCK_OVERRIDE_* is used
         # nix.registry use builtins.toJSON, which will also copy paths to nix store
         # https://nix.dev/manual/nix/2.26/language/builtins#builtins-toJSON
         builtins.mapAttrs (_: value: toString value.outPath) inputs
