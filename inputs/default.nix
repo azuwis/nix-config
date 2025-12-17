4dab0de8dc490d1756f09c1a018893fb78a0c08f
Zhong Jianxin <azuwis@gmail.com>
2025-12-17 20:13:34 +0800

    nixlock: Provide NIXLOCK_OVERRIDE_* override API

--- inputs/default.nix
+++ inputs/default.nix
@@ -1,2 +1,26 @@
 # https://codeberg.org/FrdrCkII/nixlock
-import ./lock.nix { }
+
+let
+  # https://github.com/nikstur/lon/blob/main/lon.nix
+  # Override with a path defined in an environment variable. If no variable is
+  # set, the original path is used.
+  overrideFromEnv =
+    name: path:
+    let
+      replacement = builtins.getEnv "NIXLOCK_OVERRIDE_${name}";
+    in
+    if replacement == "" then
+      path
+    else
+      # this turns the string into an actual Nix path (for both absolute and
+      # relative paths)
+      path
+      // {
+        outPath =
+          if builtins.substring 0 1 replacement == "/" then
+            /. + replacement
+          else
+            /. + builtins.getEnv "PWD" + "/${replacement}";
+      };
+in
+builtins.mapAttrs overrideFromEnv (import ./lock.nix { })
