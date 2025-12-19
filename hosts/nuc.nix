bbca28baf57bc081ddab69f9d51f9520db5422f6
Zhong Jianxin <azuwis@gmail.com>
2025-12-19 21:09:35 +0800

    nuc: Use services.hwheel2swipe

--- hosts/nuc.nix
+++ hosts/nuc.nix
@@ -55,7 +55,7 @@
   programs.wayland.startup.initlock = lib.mkForce [ ];
   services.evsieve.enable = true;
   services.evsieve.dualsense = true;
-  services.evsieve.mskb = true;
+  services.hwheel2swipe.enable = true;
   services.scinet.enable = true;
   zramSwap.enable = true;
 
