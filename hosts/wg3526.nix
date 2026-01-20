5ebe346ab2c7f9e199b486f3175bbe9a51e60482
Zhong Jianxin <azuwis@gmail.com>
2026-01-20 19:57:45 +0800

    openwrt: Set hostname

--- hosts/wg3526.nix
+++ hosts/wg3526.nix
@@ -9,4 +9,5 @@
   imports = [ ../openwrt ];
 
   builder.profile = "zbtlink_zbt-wg3526-16m";
+  uci.system.hostname = "wg3526";
 }
--- hosts/xr500.nix
+++ hosts/xr500.nix
@@ -9,4 +9,5 @@
   imports = [ ../openwrt ];
 
   builder.profile = "netgear_xr500";
+  uci.system.hostname = "xr500";
 }
