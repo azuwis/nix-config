98d6782afc0fb6b49b2f94005059c58ca3e351dd
Zhong Jianxin <azuwis@gmail.com>
2026-01-20 19:53:07 +0800

    openwrt: Add uci module

--- openwrt/system/default.nix
+++ openwrt/system/default.nix
@@ -12,31 +12,24 @@ in
 {
   options.system = {
     enable = lib.mkEnableOption "system";
-    hostname = lib.mkOption {
-      type = lib.types.str;
-      default = "OpenWrt";
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    files.file."/etc/dropbear/authorized_keys".text = lib.concatStringsSep "\n" config.my.keys;
+    files.file."etc/dropbear/authorized_keys".text = lib.concatStringsSep "\n" config.my.keys;
     files.file."etc/sysctl.conf".text = ''
       net.netfilter.nf_conntrack_max=32768
     '';
-    files.file."/etc/uci-defaults/99-system".text = ''
-      uci batch <<EOF
-      set dhcp.@dnsmasq[0].cachesize=1024
-      add_list dhcp.@dnsmasq[0].rebind_domain='/netease.com/'
-      set dhcp.lan.start='10'
-      set dhcp.lan.limit='245'
-      set dhcp.lan.leasetime='72h'
-      set dropbear.@dropbear[0].PasswordAuth='0'
-      set dropbear.@dropbear[0].RootPasswordAuth='0'
-      set system.@system[0].hostname='${cfg.hostname}'
-      set system.@system[0].log_buffer_size='256'
-      set system.@system[0].timezone='CST-8'
-      set system.@system[0].zonename='Asia/Shanghai'
-      EOF
-    '';
+    uci = {
+      dhcp."@dnsmasq[0]".cachesize = "1024";
+      dhcp."@dnsmasq[0]".rebind_domain = [ "/netease.com/" ];
+      dhcp.lan.leasetime = "72h";
+      dhcp.lan.limit = "245";
+      dhcp.lan.start = "10";
+      dropbear.PasswordAuth = "0";
+      dropbear.RootPasswordAuth = "0";
+      system.log_buffer_size = "256";
+      system.timezone = "CST-8";
+      system.zonename = "Asia/Shanghai";
+    };
   };
 }
---
+++ openwrt/uci/default.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.uci;
+in
+
+{
+  options.uci = lib.mkOption {
+    default = { };
+    type =
+      with lib.types;
+      attrsOf (
+        attrsOf (oneOf [
+          (oneOf [
+            str
+            (listOf str)
+          ])
+          (attrsOf (oneOf [
+            str
+            (listOf str)
+          ]))
+        ])
+      );
+  };
+
+  config = {
+    files.file = lib.mapAttrs' (
+      name: attrs:
+      let
+        uciList = lib.collect lib.isString (
+          lib.mapAttrsRecursive (
+            path: value:
+            let
+              pathStr =
+                if (lib.length path) == 1 then "@${name}[0].${lib.head path}" else lib.concatStringsSep "." path;
+            in
+            if lib.isList value then
+              (lib.concatMapStringsSep "\n" (x: "add_list ${name}.${pathStr}='${toString x}'") value)
+            else
+              "set ${name}.${pathStr}='${toString value}'"
+          ) attrs
+        );
+      in
+      lib.nameValuePair "etc/uci-defaults/99-${name}" {
+        text = ''
+          uci batch <<EOF
+          ${lib.concatStringsSep "\n" uciList}
+          EOF
+        '';
+      }
+    ) cfg;
+  };
+}
