89c5219179033d1af03e9e1a1b3667765b326878
Zhong Jianxin <azuwis@gmail.com>
2026-01-29 19:29:01 +0800

    wg3526: More uci settings

--- hosts/wg3526.nix
+++ hosts/wg3526.nix
@@ -9,5 +9,11 @@
   imports = [ ../openwrt ];
 
   builder.profile = "zbtlink_zbt-wg3526-16m";
-  uci.system.hostname = "wg3526";
+  uci = {
+    system.hostname = "wg3526";
+    wireless.radio0.disabled = "1";
+    wireless.radio1.channel = "149";
+    wireless.radio1.country = "CN";
+    wireless.radio1.htmode = "VHT80";
+  };
 }
