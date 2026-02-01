1a4b7bec5d261856e5b588e2ee02764714c7ee35
Zhong Jianxin <azuwis@gmail.com>
2026-02-01 21:15:29 +0800

    wg3526: More config

--- hosts/wg3526.nix
+++ hosts/wg3526.nix
@@ -9,7 +9,7 @@
   imports = [ ../openwrt ];
 
   builder.profile = "zbtlink_zbt-wg3526-16m";
-  wireguard.enable = true;
+
   uci = {
     system.hostname = "wg3526";
     wireless.radio0.disabled = "1";
@@ -17,4 +17,7 @@
     wireless.radio1.country = "CN";
     wireless.radio1.htmode = "VHT80";
   };
+
+  wireguard.enable = true;
+  uci.firewall."@zone[0]".network = [ "wg0" ];
 }
