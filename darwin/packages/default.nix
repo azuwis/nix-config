6d5648376c56632b36375786166a10dff14213a8
Zhong Jianxin <azuwis@gmail.com>
2026-02-08 20:26:23 +0800

    darwin/packages: Remove inetutils/telnet
    Just use `nc -v`, `inetutils` is currently broken on darwin

--- darwin/packages/default.nix
+++ darwin/packages/default.nix
@@ -7,11 +7,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    (pkgs.runCommand "telnet-0.0.0" { preferLocalBuild = true; } ''
-      mkdir -p $out/bin $out/share/man/man1/
-      ln -s ${pkgs.inetutils}/bin/telnet $out/bin/
-      ln -s ${pkgs.inetutils}/share/man/man1/telnet.1.gz $out/share/man/man1/
-    '')
     # android-file-transfer bump to use fuse3, not available in nixpkgs' macfuse yet
     # https://github.com/NixOS/nixpkgs/pull/458276
     # android-file-transfer
