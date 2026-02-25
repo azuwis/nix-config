faba8ab98bcbf6847427d01c4ded4d3fa67baaab
Zhong Jianxin <azuwis@gmail.com>
2026-02-25 20:11:25 +0800

    nps: Refactor for upstreaming to nixpkgs

--- common/nps/default.nix
+++ common/nps/default.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   config,
   lib,
   pkgs,
@@ -9,44 +8,96 @@
 let
   cfg = config.programs.nps;
 
-  npsCache =
-    pkgs.runCommand "nps-cache"
+  nixpkgs =
+    let
+      str = toString pkgs.path;
+    in
+    # Allow reference to nixpkgs without copying to nix store again
+    builtins.appendContext str {
+      "${str}" = {
+        path = true;
+      };
+    };
+
+  npsPackageCache =
+    pkgs.runCommand "nps-package-cache"
       {
         nativeBuildInputs = [
+          cfg.package
           pkgs.nix
-          pkgs.nps
           pkgs.writableTmpDirAsHomeHook
         ];
         preferLocalBuild = true;
       }
-      # ref: nixpkgs/lib/path/tests/default.nix
       ''
-        export NIX_CONF_DIR=$HOME/etc
-        export NIX_STATE_DIR=$HOME/state
-        export NIX_STORE_DIR=$HOME/store
-        mkdir -p "$NIX_CONF_DIR"
-        cat >"$NIX_CONF_DIR/nix.conf" <<EOF
+        export NIX_STATE_DIR="$HOME/nix/state"
+        export NIX_STORE_DIR="$HOME/nix/store"
+
+        mkdir -p "$HOME/.config/nix"
+
+        cat >"$HOME/.config/nix/nix.conf" <<EOF
         experimental-features = nix-command flakes
         flake-registry =
         EOF
-        nix registry add nixpkgs path:${inputs.nixpkgs.outPath}
-        NIX_PACKAGE_SEARCH_EXPERIMENTAL=true nps --refresh -dd
+
+        cat >"$HOME/.config/nix/registry.json" <<EOF
+        {
+          "flakes": [
+            {
+              "from": {
+                "id": "nixpkgs",
+                "type": "indirect"
+              },
+              "to": {
+                "path": "${nixpkgs}",
+                "type": "path"
+              }
+            }
+          ],
+          "version": 2
+        }
+        EOF
+
+        NIX_PACKAGE_SEARCH_EXPERIMENTAL=true nps --debug --refresh
         cp -r "$HOME/.nix-package-search" "$out"
       '';
 
-  nps = pkgs.wrapper {
-    package = pkgs.nps;
-    env.NIX_PACKAGE_SEARCH_CACHE_FOLDER_ABSOLUTE_PATH = npsCache;
-    env.NIX_PACKAGE_SEARCH_EXPERIMENTAL = "true";
-  };
+  npsWrapped =
+    pkgs.runCommand "nps-wrapped"
+      {
+        inherit (cfg.package) meta;
+        nativeBuildInputs = [
+          pkgs.makeWrapper
+        ];
+        preferLocalBuild = true;
+      }
+      ''
+        exe="${lib.getExe cfg.package}"
+        makeWrapper "$exe" "$out/bin/$(basename "$exe")" \
+          --set NIX_PACKAGE_SEARCH_CACHE_FOLDER_ABSOLUTE_PATH "${npsPackageCache}" \
+          --set NIX_PACKAGE_SEARCH_EXPERIMENTAL "true"
+      '';
 in
 
 {
   options.programs.nps = {
-    enable = lib.mkEnableOption "programs.nps";
+    enable = lib.mkEnableOption "nps and generate package cache";
+
+    package = lib.mkPackageOption pkgs "nps" { };
+
+    finalPackage = lib.mkOption {
+      type = lib.types.package;
+      readOnly = true;
+      default = npsWrapped;
+      description = ''
+        Resulting nps package.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ nps ];
+    environment.systemPackages = [ cfg.finalPackage ];
   };
+
+  meta.maintainers = with lib.maintainers; [ azuwis ];
 }
