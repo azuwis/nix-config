c6edf26b05c08881a95b19424a01d71707758a6e
Zhong Jianxin <azuwis@gmail.com>
2026-02-26 20:28:15 +0800

    wg3526: Add etherwake

--- hosts/wg3526.nix
+++ hosts/wg3526.nix
@@ -10,10 +10,10 @@
 
   builder.profile = "zbtlink_zbt-wg3526-16m";
 
-  uci = {
-    system.hostname = "wg3526";
-  };
+  uci.system.hostname = "wg3526";
 
   wireguard.enable = true;
   uci.firewall."@zone[0]".network = [ "wg0" ];
+
+  builder.packages = [ "etherwake" ];
 }
