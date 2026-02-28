d79932a757c76218ab866d28d97fd2665da5ea12
Zhong Jianxin <azuwis@gmail.com>
2026-02-28 19:32:08 +0800

    xr500: More config

--- hosts/xr500.nix
+++ hosts/xr500.nix
@@ -10,4 +10,13 @@
 
   builder.profile = "netgear_xr500";
   uci.system.hostname = "xr500";
+
+  ddns.enable = true;
+  hass.enable = true;
+
+  wireguard.enable = true;
+  uci.firewall."@zone[0]".network = [ "wg1" ];
+  uci.firewall."@zone[1]".network = [ "wg0" ];
+
+  builder.packages = [ "etherwake" ];
 }
