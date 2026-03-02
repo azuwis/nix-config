ee5641abcc2d77fc8bc47dc4d6f90047e653cf0b
Zhong Jianxin <azuwis@gmail.com>
2026-03-02 21:10:15 +0800

    openwrt/uci: Make sure wireless.radio0 is always 2g

--- openwrt/uci/default.nix
+++ openwrt/uci/default.nix
@@ -51,7 +51,7 @@ in
             ) attrs
           );
         in
-        lib.nameValuePair "etc/uci-defaults/90-${name}" {
+        lib.nameValuePair "etc/uci-defaults/92-${name}" {
           text = ''
             uci batch <<'EOF'
             ${lib.concatStringsSep "\n" uciList}
@@ -61,6 +61,22 @@ in
         }
       ) cfg
       // {
+        # radio0 and radio1 are not consistent between fresh install, swap them
+        # to make sure radio0 is always 2g
+        "etc/uci-defaults/90-wireless-order".text = ''
+          if [ "$(uci get wireless.radio1.band)" = 2g ]; then
+            uci batch <<'EOF'
+            set wireless.default_radio0.device=radio1
+            set wireless.default_radio1.device=radio0
+            rename wireless.default_radio0=default_radio9
+            rename wireless.default_radio1=default_radio0
+            rename wireless.default_radio9=default_radio1
+            rename wireless.radio0=radio9
+            rename wireless.radio1=radio0
+            rename wireless.radio9=radio1
+          EOF
+          fi
+        '';
         "etc/uci-defaults/99-chmod-etc-config".text = ''
           chmod 600 /etc/config/*
         '';
