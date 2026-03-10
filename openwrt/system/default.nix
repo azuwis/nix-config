9db2805d770768c4b9f28b1a14716b835398f89b
Zhong Jianxin <azuwis@gmail.com>
2026-03-10 20:50:24 +0800

    openwrt/system: Add some comments

--- openwrt/system/default.nix
+++ openwrt/system/default.nix
@@ -18,8 +18,7 @@ in
     files.file."etc/dropbear/authorized_keys".text = lib.concatStringsSep "\n" config.my.keys;
     files.file."etc/sysctl.conf".text = lib.mkMerge [
       "net.netfilter.nf_conntrack_max=32768"
-      # Add trailing newline
-      (lib.mkAfter "")
+      (lib.mkAfter "") # Add trailing newline
     ];
     files.file."etc/uci-defaults/96-root-password".text = ''
       root_password_hash=$(uci -q get 'system.@system[0].password')
@@ -33,11 +32,11 @@ in
       dhcp."@dnsmasq[0]".rebind_domain = [ "/netease.com/" ];
       dhcp.lan.force = "1"; # Skip checking other DHCP servers in lan to reduce startup time
       dhcp.lan.leasetime = "72h";
-      dhcp.lan.limit = "245";
-      dhcp.lan.start = "10";
+      dhcp.lan.limit = "245"; # DHCP IP 10~254, 245 total
+      dhcp.lan.start = "10"; # Static IP 1~9
       dropbear."@dropbear[0]".PasswordAuth = "0";
       dropbear."@dropbear[0]".RootPasswordAuth = "0";
-      system."@system[0]".log_buffer_size = "256";
+      system."@system[0]".log_buffer_size = "256"; # Size in KiB, for logread
       system."@system[0]".timezone = "CST-8";
       system."@system[0]".zonename = "Asia/Shanghai";
     };
