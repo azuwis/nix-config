a063e399a44dce0e274848bc5288a1cc47218195
Zhong Jianxin <azuwis@gmail.com>
2026-03-10 21:02:23 +0800

    openwrt/firewall: Enable flow_offloading_hw for all mt7621 devices

--- hosts/wg3526.nix
+++ hosts/wg3526.nix
@@ -11,8 +11,6 @@
   builder.profile = "zbtlink_zbt-wg3526-16m";
   uci.system."@system[0]".hostname = "wg3526";
 
-  uci.firewall."@defaults[0]".flow_offloading_hw = "1";
-
   wireguard.enable = true;
   uci.firewall."@zone[0]".network = [ "wg0" ];
 
