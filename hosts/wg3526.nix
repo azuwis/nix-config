78a067b67db0aaeaeff867ebf8181afc848dc5fb
Zhong Jianxin <azuwis@gmail.com>
2026-03-12 21:36:18 +0800

    wg3526: Move mt7621 workaround to host, drop mt7621 module
    The module access config.image, break running openwrtConfigurations.*.config.sops.save on platforms other than x86_64-linux

--- hosts/wg3526.nix
+++ hosts/wg3526.nix
@@ -16,4 +16,23 @@
   uci.firewall."@zone[0]".network = [ "wg0" ];
 
   builder.packages = [ "etherwake" ];
+
+  # >900Mb/s NAT, low CPU usage, may break SQM on wan
+  uci.firewall."@defaults[0]".flow_offloading_hw = "1";
+
+  # Workaround for mt76x2e firmware crash https://github.com/openwrt/mt76/issues/407
+  # mt76x2e 0000:01:00.0: Firmware running!
+  # ieee80211 phy1: Hardware restart was requested
+  sqm.enable = true;
+  uci.sqm.wifi = {
+    ".type" = "queue";
+    # Download for wifi interface is upload for wifi clients, result max
+    # upload speed: 270 Mb/s to devices ln lan, 116 Mb/s to devices on wan,
+    # softirq is the bottleneck
+    download = "300000";
+    enabled = "1";
+    interface = "phy1-ap0";
+    script = "simplest.qos";
+    upload = "0";
+  };
 }
