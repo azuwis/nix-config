7eb87c128417bbacd128b285f466de257c47c54d
Zhong Jianxin <azuwis@gmail.com>
2026-03-12 20:56:50 +0800

    openwrt/mt7621: Init module, add workaround for firmware crash problem

--- openwrt/firewall/default.nix
+++ openwrt/firewall/default.nix
@@ -17,14 +17,8 @@ in
   config = lib.mkIf cfg.enable {
     # https://github.com/openwrt/openwrt/blob/main/package/network/config/firewall/files/firewall.config
     uci = {
-      firewall."@defaults[0]" = {
-        # For >800Mb/s NAT, but may break SQM
-        flow_offloading = "1";
-      }
-      // lib.optionalAttrs (config.image.config.variant == "mt7621") {
-        # >900Mb/s NAT, low CPU usage
-        flow_offloading_hw = "1";
-      };
+      # For >800Mb/s NAT
+      firewall."@defaults[0]".flow_offloading = "1";
       firewall."@zone[1]".forward = "DROP";
       firewall."@zone[1]".input = "DROP";
     };
---
+++ openwrt/mt7621/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  config = lib.mkIf (config.image.config.variant == "mt7621") {
+    # >900Mb/s NAT, low CPU usage, may break SQM on wan
+    uci.firewall."@defaults[0]".flow_offloading_hw = "1";
+
+    # Workaround for mt76x2e firmware crash https://github.com/openwrt/mt76/issues/407
+    # mt76x2e 0000:01:00.0: Firmware running!
+    # ieee80211 phy1: Hardware restart was requested
+    # XXX: Not all mt7621 devices have mt76x2e, but many have this problem,
+    # let's put this in mt7621 module
+    sqm.enable = true;
+    uci.sqm.wifi = {
+      ".type" = "queue";
+      download = "200000"; # Download for wifi interface is upload for wifi clients
+      enabled = "1";
+      interface = "phy1-ap0";
+      script = "piece_of_cake.qos";
+      upload = "0";
+    };
+  };
+}
