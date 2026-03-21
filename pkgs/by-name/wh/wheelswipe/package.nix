509010c828701f36c13cd623e81fd9c6662ebf5e
Zhong Jianxin <azuwis@gmail.com>
2026-03-21 21:51:11 +0800

    wheelswipe: 0-unstable-2026-03-14 -> 0-unstable-2026-03-21

--- pkgs/by-name/wh/wheelswipe/package.nix
+++ pkgs/by-name/wh/wheelswipe/package.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "wheelswipe";
-  version = "0-unstable-2026-03-14";
+  version = "0-unstable-2026-03-21";
 
   src = fetchFromGitHub {
     owner = "azuwis";
     repo = "wheelswipe";
-    rev = "03ee161575d2264ca8b7e7db1594163720052f23";
-    hash = "sha256-kUBdhEjpcpLJVqr+z9SnatHma4AHxziWw/AlAHbBeMw=";
+    rev = "d1104a571bac293901351df8d51a98854ea295d9";
+    hash = "sha256-f7H5yuRT5Jr1CoN+5HeLp72w38wPZ4EBAHiWG8qidek=";
   };
 
   installPhase = ''
