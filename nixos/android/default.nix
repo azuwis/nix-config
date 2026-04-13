a2c961c0c0c8269226824db9182e174e7ee08505
Zhong Jianxin <azuwis@gmail.com>
2026-04-13 20:19:13 +0800

    android: Add udev rules for MTP mode

--- nixos/android/default.nix
+++ nixos/android/default.nix
@@ -17,5 +17,9 @@ in
   config = mkIf cfg.enable {
     programs.adb.enable = true;
     environment.systemPackages = [ pkgs.android-file-transfer ];
+    services.udev.extraRules = ''
+      # Huawei phones in MTP mode
+      SUBSYSTEM=="usb", ATTR{idVendor}=="12d1", ENV{ID_USB_INTERFACES}=="*:ffff00:*", ENV{ID_MEDIA_PLAYER}="1"
+    '';
   };
 }
