c0d4e27dd0f66ba2675b0b68bf191ee4294efefa
Zhong Jianxin <azuwis@gmail.com>
2026-04-20 19:49:08 +0800

    openwrt/system: Rewrite wireless-order using ucode, support arbitrary radios

--- openwrt/system/default.nix
+++ openwrt/system/default.nix
@@ -47,23 +47,9 @@ in
         '
       '';
 
-      # radio0 and radio1 are not consistent between fresh install, swap them
-      # if needed to make sure radio0 is always 2g
-      "etc/uci-defaults/90-wireless-order".text = ''
-        if [ "$(uci -q get wireless.radio1.band)" = 2g ]; then
-          uci batch <<'EOF'
-        set wireless.default_radio0.device=radio1
-        set wireless.default_radio1.device=radio0
-        rename wireless.default_radio0=default_radio9
-        rename wireless.default_radio1=default_radio0
-        rename wireless.default_radio9=default_radio1
-        rename wireless.radio0=radio9
-        rename wireless.radio1=radio0
-        rename wireless.radio9=radio1
-        commit wireless
-        EOF
-        fi
-      '';
+      # Radios are not consistent between fresh install, sort them by band (2g,
+      # 5g, 6g) so radio0 is always 2g, radio1 is 5g, etc.
+      "etc/uci-defaults/90-wireless-order".source = ./wireless-order.sh;
 
       # etc/uci-defaults/92-uci-import, in ../uci/default.nix
       # etc/uci-defaults/95-sops-enc, in ../sops/default.nix
---
+++ openwrt/system/wireless-order.sh
@@ -0,0 +1,33 @@
+ucode -e '
+import { cursor } from "uci";
+
+const uci = cursor();
+let radios = [];
+uci.foreach("wireless", "wifi-device", (s) => {
+	let m = match(s[".name"], /^radio(\d+)$/);
+	if (m)
+		push(radios, { idx: +m[1], band: s.band });
+});
+
+let sorted = sort([...radios], (a, b) =>
+	(a.band > b.band) - (a.band < b.band) || a.idx - b.idx);
+
+if (length(filter(sorted, (r, i) => r.idx != i)) == 0)
+	exit(0);
+
+for (let r in radios) {
+	uci.rename("wireless", `radio${r.idx}`, `_radio${r.idx}`);
+	if (uci.get("wireless", `default_radio${r.idx}`))
+		uci.rename("wireless", `default_radio${r.idx}`, `_default_radio${r.idx}`);
+}
+
+for (let i = 0; i < length(sorted); i++) {
+	uci.rename("wireless", `_radio${sorted[i].idx}`, `radio${i}`);
+	if (uci.get("wireless", `_default_radio${sorted[i].idx}`)) {
+		uci.set("wireless", `_default_radio${sorted[i].idx}`, "device", `radio${i}`);
+		uci.rename("wireless", `_default_radio${sorted[i].idx}`, `default_radio${i}`);
+	}
+}
+
+uci.commit("wireless");
+'
