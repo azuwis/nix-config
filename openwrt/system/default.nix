0447a06df2babc249cd8def49dd2ef6b0d9a4a7c
Zhong Jianxin <azuwis@gmail.com>
2026-04-20 19:29:24 +0800

    openwrt/system: Move some config from uci
    Leave openwrt/uci/default.nix only handle config.uci

--- openwrt/system/default.nix
+++ openwrt/system/default.nix
@@ -32,6 +32,24 @@ in
         (lib.mkAfter "") # Add trailing newline
       ];
 
+      # radio0 and radio1 are not consistent between fresh install, swap them
+      # if needed to make sure radio0 is always 2g
+      "etc/uci-defaults/90-wireless-order".text = ''
+        if [ "$(uci -q get wireless.radio1.band)" = 2g ]; then
+          uci batch <<'EOF'
+        set wireless.default_radio0.device=radio1
+        set wireless.default_radio1.device=radio0
+        rename wireless.default_radio0=default_radio9
+        rename wireless.default_radio1=default_radio0
+        rename wireless.default_radio9=default_radio1
+        rename wireless.radio0=radio9
+        rename wireless.radio1=radio0
+        rename wireless.radio9=radio1
+        commit wireless
+        EOF
+        fi
+      '';
+
       "etc/uci-defaults/91-dnsmasq-rename".text = ''
         ucode -e '
         import { cursor } from "uci";
@@ -51,6 +69,10 @@ in
         if [ -n "$root_password_hash" ]; then
           sed -i "s|^root:[^:]*|root:$root_password_hash|g" /etc/shadow
         fi
+      '';
+
+      "etc/uci-defaults/99-chmod-etc-config".text = ''
+        chmod 600 /etc/config/*
       '';
     };
     uci = {
--- openwrt/uci/default.nix
+++ openwrt/uci/default.nix
@@ -26,31 +26,11 @@ in
 
   config = {
     files.file = {
-      # radio0 and radio1 are not consistent between fresh install, swap them
-      # if needed to make sure radio0 is always 2g
-      "etc/uci-defaults/90-wireless-order".text = ''
-        if [ "$(uci -q get wireless.radio1.band)" = 2g ]; then
-          uci batch <<'EOF'
-        set wireless.default_radio0.device=radio1
-        set wireless.default_radio1.device=radio0
-        rename wireless.default_radio0=default_radio9
-        rename wireless.default_radio1=default_radio0
-        rename wireless.default_radio9=default_radio1
-        rename wireless.radio0=radio9
-        rename wireless.radio1=radio0
-        rename wireless.radio9=radio1
-        commit wireless
-        EOF
-        fi
-      '';
       "etc/uci-defaults/92-uci-import".text = ''
         uci-import <<'EOF'
         ${builtins.toJSON cfg}
         EOF
         uci commit
-      '';
-      "etc/uci-defaults/99-chmod-etc-config".text = ''
-        chmod 600 /etc/config/*
       '';
       "usr/bin/uci-import".source = ./uci-import.js;
     };
