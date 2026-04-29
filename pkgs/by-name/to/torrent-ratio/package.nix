686ce30d1a60d266ed23c71a740840d5b11dba71
Zhong Jianxin <azuwis@gmail.com>
2026-04-29 19:45:53 +0800

    torrent-ratio: 0.10 -> 0.11

--- pkgs/by-name/to/torrent-ratio/package.nix
+++ pkgs/by-name/to/torrent-ratio/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "torrent-ratio";
-  version = "0.10";
+  version = "0.11";
 
   src = fetchFromGitHub {
     owner = "azuwis";
     repo = "torrent-ratio";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-itIvizeedkN6VoTyHrraDV1yLC4v8dO8vzt5K0KRzq4=";
+    sha256 = "sha256-iJOOQNuWmD5++ZMmsK0Hq10QRufbBlOt5sXMmhp1jpQ=";
   };
 
   vendorHash = "sha256-+eqtiSDnAXlFqjOVunEDD862gf4k569/DLpmshQBJog=";
