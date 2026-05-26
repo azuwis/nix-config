005fb0f47a9799ba8605cab7d134ca9f80c74b36
Zhong Jianxin <azuwis@gmail.com>
2026-05-26 23:08:30 +0800

    darwin: Add back android-file-transfer

--- darwin/packages/default.nix
+++ darwin/packages/default.nix
@@ -7,9 +7,7 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # android-file-transfer bump to use fuse3, not available in nixpkgs' macfuse yet
-    # https://github.com/NixOS/nixpkgs/pull/458276
-    # android-file-transfer
+    android-file-transfer
     android-tools
     blueutil
     coreutils-full
