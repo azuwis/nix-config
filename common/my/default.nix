97c5c4e7a1980891244a289df3cf41e116d6ea7c
azuwis <azuwis@users.noreply.github.com>
2026-05-29 22:17:51 +0800

    common/my: Update

--- common/my/default.nix
+++ common/my/default.nix
@@ -24,10 +24,8 @@ in
 
   config = {
     my = {
-      inherit (import inputs.my.outPath) builder domain;
+      inherit (import inputs.my.outPath) builder email domain;
       user = "azuwis";
-      name = "Zhong Jianxin";
-      email = "azuwis@gmail.com";
       uid = 1000;
       keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBa4stB3iashVWVH3XA4CgciZ7UvnvMCNyc7nkKqpiqMs1TQNg+dayNv+rX6zJh+WEWL4bZ5SNr+cnlHpF6wCcZlicFh7vli4FKK0DgmcASJPRHqN98dBQ9aH+4ZIUcjuPaAPeA/0EO+o8RVNTCnOD3QtJjNPh6Kr1FW2THE+AMpdotnS5pUpH4hrFaaEhRkvOwUeaJ/kxs+KYqPTyq4pNyoSuG6aTzjwCpTJB1tbma+7/t8809Cpy6q48jh2VhB6Sb6sxcFi3KMo/i6I7SpmhlgPTEBDDtloQaP7uhgtkP7UTCldUU2UV8BrAXIRFHBccan1La2MJ3Z5vrr67zSkb"
