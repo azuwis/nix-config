de48c47cd1837d54703a200fbafe09356290bafd
azuwis <azuwis@users.noreply.github.com>
2026-06-10 20:50:57 +0800

    fence-pi: Add pi-exa-mcp and pi-superpowers extensions

--- pkgs/by-name/fe/fence-pi/package.nix
+++ pkgs/by-name/fe/fence-pi/package.nix
@@ -1,6 +1,31 @@
 {
+  lib,
+  stdenv,
   fence-agent,
+  fetchFromGitHub,
   pi-coding-agent,
+  writeScript,
+  piExtensions ? {
+    pi-exa-mcp = {
+      version = "2026-06-08";
+    }
+    // fetchFromGitHub {
+      owner = "ben-vargas";
+      repo = "pi-packages";
+      tag = "2026-06-08";
+      rootDir = "packages/pi-exa-mcp";
+      hash = "sha256-Wqw+Gp7skL5S4xB9Ktq9rs6F6ulA1XAuXb9PKe2pQV0=";
+    };
+    pi-superpowers = {
+      version = "0-unstable-2026-03-05";
+    }
+    // fetchFromGitHub {
+      owner = "coctostan";
+      repo = "pi-superpowers";
+      rev = "efe1d158691bf064c24f0460fd4e46ca58de0055";
+      hash = "sha256-cwGs/Hdbk3h9Yk+vEMtXSyAgy+grxEc6eZAHZZjpcDk=";
+    };
+  },
 }:
 
 # Network access: configure in ~/.config/fence/fence.json, e.g.:
@@ -10,7 +35,6 @@
 #     "deniedDomains": ["*.sentry.io"]
 #   }
 # }
-#
 
 fence-agent {
   name = "fence-pi";
@@ -27,9 +51,30 @@ fence-agent {
     "PI_TELEMETRY"
     "false"
     "--add-flags"
-    "--extension ${pi-coding-agent}/lib/node_modules/pi-monorepo/examples/extensions/subagent"
+    "--extension ${pi-coding-agent}/lib/node_modules/pi-monorepo/examples/extensions/subagent ${
+      lib.concatMapAttrsStringSep " " (_: p: "--extension ${p}") piExtensions
+    }"
   ];
   preExecScript = ''
     mkdir -p ~/.pi
   '';
+  extraPassthru = {
+    extensionsUpdate = builtins.mapAttrs (
+      name: plugin:
+      stdenv.mkDerivation {
+        inherit (plugin) version;
+        pname = "pi-plugin-${name}";
+        src = plugin;
+      }
+    ) piExtensions;
+    updateScript = writeScript "update-fence-pi-plugins" ''
+      #!/usr/bin/env nix-shell
+      #!nix-shell -i bash -p nix-update
+
+      set -eu -o pipefail
+
+      nix-update fence-pi.extensionsUpdate.pi-exa-mcp
+      nix-update fence-pi.extensionsUpdate.pi-superpowers --version=branch
+    '';
+  };
 }
