0461ef08d5074c35acb63cfb870b6cc2c24a5447
azuwis <azuwis@users.noreply.github.com>
2026-06-10 20:40:29 +0800

    llama-cpp: Init module, split from aor

---
+++ nixos/llama-cpp/default.nix
@@ -0,0 +1,80 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.services.llama-cpp;
+in
+
+{
+  options.services.llama-cpp = {
+    enhance = lib.mkEnableOption "enable and enhance llama server";
+  };
+
+  config = lib.mkIf cfg.enhance {
+    services.llama-cpp = {
+      enable = true;
+      package = pkgs.llama-cpp.override { cudaSupport = true; };
+      extraFlags = [
+        # `models-max` does not work in modelsPreset."*"
+        "--models-max"
+        "1"
+      ];
+      modelsPreset = {
+        "*" = {
+          sleep-idle-seconds = "600";
+        };
+        # https://unsloth.ai/docs/models/qwen3.6
+        # https://knightli.com/en/2026/05/26/rtx-3060-llama-cpp-n-cpu-moe-local-35b/
+        "qwen3.6" = {
+          alias = "qwen3.6";
+          # download
+          hf-repo = "unsloth/Qwen3.6-35B-A3B-GGUF:UD-Q4_K_XL"; # 22.4G
+          # sampling
+          temperature = "0.6";
+          top-p = "0.95";
+          top-k = "20";
+          min-p = "0.00";
+          jinja = true; # use embedded chat template
+          reasoning-format = "deepseek"; # parse <think> tags
+          # engine
+          flash-attn = "on";
+          n-gpu-layers = "99"; # all layers to GPU
+          n-cpu-moe = "32"; # MoE experts on CPU (key for 12GB VRAM)
+          # memory
+          ctx-size = "262144"; # 256K
+          cache-type-k = "q8_0"; # K cache must stay >= q8_0 for Qwen (q4_0 = catastrophic)
+          cache-type-v = "q8_0"; # V cache can drop to q4_0 if VRAM tight (~0.3% PPL)
+          parallel = "1"; # single user, save ~700MB VRAM (no extra KV cache slots)
+          no-context-shift = true; # stop at context limit instead of evicting old messages
+        };
+        # https://huggingface.co/HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive
+        "qwen3.6-hau" = {
+          alias = "qwen3.6-hau";
+          # download
+          hf-repo = "HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive:Q4_K_P"; # 23.4G
+          # sampling
+          temperature = "1.0";
+          top-p = "0.95";
+          top-k = "20";
+          min-p = "0.00";
+          jinja = true;
+          reasoning-format = "deepseek";
+          # engine
+          flash-attn = "on";
+          n-gpu-layers = "99";
+          n-cpu-moe = "32";
+          # memory
+          ctx-size = "262144";
+          cache-type-k = "q8_0";
+          cache-type-v = "q8_0";
+          parallel = "1";
+          no-context-shift = true;
+        };
+      };
+    };
+  };
+}
