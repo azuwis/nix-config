7eca83f57c8aa0297d6795c3274dd9f306df8d5b
azuwis <azuwis@users.noreply.github.com>
2026-06-10 20:37:58 +0800

    aor: Add more llama-cpp model

--- hosts/aor.nix
+++ hosts/aor.nix
@@ -152,6 +152,29 @@
         parallel = "1"; # single user, save ~700MB VRAM (no extra KV cache slots)
         no-context-shift = true; # stop at context limit instead of evicting old messages
       };
+      # https://huggingface.co/HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive
+      "qwen3.6-hau" = {
+        alias = "qwen3.6-hau";
+        # download
+        hf-repo = "HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive:Q4_K_P"; # 23.4G
+        # sampling
+        temperature = "1.0";
+        top-p = "0.95";
+        top-k = "20";
+        min-p = "0.00";
+        jinja = true;
+        reasoning-format = "deepseek";
+        # engine
+        flash-attn = "on";
+        n-gpu-layers = "99";
+        n-cpu-moe = "32";
+        # memory
+        ctx-size = "262144";
+        cache-type-k = "q8_0";
+        cache-type-v = "q8_0";
+        parallel = "1";
+        no-context-shift = true;
+      };
     };
   };
 }
