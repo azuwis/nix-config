ef37a5ad25537db792b7905410cdfb6ea9470366
azuwis <azuwis@users.noreply.github.com>
2026-06-25 19:45:18 +0800

    pen-claude: Init

---
+++ pkgs/by-name/pe/pen-claude/package.nix
@@ -0,0 +1,109 @@
+{
+  lib,
+  stdenv,
+  pen,
+  claude-code,
+  fetchFromGitHub,
+  writeScript,
+  claudePlugins ? {
+    mattpocock-skills =
+      let
+        version = "1.0.1";
+      in
+      {
+        inherit version;
+      }
+      // fetchFromGitHub {
+        owner = "mattpocock";
+        repo = "skills";
+        tag = "v${version}";
+        hash = "sha256-nuHQ+SG5UerKs334Yk5nsxHOncGXQKF1yVdnwwVpLZ8=";
+      };
+    ponytail =
+      let
+        version = "4.8.0";
+      in
+      {
+        inherit version;
+      }
+      // fetchFromGitHub {
+        owner = "DietrichGebert";
+        repo = "ponytail";
+        tag = "v${version}";
+        hash = "sha256-YUHjZfCTOIWrHJUUvnuoRSNG/l7wBuMQx/EdRdbLO3w=";
+      };
+    superpowers =
+      let
+        version = "6.0.3";
+      in
+      {
+        inherit version;
+      }
+      // fetchFromGitHub {
+        owner = "obra";
+        repo = "superpowers";
+        tag = "v${version}";
+        hash = "sha256-+lT2a/qq0SF4k0PgnEDKiuidVlZX2p0vEso4d/5T1os=";
+      };
+  },
+  claudeSettings ? {
+    attribution = {
+      commit = "";
+      pr = "";
+    };
+    effortLevel = "xhigh";
+    env = {
+      CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
+      CLAUDE_CODE_WORKFLOWS = "1";
+    };
+    statusLine = {
+      type = "command";
+      command = ./statusline.jq;
+    };
+    skipDangerousModePermissionPrompt = true;
+    skipWebFetchPreflight = true;
+  },
+}:
+
+let
+  claudeSettingsJson = builtins.toFile "claude-settings.json" (builtins.toJSON claudeSettings);
+in
+
+pen {
+  name = "pen-claude";
+  agentPackage = claude-code;
+  agentWrapperArgs = [
+    "--add-flags"
+    "--dangerously-skip-permissions --effort max --settings ${claudeSettingsJson} ${
+      lib.concatMapAttrsStringSep " " (_: p: "--plugin-dir ${p}") claudePlugins
+    }"
+  ];
+  allowWrite = [
+    "."
+    "~/.claude"
+    "~/.claude.json"
+  ];
+  extraPassthru = {
+    pluginsUpdate = builtins.mapAttrs (
+      name: plugin:
+      if plugin ? src then
+        plugin
+      else
+        stdenv.mkDerivation {
+          pname = "claude-plugin-${name}";
+          version = plugin.version or "0";
+          src = plugin;
+        }
+    ) claudePlugins;
+    updateScript = writeScript "update-eden" ''
+      #!/usr/bin/env nix-shell
+      #!nix-shell -i bash -p nix-update
+
+      set -eu -o pipefail
+
+      nix-update pen-claude.pluginsUpdate.mattpocock-skills
+      nix-update pen-claude.pluginsUpdate.ponytail
+      nix-update pen-claude.pluginsUpdate.superpowers
+    '';
+  };
+}
---
+++ pkgs/by-name/pe/pen-claude/statusline.jq
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S jq -Mrf
+
+((.cwd // .workspace.current_dir) | if startswith(env.HOME) then "~" + ltrimstr(env.HOME) else . end) as $cwd |
+.model.id as $model |
+((.context_window.used_percentage // 0) | floor | tostring + "%") as $usage |
+"\($cwd) │ \($model) \($usage)"
