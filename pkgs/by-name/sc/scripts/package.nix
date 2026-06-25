048ab01e27c99dc28bf84b1d40f4cee2f4378eb5
azuwis <azuwis@users.noreply.github.com>
2026-06-25 20:01:14 +0800

    scripts: add zsh completion for os command

--- pkgs/by-name/sc/scripts/package.nix
+++ pkgs/by-name/sc/scripts/package.nix
@@ -2,10 +2,12 @@
 
 let
   bin = ./bin;
+  zsh = ./zsh;
 in
 
 runCommand "scripts" { preferLocalBuild = true; } ''
-  mkdir -p $out/bin
+  mkdir -p $out/bin $out/share/zsh/site-functions
   cp ${bin}/* $out/bin
   chmod +x $out/bin/*
+  cp ${zsh}/_os $out/share/zsh/site-functions/_os
 ''
