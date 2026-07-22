7ef6295b3c3f0c9a2f5d872853af86235a2bac81
azuwis <azuwis@users.noreply.github.com>
2026-07-22 21:02:31 +0800

    inputs: add ref for all inputs
    So builtins.fetchGit does not call git ls-remote --symref

--- inputs/inputs.nix
+++ inputs/inputs.nix
@@ -1,9 +1,11 @@
 {
   agenix = {
     url = "https://github.com/ryantm/agenix";
+    ref = "main";
   };
   devshell = {
     url = "https://github.com/numtide/devshell";
+    ref = "main";
   };
   disko = {
     url = "https://github.com/nix-community/disko";
@@ -11,9 +13,11 @@
   };
   homebrew-cask = {
     url = "https://github.com/Homebrew/homebrew-cask";
+    ref = "main";
   };
   jovian-nixos = {
     url = "https://github.com/Jovian-Experiments/Jovian-NixOS";
+    ref = "development";
     rev = "8f97d1dee8c0971b01cd6b17ccf913e8bb70f5d5"; # last version compatible with nixos-26.05
   };
   my = {
@@ -26,19 +30,24 @@
   };
   nix-homebrew = {
     url = "https://github.com/zhaofengli/nix-homebrew";
+    ref = "main";
   };
   nix-index-database = {
     url = "https://github.com/azuwis/nix-index-database";
+    ref = "main";
   };
   nix-on-droid = {
     url = "https://github.com/nix-community/nix-on-droid";
+    ref = "master";
   };
   nix-openwrt-imagebuilder = {
     url = "https://github.com/astro/nix-openwrt-imagebuilder";
+    ref = "main";
     freeze = true;
   };
   nixos-wsl = {
     url = "https://github.com/nix-community/NixOS-WSL";
+    ref = "main";
   };
   nixpkgs = {
     url = "https://github.com/NixOS/nixpkgs";
