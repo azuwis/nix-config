d68fc4bc1eb4656704e2171c524b45dfe5f09f49
azuwis <azuwis@users.noreply.github.com>
2026-07-28 20:57:05 +0800

    yabai-git: Use yabai.overrideAttrs
    It's possible to build yabai from source purely now

--- pkgs/by-name/ya/yabai-git/package.nix
+++ pkgs/by-name/ya/yabai-git/package.nix
@@ -1,55 +1,16 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-}:
+{ yabai, nix-update-script }:
 
-stdenv.mkDerivation {
-  pname = "yabai";
+yabai.overrideAttrs (old: {
+  pname = "yabai-git";
   version = "unstable-2023-05-16";
 
-  src = fetchFromGitHub {
-    owner = "koekeishiya";
-    repo = "yabai";
-    # rev = "v${version}";
+  src = old.src.override {
     rev = "4d81baf14f7219ed2d929761fed1186bcd793b29";
     sha256 = "03qmwk0qb9wzh6nlk06zb3r1ff6abw3xawdhxbmis98678zwkzh7";
   };
 
-  postPatch =
-    let
-      replace =
-        {
-          aarch64-darwin = ''--replace-fail "-arch x86_64" ""'';
-          x86_64-darwin = ''--replace-fail "-arch arm64e" "" --replace-fail "-arch arm64" ""'';
-        }
-        .${stdenv.system};
-    in
-    ''
-      substituteInPlace makefile ${replace}
-    '';
-
-  buildPhase = ''
-    PATH=/usr/bin:/bin /usr/bin/make install
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    mkdir -p $out/share/man/man1/
-    cp ./bin/yabai $out/bin/yabai
-    cp ./doc/yabai.1 $out/share/man/man1/yabai.1
-  '';
-
-  meta = with lib; {
-    description = ''
-      A tiling window manager for macOS based on binary space partitioning
-    '';
-    homepage = "https://github.com/koekeishiya/yabai";
-    platforms = platforms.darwin;
-    maintainers = with maintainers; [
-      cmacrae
-      shardy
-    ];
-    license = licenses.mit;
+  passthru = (old.passthru or { }) // {
+    enable = false;
+    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
   };
-}
+})
