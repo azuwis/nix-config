4ee131841a44f6ae2821f13bf64ed82799faf985
azuwis <azuwis@users.noreply.github.com>
2026-08-05 20:40:21 +0800

    sketchybar: Add thunderbird icon

--- darwin/sketchybar/config/icons.lua
+++ darwin/sketchybar/config/icons.lua
@@ -1,9 +1,11 @@
 return {
   apps = {
+    -- https://www.nerdfonts.com/cheat-sheet
     Finder = "",
     Firefox = "",
     Mail = "󰻧",
     Moonlight = "󰺵",
+    Thunderbird = "",
     ["Google Chrome"] = "",
     ["System Settings"] = "",
     ["网易POPO"] = "󰍦",
