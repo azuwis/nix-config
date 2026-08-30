1149b148762ecba4030b7185bab1ce43ad4042f7
azuwis <azuwis@users.noreply.github.com>
2026-08-30 20:37:20 +0800

    xiaomi_miot: Switch to unstable version

--- pkgs/by-name/xi/xiaomi_miot/package.nix
+++ pkgs/by-name/xi/xiaomi_miot/package.nix
@@ -1,15 +1,16 @@
-{ home-assistant-custom-components }:
+{ home-assistant-custom-components, nix-update-script }:
 
 home-assistant-custom-components.xiaomi_miot.overridePythonAttrs (old: rec {
-  version = "1.1.4";
+  version = "1.1.4-unstable-2026-07-30";
 
   src = old.src.override {
-    rev = "v${version}";
-    hash = "sha256-t1kOPiZR0CxOsp2V4cJNi+aiDdr7VhqhX8jOAiKTemk=";
+    rev = "17945db6b2f1f02249be49dfda14ae650263a2ca";
+    hash = "sha256-57vcF6UpD4r+cMRWDFjg8uXXOdT3hHj1vYpCrlnEldY=";
   };
 
   passthru = (old.passthru or { }) // {
     enable = true;
+    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
   };
 
   meta = (old.meta or { }) // {
